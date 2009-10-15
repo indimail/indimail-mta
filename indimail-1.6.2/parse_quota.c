13bdfa9fa78e62a04874b8593fb1296d16e5e1e1
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2009-10-15 10:48:01 +0530

    use strtoll and check for LLONG_MIN, LLONG_MAX

--- indimail-1.6.2/parse_quota.c
+++ indimail-1.6.2/parse_quota.c
@@ -1,5 +1,11 @@
 /*
  * $Log: parse_quota.c,v $
+ * Revision 2.4  2009-10-15 10:47:32+05:30  Cprogrammer
+ * skip LLONG_MAX, LLONG_MIN if not defined and use ERANGE
+ *
+ * Revision 2.3  2009-10-14 20:44:57+05:30  Cprogrammer
+ * use strtoll() instead of atol() for better error checking
+ *
  * Revision 2.2  2008-06-24 21:48:53+05:30  Cprogrammer
  * porting for 64 bit
  *
@@ -11,12 +17,17 @@
  *
  */
 #include "indimail.h"
+#define XOPEN_SOURCE = 600
 #include <stdlib.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <string.h>
+#if !defined(LLONG_MIN) && !defined(LLONG_MAX)
+#include <errno.h>
+#endif
 
 #ifndef	lint
-static char     sccsid[] = "$Id: parse_quota.c,v 2.2 2008-06-24 21:48:53+05:30 Cprogrammer Stab mbhangui $";
+static char     sccsid[] = "$Id: parse_quota.c,v 2.4 2009-10-15 10:47:32+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 mdir_t
@@ -32,11 +43,25 @@ parse_quota(char *quota, mdir_t *count)
 	{
 		*ptr = 0;
 		if(count)
-			*count = atol(ptr + 1);
+		{
+			*count = strtoll(ptr + 1, 0, 0);
+#if defined(LLONG_MIN) && defined(LLONG_MAX)
+			if (*count == LLONG_MIN || *count == LLONG_MAX)
+#else
+			if (errno == ERANGE)
+#endif
+				return (-1);
+		}
 	} else
 	if(count)	
 		*count = 0;
-	per_user_limit = atol(tmpbuf);
+	per_user_limit = strtoll(tmpbuf, 0, 0);
+#if defined(LLONG_MIN) && defined(LLONG_MAX)
+	if (per_user_limit == LLONG_MIN || per_user_limit == LLONG_MAX)
+#else
+	if (errno == ERANGE)
+#endif
+		return (-1);
 	for (i = 0; quota[i] != 0; ++i)
 	{
 		if (quota[i] == 'k' || quota[i] == 'K')
