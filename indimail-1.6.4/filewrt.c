e7eb311842415616d5f0b8ee486249ce13cd0c3e
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2009-11-08 00:58:15 +0530

    made code more compact

--- indimail-1.6.4/filewrt.c
+++ indimail-1.6.4/filewrt.c
@@ -1,5 +1,8 @@
 /*
  * $Log: filewrt.c,v $
+ * Revision 2.3  2009-11-08 00:56:18+05:30  Cprogrammer
+ * makde code more compact
+ *
  * Revision 2.2  2004-05-03 22:04:37+05:30  Cprogrammer
  * use stdarg.h instead of varargs.h
  *
@@ -11,7 +14,7 @@
 #include <unistd.h>
 
 #ifndef	lint
-static char     sccsid[] = "$Id: filewrt.c,v 2.2 2004-05-03 22:04:37+05:30 Cprogrammer Stab mbhangui $";
+static char     sccsid[] = "$Id: filewrt.c,v 2.3 2009-11-08 00:56:18+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 #ifdef HAVE_STDARG_H
@@ -19,43 +22,38 @@ static char     sccsid[] = "$Id: filewrt.c,v 2.2 2004-05-03 22:04:37+05:30 Cprog
 /* function to write to a file */
 int
 filewrt(int fout, char *fmt, ...)
-{
-	va_list         ap;
-	char           *ptr;
-	char            buf[2048];
-	unsigned        len;
-
-	va_start(ap, fmt);
-	(void) vsnprintf(buf, sizeof(buf), fmt, ap);
-	va_end(ap);
-	for (len = 0, ptr = buf; *ptr++; len++);
-	return(write(fout, buf, len) != len ? -1 : len);
-}
 #else
 #include <varargs.h>
 int
 filewrt(va_alist)
 va_dcl
+#endif
 {
-	va_list         args;
-	char           *fmt;
+	va_list         ap;
+	char           *ptr;
 	char            buf[2048];
-	int             fout;
 #ifdef SUN41
 	int             len;
 #else
 	unsigned        len;
 #endif
+#ifndef HAVE_STDARG_H
+	int             fout;
+	char           *fmt;
+#endif
 
-	va_start(args);
-	fout = va_arg(args, int);	/* file descriptor */
-	fmt = va_arg(args, char *);
-	(void) vsnprintf(buf, sizeof(buf), fmt, args);
-	va_end(args);
-	for (len = 0, fmt = buf; *fmt++; len++);
+#ifdef HAVE_STDARG_H
+	va_start(ap, fmt);
+#else
+	va_start(ap);
+	fout = va_arg(ap, int);	/* file descriptor */
+	fmt = va_arg(ap, char *);
+#endif
+	(void) vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	for (len = 0, ptr = buf; *ptr++; len++);
 	return(write(fout, buf, len) != len ? -1 : len);
 }
-#endif
 
 void
 getversion_filewrt_c()
--- indimail-1.6.4/mysql_perror.c
+++ indimail-1.6.4/mysql_perror.c
@@ -1,5 +1,8 @@
 /*
  * $Log: mysql_perror.c,v $
+ * Revision 2.4  2009-11-08 00:56:45+05:30  Cprogrammer
+ * made code more compact
+ *
  * Revision 2.3  2008-08-02 09:08:15+05:30  Cprogrammer
  * use new function error_stack
  *
@@ -25,7 +28,7 @@
 #include "indimail.h"
 
 #ifndef	lint
-static char     sccsid[] = "$Id: mysql_perror.c,v 2.3 2008-08-02 09:08:15+05:30 Cprogrammer Stab mbhangui $";
+static char     sccsid[] = "$Id: mysql_perror.c,v 2.4 2009-11-08 00:56:45+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 #include <stdio.h>
@@ -34,37 +37,32 @@ static char     sccsid[] = "$Id: mysql_perror.c,v 2.3 2008-08-02 09:08:15+05:30 
 #include <stdarg.h>
 char           *
 mysql_perror(char *fmt, ...)
+#else
+#include <varargs.h>
+char           *
+mysql_perror(va_alist)
+	va_dcl
+#endif
 {
 	va_list         ap;
 	char           *ptr;
 	char            temp[2048];
+#ifndef HAVE_STDARG_H
+	char           *fmt;
+#endif
 
+#ifdef HAVE_STDARG_H
 	va_start(ap, fmt);
+#else
+	va_start(ap);
+	fmt = va_arg(ap, char *);
+#endif
 	if(vsnprintf(temp, 2048, fmt, ap) == -1)
 		temp[2047] = 0;
 	va_end(ap);
 	error_stack(stderr, "%s: %s\n", temp, ptr = (char *) mysql_error(&mysql[1]));
 	return(ptr);
 }
-#else
-#include <varargs.h>
-char           *
-mysql_perror(va_alist)
-	va_dcl
-{
-	va_list         args;
-	char           *fmt, *ptr;
-	char            temp[2048];
-
-	va_start(args);
-	fmt = va_arg(args, char *);
-	if(vsnprintf(temp, 2048, fmt, args) == -1)
-		temp[2047] = 0;
-	va_end(args);
-	error_stack(stderr, "%s: %s\n", temp, ptr = (char *) mysql_error(&mysql[1]));
-	return(ptr);
-}
-#endif
 
 void
 getversion_mysql_perror_c()
