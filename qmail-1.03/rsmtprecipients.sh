ee5227f7649e96c515001e5479f10881d643f14c
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2009-12-17 16:04:37 +0530

    fix for null host obtained from smtp logs

--- qmail-1.03/rsmtprecipients.sh
+++ qmail-1.03/rsmtprecipients.sh
@@ -1,4 +1,7 @@
 # $Log: rsmtprecipients.sh,v $
+# Revision 1.6  2009-12-17 16:03:06+05:30  Cprogrammer
+# fix for null host obtained from smtp logs
+#
 # Revision 1.5  2009-11-11 08:34:53+05:30  Cprogrammer
 # assign X-Bogosity to spamheader_name by default
 #
