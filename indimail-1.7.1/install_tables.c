8b53f2ba869960c14ec04b806ef84810df27fffe
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2010-03-07 13:54:14 +0530

    BUG - host.master path was not set correctly

--- indimail-1.7.1/install_tables.c
+++ indimail-1.7.1/install_tables.c
@@ -1,5 +1,8 @@
 /*
  * $Log: install_tables.c,v $
+ * Revision 2.6  2010-03-07 12:31:49+05:30  Cprogrammer
+ * BUG - host.master path was not set correctly
+ *
  * Revision 2.5  2010-02-19 11:31:53+05:30  Cprogrammer
  * skip creating tables for distributed setup on a non-clustered setup
  *
@@ -19,10 +22,11 @@
 #include "indimail.h"
 #ifdef CLUSTERED_SITE
 #include <unistd.h>
+#include <stdlib.h>
 #endif
 
 #ifndef	lint
-static char     sccsid[] = "$Id: install_tables.c,v 2.5 2010-02-19 11:31:53+05:30 Cprogrammer Stab mbhangui $";
+static char     sccsid[] = "$Id: install_tables.c,v 2.6 2010-03-07 12:31:49+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 int
@@ -34,14 +38,12 @@ main(int argc, char **argv)
 #ifdef CLUSTERED_SITE
 	static char     host_path[MAX_BUFF];
 	char           *qmaildir, *controldir;
+	int             hostmaster_present = 0;
 #endif
 	int             i;
 
 	getEnvConfigStr(&default_table, "MYSQL_TABLE", MYSQL_DEFAULT_TABLE);
 	getEnvConfigStr(&inactive_table, "MYSQL_INACTIVE_TABLE", MYSQL_INACTIVE_TABLE);
-	disable_mysql_escape(1);
-	if (snprintf(host_path, MAX_BUFF, "%s/%s/host.master", qmaildir, controldir) == -1)
-		host_path[MAX_BUFF - 1] = 0;
 	for (i = 0; i < 2; i++)
 	{
 		if (create_table(ON_LOCAL, i == 0 ? default_table : inactive_table,
@@ -51,20 +53,6 @@ main(int argc, char **argv)
 			return (1);
 		} else
 			printf("created table %s on local\n", i == 0 ? default_table : inactive_table);
-	}
-	for (i = 0; IndiMailTable[i].table_name; i++)
-	{
-		if (IndiMailTable[i].which == ON_MASTER && access(host_path, F_OK))
-		{
-			printf("skipped table %s on %s\n", IndiMailTable[i].table_name, IndiMailTable[i].which == ON_LOCAL ? "local" : "master");
-			continue;
-		}
-		if (create_table(IndiMailTable[i].which, IndiMailTable[i].table_name, IndiMailTable[i].template))
-		{
-			fprintf(stderr, "failed to create table %s\n", IndiMailTable[i].table_name);
-			return (1);
-		} else
-			printf("created table %s on %s\n", IndiMailTable[i].table_name, IndiMailTable[i].which == ON_LOCAL ? "local" : "master");
 	}
 #if defined(POP_AUTH_OPEN_RELAY)
 	getEnvConfigStr(&relay_table, "RELAY_TABLE", RELAY_DEFAULT_TABLE);
@@ -76,8 +64,27 @@ main(int argc, char **argv)
 #ifdef CLUSTERED_SITE
 	getEnvConfigStr(&qmaildir, "QMAILDIR", QMAILDIR);
 	getEnvConfigStr(&controldir, "CONTROLDIR", "control");
+	disable_mysql_escape(1);
+	if (snprintf(host_path, MAX_BUFF, "%s/%s/host.master", qmaildir, controldir) == -1)
+		host_path[MAX_BUFF - 1] = 0;
+	if (!access(host_path, F_OK) || getenv("MASTER_HOST"))
+		hostmaster_present = open_master() ? 0 : 1;
+	for (i = 0; IndiMailTable[i].table_name; i++)
+	{
+		if (IndiMailTable[i].which == ON_MASTER && !hostmaster_present)
+		{
+			printf("skipped table %s on %s\n", IndiMailTable[i].table_name, IndiMailTable[i].which == ON_LOCAL ? "local" : "master");
+			continue;
+		}
+		if (create_table(IndiMailTable[i].which, IndiMailTable[i].table_name, IndiMailTable[i].template))
+		{
+			fprintf(stderr, "failed to create table %s\n", IndiMailTable[i].table_name);
+			return (1);
+		} else
+			printf("created table %s on %s\n", IndiMailTable[i].table_name, IndiMailTable[i].which == ON_LOCAL ? "local" : "master");
+	}
 	getEnvConfigStr(&cntrl_table, "CNTRL_TABLE", CNTRL_DEFAULT_TABLE);
-	if (access(host_path, F_OK))
+	if (!hostmaster_present)
 		return (0);
 	if (create_table(ON_MASTER, cntrl_table, CNTRL_TABLE_LAYOUT))
 		return (1);
