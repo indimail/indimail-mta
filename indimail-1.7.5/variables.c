b50d8271158034a20ffb6ca4dc2df3a403acee1f
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2010-05-01 15:14:25 +0530

    removed editor from rfc_ids

--- indimail-1.7.5/variables.c
+++ indimail-1.7.5/variables.c
@@ -197,7 +197,7 @@
 #include "indimail.h"
 
 #ifndef	lint
-static char     sccsid[] = "$Id: variables.c,v 2.50 2010-05-01 15:12:24+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsid[] = "$Id: variables.c,v 2.50 2010-05-01 15:12:24+05:30 Cprogrammer Stab mbhangui $";
 #endif
 
 vdir_type       vdir;
