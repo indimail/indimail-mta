655ca73488f6e9d4ac058b199eece1614f0c8d15
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2010-05-18 18:52:01 +0530

    fixed chown command

--- qmail-1.03/update_tmprsadh.sh
+++ qmail-1.03/update_tmprsadh.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
-
+#
 # Update temporary RSA and DH keys
 # Frederik Vermeulen 2004-04-19 GPL
+#
+# $Log: update_tmprsadh.sh,v $
+# Revision 1.1  2010-05-18 17:42:03+05:30  Cprogrammer
+# Initial revision
+#
+#
 
 umask 0077 || exit 0
-MYUID=`/usr/xpg4/bin/id -u indimail`
-MYGID=`/usr/xpg4/bin/id -g indimail`
 
 export PATH="$PATH:/usr/local/bin/ssl:/usr/sbin"
 
 openssl genrsa -out QMAIL/control/rsa512.new 512 &&
 chmod 600 QMAIL/control/rsa512.new &&
-chown $MYUID.$MYGID QMAIL/control/rsa512.new &&
+chown indimail:indimail QMAIL/control/rsa512.new &&
 mv -f QMAIL/control/rsa512.new QMAIL/control/rsa512.pem
 echo
 
 openssl dhparam -2 -out QMAIL/control/dh512.new 512 &&
 chmod 600 QMAIL/control/dh512.new &&
-chown $MYUID.$MYGID QMAIL/control/dh512.new &&
+chown indimail:indimail QMAIL/control/dh512.new &&
 mv -f QMAIL/control/dh512.new QMAIL/control/dh512.pem
 echo
 
 openssl dhparam -2 -out QMAIL/control/dh1024.new 1024 &&
 chmod 600 QMAIL/control/dh1024.new &&
-chown $MYUID.$MYGID QMAIL/control/dh1024.new &&
+chown indimail:indimail QMAIL/control/dh1024.new &&
 mv -f QMAIL/control/dh1024.new QMAIL/control/dh1024.pem
