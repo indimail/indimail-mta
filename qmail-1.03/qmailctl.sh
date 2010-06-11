9404e374f3d0874136e9153e0f949c2d958c486a
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2010-06-11 19:52:54 +0530

    qmailctl can now be installed as a SYSV style startup script

--- qmail-1.03/qmailctl.sh
+++ qmail-1.03/qmailctl.sh
@@ -1,6 +1,9 @@
 # chkconfig: 345 50 80
 # description: Starts qmail system and associated services
 # $Log: qmailctl.sh,v $
+# Revision 1.22  2010-06-11 19:37:54+05:30  Cprogrammer
+# added options for qmailctl to be installed as SYSV style startup script
+#
 # Revision 1.21  2010-05-28 11:52:48+05:30  Cprogrammer
 # fix for Mac OS X
 #
@@ -71,6 +74,9 @@ SERVICE=/service
 #
 #
 SYSTEM=`uname -s | tr "[:lower:]" "[:upper:]"`
+
+# Check that we're a privileged user
+[ `id -u` = 0 ] || exit 4
 case "$SYSTEM" in
 	DARWIN*)
 	. /etc/rc.common
@@ -82,7 +88,9 @@ case "$SYSTEM" in
 	;;
 	LINUX)
 	# Source function library.
-	if [ -f /etc/init.d/functions ] ; then
+	if [ -f /etc/rc.d/init.d/functions ] ; then
+		. /etc/rc.d/init.d/functions
+	elif [ -f /etc/init.d/functions ] ; then
 		. /etc/init.d/functions
 	fi
 	# Get config.
@@ -98,61 +106,184 @@ esac
 
 PATH=$PATH:QMAIL/bin:QMAIL/sbin:/usr/local/bin:/usr/bin:/bin
 export PATH
+myhelp()
+{
+    cat <<HELP
+  start -- starts mail service (smtp connection accepted, mail can go out)
+   stop -- stops mail service (smtp connections refused, nothing goes out)
+restart -- stops and restarts smtp, sends qmail-send a TERM & restarts it
+   kill -- Send kill signal to tcpserver, supvervise, qmail-send processes.
+   shut -- Shutdown entire indimail service.
+  flush -- Schedules queued messages for immediate delivery
+ reload -- sends qmail-send HUP, rereading locals and virtualdomains
+ status -- status of svscan process
+   stat -- displays status of mail service
+  queue -- shows status of queue
+  pause -- temporarily stops mail service (connections accepted, nothing leaves)
+   cont -- continues paused mail service
+    cdb -- rebuild the tcpserver cdb file for smtp, qmtp, qmqp, imap, pop3 and poppass
+HELP
+}
+
+stop()
+{
+	ret=0
+	for i in `echo $SERVICE/qmail-smtpd.* $SERVICE/qmail-qmqpd.* $SERVICE/qmail-qmtpd.* $SERVICE/qmail-send.*`
+	do
+		echo -n $"Stopping $i: "
+		QMAIL/bin/svc -d $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	[ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/indimail
+	[ $ret -eq 0 ] && exit 0 || exit 1
+	return $ret
+}
+
+start()
+{
+	ret=0
+	QMAIL/bin/svstat $SERVICE/.svscan/log > /dev/null
+	if [ $? -ne 0 ] ; then
+		echo -n $"Starting svscan: "
+		QMAIL/sbin/initsvc -on > /dev/null
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	else
+		for i in `echo $SERVICE/qmail-smtpd.* $SERVICE/qmail-qmqpd.* $SERVICE/qmail-qmtpd.* $SERVICE/qmail-send.*`
+		do
+			if [ ! -f $i/down ] ; then
+				echo -n $"Starting $i: "
+				QMAIL/bin/svc -u $i
+				RETVAL=$?
+				echo
+				let ret+=$RETVAL
+			fi
+		done
+		[ $RETVAL -eq 0 ] && touch /var/lock/subsys/indimail
+		[ $ret -eq 0 ] && exit 0 || exit 1
+	fi
+	return $ret
+}
 
 RETVAL=0
 case "$1" in
   stop)
-    QMAIL/bin/svc -d /service/qmail-smtpd.*
-    QMAIL/bin/svc -d /service/qmail-send.*
-    ;;
+	stop
+	;;
   start)
-    QMAIL/bin/svc -u /service/qmail-smtpd.*
-    QMAIL/bin/svc -u /service/qmail-send.*
-    ;;
-  restart)
-    QMAIL/bin/svc -d /service/qmail-smtpd.*
-    QMAIL/bin/svc -t /service/qmail-send.*
-    QMAIL/bin/svc -u /service/qmail-smtpd.*
-    ;;
-  kill)
-    kill `ps -ef|egrep "tcpserver|supervise|qmail-send" | grep -v grep | awk '{print $2}'`
+	start
+	;;
+  restart|force-reload)
+	stop
+	start
 	RETVAL=$?
-    ;;
+	;;
+  condrestart|try-restart)
+	ret=0
+	for i in `echo $SERVICE/qmail-smtpd.* $SERVICE/qmail-qmqpd.* $SERVICE/qmail-qmtpd.*`
+	do
+		echo -n $"Stopping $i: "
+		QMAIL/bin/svc -d $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	for i in `echo $SERVICE/qmail-send.*`
+	do
+		echo -n $"Terminating $i: "
+		QMAIL/bin/svc -t $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	for i in `echo $SERVICE/qmail-smtpd.* $SERVICE/qmail-qmqpd.* $SERVICE/qmail-qmtpd.*`
+	do
+		if [ ! -f $i/down ] ; then
+		echo -n $"Starting $i: "
+			QMAIL/bin/svc -u $i
+			RETVAL=$?
+			echo
+			let ret+=$RETVAL
+		fi
+	done
+	[ $RETVAL -eq 0 ] && touch /var/lock/subsys/indimail
+	[ $ret -eq 0 ] && exit 0 || exit 1
+	;;
+  shut)
+	echo -n $"shutdown svscan: "
+	QMAIL/sbin/initsvc -off > /dev/null
+	echo
+	;;
+  kill)
+	kill `ps -ef|egrep "tcpserver|supervise|qmail-send" | grep -v grep | awk '{print $2}'`
+	RETVAL=$?
+	;;
   stat)
-    QMAIL/bin/svstat $SERVICE/* $SERVICE/*/log
-    ;;
+	QMAIL/bin/svstat $SERVICE/* $SERVICE/*/log
+	;;
   flush)
-    echo "Flushing timeout table and sending ALRM signal to qmail-send."
-    QMAIL/bin/qmail-tcpok
-    QMAIL/bin/svc -a $SERVICE/qmail-send*
-    ;;
+	echo "Flushing timeout table and sending ALRM signal to qmail-send."
+	QMAIL/bin/qmail-tcpok
+	for i in `echo $SERVICE/qmail-send.*`
+	do
+		echo -n $"Flushing $i: "
+		QMAIL/bin/svc -a $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	;;
+  status)
+	if [ -x /sbin/initctl ] ; then
+		/sbin/initctl status svscan
+	else
+		ps -ef|grep svscanboot|grep -v grep
+	fi
+	;;
   queue)
-    QMAIL/bin/qmail-qstat
-    QMAIL/bin/qmail-qread
-    ;;
+	QMAIL/bin/qmail-qstat
+	QMAIL/bin/qmail-qread
+	;;
   reload)
-    echo "Sending HUP signal to qmail-send."
-    QMAIL/bin/svc -h $SERVICE/qmail-send*
-    ;;
+	for i in `echo $SERVICE/qmail-send.*`
+	do
+		echo -n $"sending HUP signal to $i: "
+		QMAIL/bin/svc -h $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	;;
   pause)
-    echo "Pausing qmail-send"
-    QMAIL/bin/svc -p $SERVICE/qmail-send*
-    echo "Pausing qmail-smtpd"
-    QMAIL/bin/svc -p $SERVICE/qmail-smtpd*
-    ;;
+	for i in `echo $SERVICE/qmail-send.* $SERVICE/qmail-smtpd.*`
+	do
+		echo -n $"pausing $i: "
+		QMAIL/bin/svc -p $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	;;
   cont)
-    echo "Continuing qmail-send"
-    QMAIL/bin/svc -c $SERVICE/qmail-send*
-    echo "Continuing qmail-smtpd"
-    QMAIL/bin/svc -c $SERVICE/qmail-smtpd*
-    ;;
+	for i in `echo $SERVICE/qmail-send.* $SERVICE/qmail-smtpd.*`
+	do
+		echo -n $"pausing $i: "
+		QMAIL/bin/svc -c $i
+		RETVAL=$?
+		echo
+		let ret+=$RETVAL
+	done
+	;;
   cdb)
 	case "$SYSTEM" in
 		DARWIN*)
 		INDIMAILDIR=`dscl . -read /Users/indimail|grep NFSHomeDi|awk '{print $2}'`
 		;;
 		*)
-    	INDIMAILDIR=`grep -w "^indimail" /etc/passwd | cut -d: -f6|head -1`
+		INDIMAILDIR=`grep -w "^indimail" /etc/passwd | cut -d: -f6|head -1`
 		;;
 	esac
 	for i in smtp qmtp qmqp imap pop3 poppass
@@ -172,36 +303,25 @@ case "$1" in
 		done
 		for j in `/bin/ls $INDIMAILDIR/etc/tcp*.$i 2>/dev/null`
 		do
-    		QMAIL/bin/tcprules $j.cdb $j.tmp < $j && /bin/chmod 664 $j.cdb \
+			QMAIL/bin/tcprules $j.cdb $j.tmp < $j && /bin/chmod 664 $j.cdb \
 				&& chown indimail:indimail $j.cdb
 			if [ $? -eq 0 ] ; then
-    			echo "Rebuilt $j.cdb"
+				echo "Rebuilt $j.cdb"
 			else
-    			echo "Rebuild $j.cdb: failed!!"
+				echo "Rebuild $j.cdb: failed!!"
 				RETVAL=1
 			fi
 		done
 	done
-    ;;
+	;;
   help)
-    cat <<HELP
-  start -- starts mail service (smtp connection accepted, mail can go out)
-   stop -- stops mail service (smtp connections refused, nothing goes out)
-restart -- stops and restarts smtp, sends qmail-send a TERM & restarts it
-   kill -- Send kill signal to tcpserver, supvervise, qmail-send processes.
-  flush -- Schedules queued messages for immediate delivery
- reload -- sends qmail-send HUP, rereading locals and virtualdomains
-   stat -- displays status of mail service
-  queue -- shows status of queue
-  pause -- temporarily stops mail service (connections accepted, nothing leaves)
-   cont -- continues paused mail service
-    cdb -- rebuild the tcpserver cdb file for smtp, qmtp, qmqp, imap, pop3 and poppass
-HELP
-    ;;
+	myhelp
+	;;
   *)
-    echo "Usage: `basename $0` {start|stop|restart|kill|flush|reload|stat|queue|pause|cont|cdb|help}"
-    exit 1
-    ;;
+    echo "Usage: `basename $0` {start|stop|condrestart|restart|shut|kill|flush|reload|stat|queue|pause|cont|cdb|help}"
+	myhelp
+	exit 1
+	;;
 esac
 
 exit $RETVAL
