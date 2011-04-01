793afc25f1dee773f174905a2cb12f691fed22cb
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-04-01 14:28:13 +0530

    removed courier-imap 3.0.8

--- courier-imap-3.0.8/authlib/libauth.sh
+++
@@ -1,8 +0,0 @@
-# $Id: libauth.sh,v 1.2 2000/10/05 04:15:41 mrsam Exp $
-
-SHELL="$1"
-srcdir="$2"
-
-tr ' ' '\012' | $SHELL ${srcdir}/libauth1.sh | $SHELL ${srcdir}/libauth2.sh \
-	| $SHELL ${srcdir}/libauth1.sh
-
--- courier-imap-3.0.8/rfc1035/rfc1035_res.c
+++
@@ -1,9 +0,0 @@
-/*
-** Copyright 1998 - 1999 Double Precision, Inc.
-** See COPYING for distribution information.
-*/
-
-#include	"rfc1035_res.h"
-
-static const char rcsid[]="$Id: rfc1035_res.c,v 1.2 1999/12/06 13:24:23 mrsam Exp $";
-struct rfc1035_res    rfc1035_default_resolver;
