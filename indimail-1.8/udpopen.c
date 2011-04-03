a83e7fb219a8b006b3fa439461d0e98fa76b5d7f
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-04-03 09:39:29 +0530

    added ipv6 code

--- indimail-1.8/udpopen.c
+++ indimail-1.8/udpopen.c
@@ -1,12 +1,15 @@
 /*
  * $Log: udpopen.c,v $
+ * Revision 2.2  2011-04-03 09:38:29+05:30  Cprogrammer
+ * ported for ipv6
+ *
  * Revision 2.1  2002-07-31 14:54:50+05:30  Cprogrammer
  * function to open a udp connection
  *
  */
 
 #ifndef	lint
-static char     sccsid[] = "$Id: udpopen.c,v 2.1 2002-07-31 14:54:50+05:30 Cprogrammer Stab mbhangui $";
+static char     sccsid[] = "$Id: udpopen.c,v 2.2 2011-04-03 09:38:29+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 #include "indimail.h"
@@ -20,28 +23,71 @@ static char     sccsid[] = "$Id: udpopen.c,v 2.1 2002-07-31 14:54:50+05:30 Cprog
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define FMT_ULONG 40
+
 int
 udpopen(rhost, servicename)
 	char           *rhost, *servicename;
 {
 	int             fd, retval;
-	struct hostent *hp;	/*- pointer to host info for nameserver host */
 	struct servent *sp;	/*- pointer to service information */
-	struct sockaddr_in servaddr_in;
-#ifndef LONGLONG /*- 64 Bit OS DEC OSF1, Solaris 2.8 */
-	unsigned int    inaddr;
+#ifdef ENABLE_IPV6
+	struct addrinfo hints, *res, *res0;
+	char            serv[FMT_ULONG];
+#else
+	struct hostent *hp;	/*- pointer to host info for nameserver host */
+	struct sockaddr_in tcp_srv_addr;
+#ifdef HAVE_IN_ADDR_T
+	in_addr_t       inaddr;
 #else
 	unsigned long   inaddr;
 #endif
+#endif
 
+#ifdef ENABLE_IPV6
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if (!servicename)
+	{
+		errno = EINVAL;
+		return (-1);
+	} 
+	if (isnum(servicename))
+		scopy(serv, servicename, FMT_ULONG);
+	else
+	{
+		if ((sp = getservbyname(servicename, "tcp")) == NULL)
+		{
+			errno = EINVAL;
+			return (-1);
+		}
+		snprintf(serv, FMT_ULONG, "%d", sp->s_port);
+	}
+	if ((retval = getaddrinfo(rhost, serv, &hints, &res0)))
+	{
+		fprintf(stderr, "tcpopen: getaddrinfo: %s\n", gai_strerror(retval));
+		return (-1);
+	}
+	for (fd = -1, res = res0; res && fd == -1; res = res->ai_next)
+	{
+		if ((fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
+			continue; /*- Try the next address record in the list. */
+		if (!(retval = connect(fd, res->ai_addr, res->ai_addrlen)))
+			break;
+		close(fd);
+		fd = -1;
+	} /*- for (res = res0; res; res = res->ai_next) */
+	freeaddrinfo(res0);
+#else
 	/*- clear out address structures */
-	memset((char *) &servaddr_in, 0, sizeof(struct sockaddr_in));
-	servaddr_in.sin_family = AF_INET;
-	if(isnum(servicename))
-		servaddr_in.sin_port = htons(atoi(servicename));
+	memset((char *) &tcp_srv_addr, 0, sizeof(struct sockaddr_in));
+	tcp_srv_addr.sin_family = AF_INET;
+	if (isnum(servicename))
+		tcp_srv_addr.sin_port = htons(atoi(servicename));
 	else
 	if ((sp = getservbyname(servicename, "udp")))
-		servaddr_in.sin_port = sp->s_port;
+		tcp_srv_addr.sin_port = sp->s_port;
 	else
 	{
 		fprintf(stderr, "%s/udp: No such service\n", servicename);
@@ -49,7 +95,7 @@ udpopen(rhost, servicename)
 		return (-1);
 	}
 	if ((inaddr = inet_addr(rhost)) != INADDR_NONE) /*- It's a dotted decimal */
-		(void) memcpy((char *) &servaddr_in.sin_addr, (char *) &inaddr, sizeof(inaddr));
+		(void) memcpy((char *) &tcp_srv_addr.sin_addr, (char *) &inaddr, sizeof(inaddr));
 	else
 	if ((hp = gethostbyname(rhost)) == NULL)
 	{
@@ -57,15 +103,16 @@ udpopen(rhost, servicename)
 		errno = EINVAL;
 		return (-1);
 	} else
-		(void) memcpy((char *) &servaddr_in.sin_addr, hp->h_addr, hp->h_length);
+		(void) memcpy((char *) &tcp_srv_addr.sin_addr, hp->h_addr, hp->h_length);
 	/*- Create the socket. */
 	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		return (-1);
-	if((retval = connect(fd, (struct sockaddr *) &servaddr_in, sizeof(servaddr_in))) == -1)
+	if ((retval = connect(fd, (struct sockaddr *) &tcp_srv_addr, sizeof(tcp_srv_addr))) == -1)
 	{
 		close(fd);
 		return (-1);
 	}
+#endif
 	return(fd);
 }
 
