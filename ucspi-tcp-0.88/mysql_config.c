8b8b7b3242e9d18e1760113f6883894795238d6c
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-04-29 12:50:17 +0530

    prevent recursive call to mysql_config

--- ucspi-tcp-0.88/mysql_config.c
+++ ucspi-tcp-0.88/mysql_config.c
@@ -1,5 +1,8 @@
 /*
  * $Log: mysql_config.c,v $
+ * Revision 1.3  2011-04-29 12:49:57+05:30  Cprogrammer
+ * prevent recursive call to mysql_config
+ *
  * Revision 1.2  2009-07-11 08:55:52+05:30  Cprogrammer
  * use /usr/local/mysql as the first mysql prefix
  *
