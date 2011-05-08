3c33fbc5a3a4b78ec695f65ea3fa61fcc50ddc6b
Manvendra Bhangui <manvendra@indimail.org>
2011-05-08 16:23:48 +0530

    renamed iwebadmin-1.0 to iwebadmin-1.1

--- iwebadmin-1.0/auth.h
+++
@@ -1,7 +0,0 @@
-/*
- * $Id: auth.h,v 1.1 2010-04-26 12:07:27+05:30 Cprogrammer Exp mbhangui $
- */
-
-void            auth_system(const char *ip_addr, struct passwd *pw);
-void            auth_user_domain(const char *ip_addr, struct passwd *pw);
-void            set_admin_type();
--- iwebadmin-1.0/limits.h
+++
@@ -1,6 +0,0 @@
-/*
- * $Id: limits.h,v 1.1 2010-04-26 12:07:53+05:30 Cprogrammer Exp mbhangui $
- */
-
-#define LIMIT_TOKENS " :\t\r\n"
-void            load_limits();
