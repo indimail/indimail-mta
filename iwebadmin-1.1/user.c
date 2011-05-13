fe47f1799ddfa9b34d124dfcfb5fca2763f69c68
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-05-13 10:37:44 +0530

    iwebadmin refresh to 1.1

--- iwebadmin-1.1/user.c
+++ iwebadmin-1.1/user.c
@@ -1,5 +1,5 @@
 /*
- * $Id: user.c,v 1.2 2010-10-09 08:31:15+05:30 Cprogrammer Stab mbhangui $
+ * $Id: user.c,v 1.3 2011-05-13 10:36:39+05:30 Cprogrammer Exp mbhangui $
  * Copyright (C) 1999-2004 Inter7 Internet Technologies, Inc. 
  *
  * This program is free software; you can redistribute it and/or modify
