1367b1ce9ece9e9eabe022bc52c8c914cad82388
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-10-29 22:03:48 +0530

    comp_digest_md5() prototype

---
+++ qmail-1.03/auth_digest_md5.h
@@ -0,0 +1,12 @@
+/*
+ * $Log: auth_digest_md5.h,v $
+ * Revision 1.1  2011-10-29 20:41:22+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef _AUTH_DIGEST_MD5_H
+#define _AUTH_DIGEST_MD5_H 1
+
+char *comp_digest_md5(char *, int, char *, int, char *, int, char *, char *, int, char *, int, char *, char *, char *);
+
+#endif
