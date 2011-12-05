2d08d4ae099e474d32815e6394b7e4bca08ba891
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2011-12-05 19:55:59 +0530

    marked Stable in rcs

--- qmail-1.03/auth_digest_md5.h
+++ qmail-1.03/auth_digest_md5.h
@@ -11,7 +11,7 @@
 #define _AUTH_DIGEST_MD5_H 1
 
 #ifndef	lint
-static char     sccsidauthdigestmd5h[] = "$Id: auth_digest_md5.h,v 1.2 2011-12-05 15:07:16+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsidauthdigestmd5h[] = "$Id: auth_digest_md5.h,v 1.2 2011-12-05 15:07:16+05:30 Cprogrammer Stab mbhangui $";
 #endif
 
 char *digest_md5(char *, int, char *, int, char *, int, char *, char *, int, char *, int, char *, char *, char *);
