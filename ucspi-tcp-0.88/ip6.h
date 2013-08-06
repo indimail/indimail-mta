9c00fed98d6bf767803aa3d407239025b0b5f6be
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2013-08-06 11:06:32 +0530

    added support for compact IPv6 addresses and CIDR notation

--- ucspi-tcp-0.88/ip6.h
+++ ucspi-tcp-0.88/ip6.h
@@ -1,5 +1,11 @@
 /*
  * $Log: ip6.h,v $
+ * Revision 1.3  2013-08-06 07:56:26+05:30  Cprogrammer
+ * added V4MAPPREFIX
+ *
+ * Revision 1.2  2013-08-06 00:49:40+05:30  Cprogrammer
+ * added ip6_expandaddr()
+ *
  * Revision 1.1  2005-06-10 12:12:12+05:30  Cprogrammer
  * Initial revision
  *
@@ -8,11 +14,15 @@
 #define IP6_H
 
 #include "byte.h"
+#include "stralloc.h"
 
 unsigned int    ip6_scan(char *, char *);
 unsigned int    ip6_fmt(char *, char *);
 unsigned int    scan_ip6_flat(char *src, char *);
 unsigned int    fmt_ip6_flat(char *dest, char *);
+int             ip6tobitstring(char *, stralloc *, unsigned int);
+unsigned int    ip6_expandaddr(char *src, stralloc *destination);
+unsigned int    ip6_compactaddr(char *s, char ip[16]);
 
 /*
  * ip6 address syntax: (h = hex digit), no leading '0' required
@@ -29,5 +39,7 @@ extern unsigned char V6loopback[16]; /*- ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}; */
 extern unsigned char V6any[16];	/*- ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; */
 
 #define ip6_isv4mapped(ip) (byte_equal(ip,12,V4mappedprefix))
+
+#define V4MAPPREFIX "::ffff:"
 
 #endif
