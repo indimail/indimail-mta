7d4a4f4c6c509d15f469c463d1f3090ee2744bd5
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2013-08-12 15:01:12 +0530

    figure out mktemp path

--- qmail-1.03/surblqueue.sh
+++ qmail-1.03/surblqueue.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 #
 # $Log: surblqueue.sh,v $
+# Revision 1.2  2013-08-12 15:00:54+05:30  Cprogrammer
+# figure out mktemp path
+#
 # Revision 1.1  2011-07-15 11:48:51+05:30  Cprogrammer
 # Initial revision
 #
 #
-# $Id: surblqueue.sh,v 1.1 2011-07-15 11:48:51+05:30 Cprogrammer Stab mbhangui $
+# $Id: surblqueue.sh,v 1.2 2013-08-12 15:00:54+05:30 Cprogrammer Exp mbhangui $
 #
 # I should be called by qmail-smtpd or anything that calls qmail-queue
 #
-out=`/bin/mktemp -t surblXXXXXXXXXX`
+if [ -f /bin/mktemp ] ; then
+	MKTEMP=/bin/mktemp
+elif [ -f /usr/bin/mktemp ] ; then
+	MKTEMP=/usr/bin/mktemp
+else
+	MKTEMP=mktemp
+fi
+out=`$MKTEMP -t surblXXXXXXXXXX`
 if [ $? -ne 0 ] ; then
 	exit 111
 fi
