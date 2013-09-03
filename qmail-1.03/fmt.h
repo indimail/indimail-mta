8e74e7c796ca8890b3d0be4f6c87ab149d8e7d85
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2013-09-03 23:00:10 +0530

    added fmt_double()

--- qmail-1.03/fmt.h
+++ qmail-1.03/fmt.h
@@ -1,5 +1,8 @@
 /*
  * $Log: fmt.h,v $
+ * Revision 1.4  2013-08-29 18:26:57+05:30  Cprogrammer
+ * added fmt_double()
+ *
  * Revision 1.3  2004-10-10 10:11:25+05:30  Cprogrammer
  * added function prototypes
  *
@@ -11,6 +14,7 @@
 #define FMT_H
 
 #define FMT_ULONG 40			/*- enough space to hold 2^128 - 1 in decimal, plus \0 */
+#define FMT_DOUBLE 63			/*- enough to hold double with 20 precision in fraction part */
 #define FMT_LEN ((char *) 0)	/*- convenient abbreviation */
 
 unsigned int    fmt_uint(char *, unsigned int);
@@ -23,6 +27,7 @@ unsigned int    fmt_nbbshort(char *, unsigned int, unsigned int, unsigned int, u
 unsigned int    fmt_ulong(char *, unsigned long);
 unsigned int    fmt_xlong(char *, unsigned long);
 unsigned int    fmt_nbblong(char *, unsigned int, unsigned int, unsigned int, unsigned long);
+unsigned int    fmt_double(char *s, double, unsigned long);
 
 unsigned int    fmt_plusminus(char *, int);
 unsigned int    fmt_minus(char *, int);
