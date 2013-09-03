afb367fe2e365528f95aca2cf5f1cf88ffecf7e0
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2013-09-03 22:59:47 +0530

    added cidr utility

--- qmail-1.03/cidr.c
+++ qmail-1.03/cidr.c
@@ -1,4 +1,11 @@
+/*
+ * $Log: cidr.c,v $
+ * Revision 1.1  2013-09-03 17:05:04+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <errno.h>
@@ -10,10 +17,82 @@
 #define MAX_CIDR_MASK    32
 #define MAX_CIDR_LEN     18+1	/*255.255.255.255/32 */
 
-int
+void
 printNotation(char *cidrNotation)
 {
 	printf("%s\n", cidrNotation);
+}
+
+/*
+ *
+ * ipToBin - convert an ipv4 address to binary representation 
+ *           and pads zeros to the beginning of the string if 
+ *           the length is not 32 
+ *           (Important for ranges like 10.10.0.1 - 20.20.20.20 )
+ *
+ * ip   - ipv4 address on host order
+ * pOut - Buffer to store binary.
+ *
+ * RETURNS: OK or ERROR 
+ */
+
+int
+ipToBin(uint32_t ip, char *pOut)
+{
+	char            hex[IP_HEX_LENGTH];
+	int             i;
+	int             result = 0;
+	int             len;
+	char            pTmp[2];
+	int             tmp;
+/*
+ * XXX: Could use bit operations instead but was easier to debug
+ */
+	char            binMap[16][5] = {
+		"0000", "0001", "0010", "0011", "0100",
+		"0101", "0110", "0111", "1000", "1001",
+		"1010", "1011", "1100", "1101", "1110", "1111",
+	};
+	pTmp[1] = 0x0;
+	memset(hex, 0x0, sizeof (hex));
+	len = sprintf(hex, "%x", ip);
+
+	for (i = 0; i < len; i++) {
+	/*
+	 * Ugly but to use strtol , we need the last byte as null 
+	 */
+		pTmp[0] = hex[i];
+		errno = 0;
+		tmp = strtol(pTmp, 0x0, 16);
+	/*
+	 * Should not happen 
+	 */
+		if (errno != 0) {
+			memset(pOut, '0', IP_BINARY_LENGTH - 1);
+#if 0
+			DEBUG("strtol failed for hex 0x%s\n", pTmp);
+#endif
+			return -1;
+		}
+		result += sprintf(pOut + result, "%s", binMap[tmp]);
+	}
+#if 0
+	DEBUG("bits %u printed for ip address for hex len %u\n", result, len);
+#endif
+/*
+ * if length is not 32 , pad the start with zeros
+ */
+	if (result < IP_BINARY_LENGTH - 1) {
+		char            pSwap[IP_BINARY_LENGTH];
+		strncpy(pSwap, pOut, IP_BINARY_LENGTH);
+		memset(pOut, '0', IP_BINARY_LENGTH);
+		strncpy(pOut + IP_BINARY_LENGTH - 1 - result, pSwap, result);
+#if 0
+		DEBUG("corrected length to 32\n");
+#endif
+	} else if (result > IP_BINARY_LENGTH - 1)
+		return -1;
+	return 0;
 }
 
 /*
@@ -45,11 +124,6 @@ rangeToCidr(uint32_t from, uint32_t to, void (callback) (char *cidrNotation))
 		return;
 	if (ipToBin(to, toIp) != 0)
 		return;
-#if 0
-	DEBUG("from %lu to %lu\n", from, to);
-	DEBUG("from %s\n", fromIp);
-	DEBUG("to   %s\n", toIp);
-#endif
 	if (from < to) {
 		/*
 		 * Compare the from and to address ranges to get the first
@@ -58,14 +132,11 @@ rangeToCidr(uint32_t from, uint32_t to, void (callback) (char *cidrNotation))
 		while (fromIp[cidrStart] == toIp[cidrStart])
 			cidrStart++;
 		cidrStart = 32 - cidrStart - 1;
-#if 0
-		DEBUG("cidrStart is %u\n", cidrStart);
-#endif
 		/*
 		 * Starting from the found point of difference make all bits on the 
 		 * right side zero 
 		 */
-		newfrom = from >> cidrStart + 1 << cidrStart + 1;
+		newfrom = from >> (cidrStart + 1) << (cidrStart + 1);
 		/*
 		 * Starting from the end iterate reverse direction to find 
 		 * cidrEnd
@@ -73,22 +144,13 @@ rangeToCidr(uint32_t from, uint32_t to, void (callback) (char *cidrNotation))
 		while (fromIp[cidrEnd] == '0' && toIp[cidrEnd] == '1')
 			cidrEnd--;
 		cidrEnd = MAX_CIDR_MASK - 1 - cidrEnd;
-#if 0
-		DEBUG("cidrEnd is %u\n", cidrEnd);
-#endif
 		if (cidrEnd <= cidrStart) {
-			/*
-			 * Make all the bit-shifted bits equal to 1, for
-			 * iteration # 1.
-			 */
+		/*
+		 * Make all the bit-shifted bits equal to 1, for
+		 * iteration # 1.
+		 */
 			mask = pow(2, cidrStart) - 1;
-#if 0
-			DEBUG("it1 is %lu \n", newfrom | mask);
-#endif
 			rangeToCidr(from, newfrom | mask, callback);
-#if 0
-			DEBUG("it2 is %lu \n", newfrom | 1 << cidrStart);
-#endif
 			rangeToCidr(newfrom | 1 << cidrStart, to, callback);
 		} else {
 			addr.s_addr = htonl(newfrom);
@@ -104,101 +166,36 @@ rangeToCidr(uint32_t from, uint32_t to, void (callback) (char *cidrNotation))
 	}
 }
 
-/*
- *
- * ipToBin - convert an ipv4 address to binary representation 
- *           and pads zeros to the beginning of the string if 
- *           the length is not 32 
- *           (Important for ranges like 10.10.0.1 - 20.20.20.20 )
- *
- * ip   - ipv4 address on host order
- * pOut - Buffer to store binary.
- *
- * RETURNS: OK or ERROR 
- */
-
 int
-ipToBin(uint32_t ip, char *pOut)
+main(int argc, char **argv)
 {
-	char            hex[IP_HEX_LENGTH];
-	int             i;
-	int             result = 0;
-	int             len;
-	char            pTmp[2];
-	int             tmp;
-	/*
-	 * XXX: Could use bit operations instead but was easier to debug
-	 */
-	char            binMap[16][5] = {
-		"0000", "0001", "0010", "0011", "0100",
-		"0101", "0110", "0111", "1000", "1001",
-		"1010", "1011", "1100", "1101", "1110", "1111",
-	};
-	pTmp[1] = 0x0;
-	memset(hex, 0x0, sizeof (hex));
-	len = sprintf(hex, "%x", ip);
+	long            fromIp, toIp;
+	struct in_addr  addr;
 
-	for (i = 0; i < len; i++) {
-		/*
-		 * Ugly but to use strtol , we need the last byte as null 
-		 */
-		pTmp[0] = hex[i];
-		errno = 0;
-		tmp = strtol(pTmp, 0x0, 16);
-		/*
-		 * Should not happen 
-		 */
-		if (errno != 0) {
-			memset(pOut, '0', IP_BINARY_LENGTH - 1);
-#if 0
-			DEBUG("strtol failed for hex 0x%s\n", pTmp);
-#endif
-			return -1;
-		}
-		result += sprintf(pOut + result, "%s", binMap[tmp]);
+	if (argc != 3) {
+		printf("Usage: %s <from> <to>\n", argv[0]);
+		return (0);
 	}
-#if 0
-	DEBUG("bits %u printed for ip address for hex len %u\n", result, len);
-#endif
 	/*
-	 * if length is not 32 , pad the start with zeros
+	 * All operation on host order 
 	 */
-	if (result < IP_BINARY_LENGTH - 1) {
-		char            pSwap[IP_BINARY_LENGTH];
-		strncpy(pSwap, pOut, IP_BINARY_LENGTH);
-		memset(pOut, '0', IP_BINARY_LENGTH);
-		strncpy(pOut + IP_BINARY_LENGTH - 1 - result, pSwap, result);
-#if 0
-		DEBUG("corrected length to 32\n");
-#endif
-	} else
-	if (result > IP_BINARY_LENGTH - 1)
-		return -1;
+	if (inet_aton(argv[1], &addr) == 0)
+		goto error;
+	fromIp = ntohl(addr.s_addr);
+	if (inet_aton(argv[2], &addr) == 0)
+		goto error;
+	toIp = ntohl(addr.s_addr);
+	rangeToCidr(fromIp, toIp, printNotation);
 	return 0;
-}
-int main (int argc,char **argv)
-    {
-    long fromIp, toIp;
-    struct in_addr addr;
-    if(argc !=3 )
-        {
-        printf("Usage: %s <from> <to>\n",argv[0]);
-        return(0);
-        }
-
-    /* All operation on host order */   
-    if (inet_aton(argv[1],&addr) == 0)
-        goto error;
-    fromIp = ntohl(addr.s_addr);
-
-    if (inet_aton(argv[2],&addr) ==0)
-        goto error;
-    toIp = ntohl(addr.s_addr);
-
-    rangeToCidr(fromIp,toIp,printNotation);
-
-    return 0;
 error:
-    printf("Invalid Argument\n");
-    return -EINVAL;
- }
+	printf("Invalid Argument\n");
+	return -EINVAL;
+}
+
+void
+getversion_cidr_c()
+{
+	static char    *x = "$Id: cidr.c,v 1.1 2013-09-03 17:05:04+05:30 Cprogrammer Exp mbhangui $";
+	if (x)
+		x++;
+}
