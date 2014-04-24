ead4f22525961b1e46179f1dd1cc334cb48879e9
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2014-04-24 11:20:54 +0530

    fix for uname on ubuntu

--- qmail-1.03/trynameser.sh
+++ qmail-1.03/trynameser.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-os=`/usr/bin/uname -s`
+if [ -x /usr/bin/uname ] ; then
+	os=`/usr/bin/uname -s`
+elif [ -x /bin/uname ] ; then
+	os=`/bin/uname -s`
+else
+	os="unknown"
+fi
 if [ " $os" = " Darwin" ] ; then
 	if [ -f /usr/include/nameser8_compat.h ] ; then
 		echo "#include <nameser8_compat.h>"
