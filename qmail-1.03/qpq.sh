5487eff77de1dc9214f13da1bac662053d182a79
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2014-06-13 17:52:07 +0530

    replaced qmail-queue-print with 822header

--- qmail-1.03/qpq.sh
+++ qmail-1.03/qpq.sh
@@ -1,26 +1,67 @@
-#!/bin/bash
-# Version 0.1
+#
+# $Log: $
+#
 # This script locates all messages currently in qmail's queue and 
-# calls the qmail-queue-print script, which in part will extract the
-# from, to and date headers from every message and display them to
-# the user <STDOUT>
-# if called wih "-h" argument then the message's header will also be 
-# displayed
-# Written by Alex Kramarov, comments and suggestions please send to 
-# alex@incredimail.com 
-if [ " $QUEUE_BASE" = " " ] ; then
-	queues=`echo QMAIL/queue*`
+# calls the 822header program.
+#
+# $Id: dk-filter.sh,v 1.18 2014-03-12 08:50:48+05:30 Cprogrammer Stab mbhangui $
+#
+
+usage ()
+{
+	echo "Wrong Usage"
+	echo "$0 [options]"
+	echo "-h	Print Headers only"
+	echo "-H	Pass arguments to 822header(1)"
+}
+
+args822="-I From -I To -I Date"
+while getopts hH:q: opt
+do
+	case $opt in
+		q) qbase=$OPTARG;;
+		h) args822="";;
+		H) args822=$OPTARG;;
+		?) usage $0;exit 1;;
+	esac
+done
+
+if [ -z "$qbase" ] ; then
+	if [ -d QMAIL/queue/nqueue ] ; then
+		queues=`echo QMAIL/queue/*`
+	else
+		queues=`echo QMAIL/queue*`
+	fi
 else
-	queues=`echo $QUEUE_BASE/queue*`
+	if [ -d $qbase/nqueue ] ; then
+		queues=`echo $qbase/*`
+	else
+		queues=`echo $qbase/queue*`
+	fi
 fi
+count=1
 for i in $queues
 do
 	queuedir=$i
-	echo "$queuedir"
 	first_char=`echo $queuedir | cut -c1`
 	if [ " $first_char" = " /" -o " $first_char" = " ." ] ; then
-		find $queuedir/mess/ -type f| QMAIL/bin/qmail-queue-print $*
+		for i in `find $queuedir/mess/ -type f`
+		do
+			echo "$count: Message $i"
+			echo
+			/bin/cat $i | QMAIL/bin/822header $args822
+			echo "----------------------------------- END ------------------------------------"
+			count=`expr $count + 1`
+		done
+		#| QMAIL/bin/qmail-queue-print $*
 	else
-		find QMAIL/$queuedir/mess/ -type f| QMAIL/bin/qmail-queue-print $*
+		for i in `find QMAIL/$queuedir/mess/ -type f`
+		do
+			echo "$count: Message $i"
+			echo
+			/bin/cat $i | QMAIL/bin/822header $args822
+			echo "----------------------------------- END ------------------------------------"
+			count=`expr $count + 1`
+		done
 	fi
 done
