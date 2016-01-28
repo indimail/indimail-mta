eca92f2fcae71a76115fbf76080206ac7a721b30
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2016-01-28 23:54:17 +0530

    chdir /var/indimail to open leapsecs.dat

--- qmail-1.03/nowutc.c
+++ qmail-1.03/nowutc.c
@@ -1,15 +1,21 @@
 /*
  * $Log: nowutc.c,v $
+ * Revision 1.2  2016-01-28 23:51:48+05:30  Cprogrammer
+ * chdir /var/indimail for leapsecs.dat
+ *
  * Revision 1.1  2016-01-28 23:42:47+05:30  Cprogrammer
  * Initial revision
  *
  */
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include "leapsecs.h"
 #include "tai.h"
 #include "taia.h"
 #include "caltime.h"
+#include "auto_qmail.h"
 
 struct taia     now;
 struct tai      sec;
@@ -20,9 +26,13 @@ char            x[TAIA_FMTFRAC];
 int
 main()
 {
+	if (chdir(auto_qmail) == -1) {
+		fprintf(stderr, "chdir: %s: %s\n", auto_qmail, strerror(errno));
+		_exit (111);
+	}
 	if (leapsecs_init() == -1) {
 		fprintf(stderr, "utcnow: fatal: unable to init leapsecs\n");
-		_exit(111);
+		_exit (111);
 	}
 
 	taia_now(&now);
@@ -31,7 +41,7 @@ main()
 	taia_tai(&now, &sec);
 	caltime_utc(&ct, &sec, (int *) 0, (int *) 0);
 
-	printf("%d-%02d-%02d %02d:%02d:%02d.%s\n", ct.date.year, ct.date.month, ct.date.day, ct.hour, ct.minute, ct.second, x);
+	printf("%ld-%02d-%02d %02d:%02d:%02d.%s\n", ct.date.year, ct.date.month, ct.date.day, ct.hour, ct.minute, ct.second, x);
 
 	return (0);
 }
@@ -39,7 +49,7 @@ main()
 void
 getversion_nowutc_c()
 {
-	static char    *x = "$Id: nowutc.c,v 1.1 2016-01-28 23:42:47+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: nowutc.c,v 1.2 2016-01-28 23:51:48+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
