e7f68fde5ab22408b0cb71402fe1e566c0c526e3
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2016-05-15 21:49:40 +0530

    use the basename of the shared object as the function to execute

--- ucspi-tcp-0.88/load_shared.c
+++ ucspi-tcp-0.88/load_shared.c
@@ -1,5 +1,8 @@
 /*
  * $Log: load_shared.c,v $
+ * Revision 1.4  2016-05-15 21:49:11+05:30  Cprogrammer
+ * use the basename of the shared object as the function to execute
+ *
  * Revision 1.3  2016-03-31 17:47:59+05:30  Cprogrammer
  * compile load_shared only if LOAD_SHARED_OBJECTS is defined
  *
@@ -21,24 +24,35 @@
 void
 load_shared(char *file, char **argv, char **envp)
 {
-	int             argc;
+	int             argc, split;
 	int             (*func) (int, char **, char **);
 	void           *handle;
-	char           *error;
+	char           *error, *fptr;
 	char          **ptr;
 
 	if (!str_end(file, ".so")) {
-		if (!(handle = dlopen(file, RTLD_NOW|RTLD_LOCAL)))
-			return;
-		dlerror(); /*- man page told me to do this */
-		func = dlsym(handle, "qmail_smtpd");
+		if (!(handle = dlopen(file, RTLD_NOW|RTLD_NOLOAD))) {
+			if (!(handle = dlopen(file, RTLD_NOW|RTLD_LOCAL|RTLD_NODELETE))) {
+				strerr_die(111, FATAL, "dlopen2: ", dlerror(), 0, 0, 0, (struct strerr *) 0);
+				return;
+			}
+		}
+		dlerror(); /*- clear existing error */
+		/*- use the basename of the shared object as the function to execute */
+		split = str_chr(file, '.');
+		if (split)
+			file[split--] = 0;
+		for (fptr = file + split;*fptr && *fptr != '/';fptr--);
+		if (*fptr == '/')
+			fptr++;
+		func = dlsym(handle, fptr);
 		if ((error = dlerror()))
-			strerr_die(0, FATAL, "dlsym: qmail-smtpd.so: ", error, 0, 0, 0, (struct strerr *) 0);
+			strerr_die(111, FATAL, "dlsym: ", fptr, ": ", error, 0, (struct strerr *) 0);
 		for (argc = 0,ptr = argv; *ptr; ptr++)
 			argc++;
 		(*func) (argc, argv, envp); /*- execute the function */
-		if (dlclose(handle))
-			strerr_die(0, FATAL, "dlclose: qmail-smtpd.so: ", error, 0, 0, 0, (struct strerr *) 0);
+		if (dlclose(handle)) /*- this will not unload the object due to RTLD_NODELETE */
+			strerr_die(111, FATAL, "dlclose: ", fptr, ": ", error, 0, (struct strerr *) 0);
 		_exit(0);
 	} else
 		execve(file, argv, envp);
