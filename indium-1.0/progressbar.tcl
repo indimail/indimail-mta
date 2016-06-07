70b31dae9e0bb76e051a9575332710b3b7155fc5
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2016-06-07 22:30:03 +0530

    fixed gpl rpmlint warning

--- indium-1.0/progressbar.tcl
+++ indium-1.0/progressbar.tcl
@@ -8,14 +8,8 @@
 # the Free Software Foundation; either version 2, or (at your option)
 # any later version.
 #
-# Progressbar is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Progressbar; see the file COPYING.  If not, write to
-# the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# Distributed under GPLV2 
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 #
 # Special thanks to Bryan Oakley his mclistbox was a very good example
 # how to build a widget - GREAT SOURCE!
