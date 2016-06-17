62583b8dc6db6a3492bf9af63320ff05b5aa4c41
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2016-06-17 17:19:15 +0530

    added conf-libexec to configure libexec directory for internal binaries

---
+++ qmail-1.03/auto_libexec.h
@@ -0,0 +1,12 @@
+/*
+ * $Log: auto_libexec.h,v $
+ * Revision 1.1  2016-06-17 17:18:41+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef AUTO_LIBEXEC_H
+#define AUTO_LIBEXEC_H
+
+extern char auto_libexec[];
+
+#endif
