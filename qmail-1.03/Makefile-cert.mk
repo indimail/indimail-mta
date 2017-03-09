019c8b77c89562e98732d4beb737e1833f0c0ffe
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-03-09 21:54:28 +0530

    FHS compliance

--- qmail-1.03/Makefile-cert.mk
+++ qmail-1.03/Makefile-cert.mk
@@ -1,11 +1,11 @@
 cert-req: req.pem
-cert cert-req: QMAIL/control/clientcert.pem
+cert cert-req: SYSCONFDIR/control/clientcert.pem
 	@:
 
-QMAIL/control/clientcert.pem: QMAIL/control/servercert.pem
-	cd QMAIL/control;ln -s servercert.pem clientcert.pem
+SYSCONFDIR/control/clientcert.pem: SYSCONFDIR/control/servercert.pem
+	cd SYSCONFDIR/control;ln -s servercert.pem clientcert.pem
 
-QMAIL/control/servercert.pem:
+SYSCONFDIR/control/servercert.pem:
 	PATH=$$PATH:/usr/local/ssl/bin \
 		openssl req -new -x509 -nodes -days 366 -out $@ -keyout $@
 	chmod 640 $@
@@ -13,9 +13,9 @@ QMAIL/control/servercert.pem:
 
 req.pem:
 	PATH=$$PATH:/usr/local/ssl/bin openssl req \
-		-new -nodes -out $@ -keyout QMAIL/control/servercert.pem
-	chmod 640 QMAIL/control/servercert.pem
-	chown indimail:indimail QMAIL/control/servercert.pem
+		-new -nodes -out $@ -keyout SYSCONFDIR/control/servercert.pem
+	chmod 640 SYSCONFDIR/control/servercert.pem
+	chown indimail:indimail SYSCONFDIR/control/servercert.pem
 	@echo
 	@echo "Send req.pem to your CA to obtain signed_req.pem, and do:"
-	@echo "cat signed_req.pem >> QMAIL/control/servercert.pem"
+	@echo "cat signed_req.pem >> SYSCONFDIR/control/servercert.pem"
