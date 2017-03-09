939b46c7deee6bdf9df46e858bcfea87d1d10d45
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-03-09 14:37:50 +0530

    fixed controldir path

--- qmail-1.03/update_tmprsadh.sh
+++ qmail-1.03/update_tmprsadh.sh
@@ -4,6 +4,9 @@
 # Frederik Vermeulen 2004-04-19 GPL
 #
 # $Log: update_tmprsadh.sh,v $
+# Revision 1.3  2017-03-09 14:37:37+05:30  Cprogrammer
+# fixed controldir path
+#
 # Revision 1.2  2016-05-17 23:11:42+05:30  Cprogrammer
 # fix for configurable control directory
 #
@@ -15,13 +18,32 @@
 umask 0077 || exit 0
 
 export PATH="$PATH:/usr/local/bin/ssl:/usr/sbin"
+while test $# -gt 0; do
+    case "$1" in
+    -*=*) optarg=`echo "$1" | sed 's/[-_a-zA-Z0-9]*=//'` ;;
+    *) optarg= ;;
+    esac
+
+    case "$1" in
+    --controldir=*)
+	CONTROLDIR=$optarg
+	;;
+
+    *)
+	echo "invalid option [$1]"
+	read key
+	usage 1
+	;;
+    esac
+    shift
+done
 
 if [ " $CONTROLDIR" = " " ] ; then
 	CONTROLDIR=@controldir@
 fi
 slash=`echo $CONTROLDIR | cut -c1`
 if [ ! " $slash" = " /" ] ; then
-	cd QMAIL
+	cd @sysconfdir@
 fi
 openssl genrsa -out $CONTROLDIR/rsa512.new 512 &&
 chmod 600 $CONTROLDIR/rsa512.new &&
