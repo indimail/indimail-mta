bf1d1880714a8b84200bd4dcf83e7a56ba712933
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-03-20 16:20:33 +0530

    new version of roundcube iwebadmin plugin

--- indimail-2.1/roundcube/usr/share/roundcubemail/plugins/iwebadmin/iwebadmin.php
+++ indimail-2.1/roundcube/usr/share/roundcubemail/plugins/iwebadmin/iwebadmin.php
@@ -7,6 +7,9 @@
  * Version 1.0.1-dev
  * Copyright (c) 2011 David C A Croft.
  *
+ * Version 1.0.2-dev
+ * Copyright (c) 2016 Eliton Claus
+ *
  * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
  * or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
@@ -35,6 +38,8 @@ class iwebadmin extends rcube_plugin
 
     global $RCMAIL;
 
+	$this->add_texts('localization', true);
+	
     if ($this->display_vacation_warning()) {
       $this->add_hook('render_page', array($this, 'render_page'));
     }
@@ -80,7 +85,7 @@ class iwebadmin extends rcube_plugin
     if ($RCMAIL->config->get('iwebadmin_allow_vacation')) {
       // Insert 'vacation' before 'mailbox'
       $new_section = array('vacation' => array('id' => 'vacation',
-					       'section' => 'Vacation Message'));
+					       'section' => rcube::Q($this->gettext('vacationmessage'))));
 
       $this->add_section_before($new_section, 'mailbox', $args);
     }
@@ -88,7 +93,7 @@ class iwebadmin extends rcube_plugin
     if ($RCMAIL->config->get('iwebadmin_allow_password')) {
       // Insert 'password' before 'server'
       $new_section = array('password' => array('id' => 'password',
-					       'section' => 'Change Password'));
+					       'section' => rcube::Q($this->gettext('changepassword'))));
 
       $this->add_section_before($new_section, 'server', $args);
     }
@@ -134,7 +139,7 @@ class iwebadmin extends rcube_plugin
       $field_id = 'vacation_enabled';
       $input_vacation_enabled = new html_checkbox(array('name' => '_vacation_enabled', 'id' => $field_id, 'value' => 1));
 
-      $blocks['main']['options']['vacation_enabled'] = array('title' => html::label($field_id, 'Auto-responder enabled'),
+      $blocks['main']['options']['vacation_enabled'] = array('title' => html::label($field_id, rcube::Q($this->gettext('autoresponderenabled'))),
 							     'content' => $input_vacation_enabled->show($config['vacation_enabled'] ? 1 : 0)
 							     );
 
@@ -143,7 +148,7 @@ class iwebadmin extends rcube_plugin
       $field_id = 'vacation_subject';
       $input_vacation_subject = new html_inputfield(array('name' => '_vacation_subject', 'id' => $field_id, 'size' => '80'));
 
-      $blocks['main']['options']['vacation_subject'] = array('title' => html::label($field_id, 'Subject'),
+      $blocks['main']['options']['vacation_subject'] = array('title' => html::label($field_id, rcube::Q($this->gettext('subject'))),
 							     'content' => $input_vacation_subject->show($config['vacation_subject'])
 							     );
 
@@ -152,7 +157,7 @@ class iwebadmin extends rcube_plugin
       $field_id = 'vacation_message';
       $input_vacation_message = new html_textarea(array('name' => '_vacation_message', 'id' => $field_id, 'cols' => 80, 'rows' => 15));
 
-      $blocks['main']['options']['vacation_message'] = array('title' => html::label($field_id, 'Message'),
+      $blocks['main']['options']['vacation_message'] = array('title' => html::label($field_id, rcube::Q($this->gettext('message'))),
 							     'content' => $input_vacation_message->show($config['vacation_message'])
 							     );
 
@@ -161,6 +166,7 @@ class iwebadmin extends rcube_plugin
     else if ($args['section'] == 'password' && $RCMAIL->config->get('iwebadmin_allow_password')) {
 
       $blocks = array('main' => array('name' => rcube::Q($RCMAIL->gettext('mainoptions', null))),
+					  'explanation' => array('name' => rcube::Q($this->gettext('qmail_password_explanation'))), 
 		      );
 
       // Old password
@@ -168,7 +174,7 @@ class iwebadmin extends rcube_plugin
       $field_id = 'password_old';
       $input_password_old = new html_passwordfield(array('name' => '_password_old', 'id' => $field_id, 'size' => '20'));
 
-      $blocks['main']['options']['password_old'] = array('title' => html::label($field_id, 'Old Password'),
+      $blocks['main']['options']['password_old'] = array('title' => html::label($field_id, rcube::Q($this->gettext('oldpassword'))),
 							 'content' => $input_password_old->show()
 							 );
 
@@ -177,7 +183,7 @@ class iwebadmin extends rcube_plugin
       $field_id = 'password_new1';
       $input_password_new1 = new html_passwordfield(array('name' => '_password_new1', 'id' => $field_id, 'size' => '20'));
 
-      $blocks['main']['options']['password_new1'] = array('title' => html::label($field_id, 'New Password'),
+      $blocks['main']['options']['password_new1'] = array('title' => html::label($field_id, rcube::Q($this->gettext('newpassword'))),
 							  'content' => $input_password_new1->show()
 							  );
 
@@ -186,10 +192,40 @@ class iwebadmin extends rcube_plugin
       $field_id = 'password_new2';
       $input_password_new2 = new html_passwordfield(array('name' => '_password_new2', 'id' => $field_id, 'size' => '20'));
 
-      $blocks['main']['options']['password_new2'] = array('title' => html::label($field_id, 'New Password (again)'),
+      $blocks['main']['options']['password_new2'] = array('title' => html::label($field_id, rcube::Q($this->gettext('newpassword2'))),
 							  'content' => $input_password_new2->show()
 							  );
 
+	  // Password requisites explanation
+	  
+	  $blocks['explanation']['options']['password_explanation1'] = array('title' => html::label('password_explanation1', 
+		rcube::Q($this->gettext('password_explanation1a')) . ' ' . $RCMAIL->config->get('iwebadmin_password_min_length') . ' ' . rcube::Q($this->gettext('password_explanation1b')) . ' ' . $RCMAIL->config->get('iwebadmin_password_max_length') . ' ' . rcube::Q($this->gettext('password_explanation1c') . '. ')),
+		'content' => '');
+	  
+	  if (
+		    ($RCMAIL->config->get('iwebadmin_password_lower_need'))
+		 || ($RCMAIL->config->get('iwebadmin_password_upper_need'))
+		 || ($RCMAIL->config->get('iwebadmin_password_number_need'))
+		 || ($RCMAIL->config->get('iwebadmin_password_special_need'))
+		 ) {
+			$blocks['explanation']['options']['password_explanation2'] = array('title' => html::label('password_explanation2', rcube::Q($this->gettext('password_explanation2'))), 'content' => '');
+	  }
+	  if ((!($RCMAIL->config->get('iwebadmin_password_lower_need'))) && (!($RCMAIL->config->get('iwebadmin_password_upper_need')))) {
+		$blocks['explanation']['options']['password_explanation3'] = array('title' => html::label('password_explanation3', '* ' . rcube::Q($this->gettext('password_explanation3')) . '; '), 'content' => '');
+	  }
+	  if ($RCMAIL->config->get('iwebadmin_password_lower_need')) {
+		$blocks['explanation']['options']['password_explanation4'] = array('title' => html::label('password_explanation4', '* ' . rcube::Q($this->gettext('password_explanation4')) . '; '), 'content' => '');
+	  }
+	  if ($RCMAIL->config->get('iwebadmin_password_upper_need')) {
+	    $blocks['explanation']['options']['password_explanation5'] = array('title' => html::label('password_explanation5', '* ' . rcube::Q($this->gettext('password_explanation5')) . '; '), 'content' => '');
+	  }
+	  if ($RCMAIL->config->get('iwebadmin_password_number_need')) {
+        $blocks['explanation']['options']['password_explanation6'] = array('title' => html::label('password_explanation6', '* ' . rcube::Q($this->gettext('password_explanation6')) . '; '), 'content' => '');
+	  }
+	  if ($RCMAIL->config->get('iwebadmin_password_special_need')) {
+		$blocks['explanation']['options']['password_explanation7'] = array('title' => html::label('password_explanation7', '* ' . rcube::Q($this->gettext('password_explanation7')) . ' "' . $RCMAIL->config->get('iwebadmin_password_special_chars') . '"; '), 'content' => '');
+	  }
+	  
       $args['blocks'] = $blocks;
     }
 
@@ -203,6 +239,7 @@ class iwebadmin extends rcube_plugin
   {
     $RCMAIL = rcmail::get_instance();
 
+	/* Vacation message section */
     if ($args['section'] == 'vacation' && $RCMAIL->config->get('iwebadmin_allow_vacation')) {
       $vacation_enabled = isset($_POST['_vacation_enabled']) ? true : false;
       $vacation_message = $_POST['_vacation_message'];
@@ -221,23 +258,25 @@ class iwebadmin extends rcube_plugin
       // Override the form values with the user's vacation settings
 
       if ($vacation_enabled) {
-	$params['vacation'] = 'on';
-	$params['vsubject'] = $vacation_subject;
-	$params['vmessage'] = $vacation_message;
+	    $charset    = strtoupper($RCMAIL->config->get('iwebadmin_charset', 'ISO-8859-1'));
+		$rc_charset = strtoupper($RCMAIL->output->get_charset());
+		$params['vacation'] = 'on';
+		$params['vsubject'] = rcube_charset::convert($vacation_subject, $rc_charset, $charset);
+		$params['vmessage'] = rcube_charset::convert($vacation_message, $rc_charset, $charset);
       }
       else {
-	unset($params['vacation']);
+		unset($params['vacation']);
       }
 
       $result = $this->http_post($url, $params);
 
       // If we're display a vacation warning, refresh the page
-
       if ($RCMAIL->config->get('iwebadmin_vacation_warning')) {
-	global $OUTPUT;
-	$OUTPUT->command('reload', 1000);
+		global $OUTPUT;
+		$OUTPUT->command('reload', 1000);
       }
     }
+	/* Password change section */
     else if ($args['section'] == 'password' && $RCMAIL->config->get('iwebadmin_allow_password')) {
       $password_old = $_POST['_password_old'];
       $password_new1 = $_POST['_password_new1'];
@@ -245,20 +284,77 @@ class iwebadmin extends rcube_plugin
 
       // Verify the input.
 
-      // Verify old password is correct
+      // Verify if old password is correct
       if ($password_old != $RCMAIL->decrypt($_SESSION['password'])) {
-	return $this->error('Old password was not correct!', $args);
+		return $this->error(rcube::Q($this->gettext('oldpasswordnotcorrect')), $args);
       }
 
-      // Verify new passwords match
+      // Verify if new passwords match
       if ($password_new1 != $password_new2) {
-        return $this->error('New passwords did not match!', $args);
+        return $this->error(rcube::Q($this->gettext('newpassnotmatch')), $args);
       }
 
-      // Verify new password is long enough
+      // Verify if new password is long enough
       if (strlen($password_new1) < $RCMAIL->config->get('iwebadmin_password_min_length')) {
-        return $this->error('New password is too short. It must be at least '.$RCMAIL->config->get('iwebadmin_password_min_length').' characters long.', $args);
+		return $this->error($this->PassNeeds('iwebadmin_password_min_length'));  
       }
+	  
+	  // Verify if new password is longer than maximum allowed
+      if (strlen($password_new1) > $RCMAIL->config->get('iwebadmin_password_max_length')) {
+		return $this->error($this->PassNeeds('iwebadmin_password_max_length'));  
+      }
+
+	  // Verify if we have any letter set if both lowercase / uppercase are false
+ 	  if ((!($RCMAIL->config->get('iwebadmin_password_lower_need'))) && (!($RCMAIL->config->get('iwebadmin_password_upper_need')))) {
+		 if (preg_match('/[a-zA-Z]/', preg_quote($password_new1, '/')) !== 1) {
+	      return $this->error($this->PassNeeds('iwebadmin_password_letter_need'));
+		 }
+	  }
+	  
+	  // Verify if new password has lowercase letters
+	  if ($RCMAIL->config->get('iwebadmin_password_lower_need')) {
+		if (preg_match('/[a-z]/', preg_quote($password_new1, '/')) !== 1) {
+	      return $this->error($this->PassNeeds('iwebadmin_password_lower_need'));
+		}
+	  }
+		
+	  // Verify if new password has uppercase letters		
+	  if ($RCMAIL->config->get('iwebadmin_password_upper_need')) {
+		if (preg_match('/[A-Z]/', preg_quote($password_new1, '/')) !== 1) {
+		  return $this->error($this->PassNeeds('iwebadmin_password_upper_need'));
+		}
+	  }
+	  
+	  // Verify if new password has numbers
+	  if ($RCMAIL->config->get('iwebadmin_password_number_need')) {
+		if (preg_match('/[0-9]/', preg_quote($password_new1, '/')) !== 1) {
+		  return $this->error($this->PassNeeds('iwebadmin_password_number_need'));
+		}
+	  }
+	  
+	  // Verify if new passowrd has special chars
+	  if ($RCMAIL->config->get('iwebadmin_password_special_need')) {
+		  $specialchars = $RCMAIL->config->get('iwebadmin_password_special_chars');
+		  if ((!isset($specialchars)) || ($specialchars == "")) { 
+			$specialchars = "@!#-_.";
+		  }
+		  if (strlen($specialchars) > 20) {
+			// configuration error on iwebadmin_password_special_chars
+			return $this->error($this->PassNeeds('iwebadmin_password_special_chars'));
+	      }
+		  $scf = false;
+		  for ($l = 0 ; $l < strlen($specialchars); $l++) {
+			if (strpos($password_new1, $specialchars[$l]) !== false) {
+			  $scf = true;
+			  break;
+			}
+	      }
+		  if (!$scf) {
+		    return $this->error($this->PassNeeds('iwebadmin_password_special_need'));
+		  }
+	  }
+ 
+	  // Everything is ok! All checks ok!
 
       // Login to iwebadmin and get the modify user form
 
@@ -272,17 +368,37 @@ class iwebadmin extends rcube_plugin
       $result = $this->http_post($url, $params);
 
       if (strpos($result, 'Invalid password') !== false) {
-	return $this->error('Invalid password, please try a more complex one.', $args);
+		return $this->error(rcube::Q($this->gettext('invalidpassword')), $args);
       }
 
       if (strpos($result, 'Email Account password changed successfully') === false) {
-	return $this->error('Password could not be changed.', $args);
+		return $this->error(rcube::Q($this->gettext('couldnotchange')), $args);
       }
 
       // Set the new password on the session so they don't have to log back in again
       $_SESSION['password'] = $RCMAIL->encrypt($password_new1);
     }
     return $args;
+  }
+  
+  /**
+   * Return string with i18n for format passwords in the expected way
+   * This allow users to know how to create a new password meeting 
+   * the settings - lowercase, uppercase, numbers, special chars, min and max lenght, and so on.
+   */
+  function PassNeeds($from = '') {
+	switch($from) {
+	  case 'iwebadmin_password_min_length': 
+	  case 'iwebadmin_password_max_length': 
+	  case 'iwebadmin_password_letter_need':
+	  case 'iwebadmin_password_lower_need': 
+	  case 'iwebadmin_password_upper_need': 
+	  case 'iwebadmin_password_number_need': 
+	  case 'iwebadmin_password_special_need': 
+		return rcube::Q($this->gettext($from)) . '.';
+	  default:
+	    return 'Error string was not defined.';
+	}
   }
 
   /**
@@ -321,7 +437,7 @@ class iwebadmin extends rcube_plugin
     $email = $_SESSION['username'];
     $pos = strpos($email, '@');
     if ($pos === false) {
-      raise_error(array('code' => 1000, 'type' => 'php', 'message' => 'Unable to locate user/domain parts in '.$email), true, true);
+      raise_error(array('code' => 1000, 'type' => 'php', 'message' => rcube::Q($this->gettext('erroruserdomainparts')).' '.$email), true, true);
     }
     $user = substr($email, 0, $pos);
     $domain = substr($email, $pos+1);
@@ -338,7 +454,7 @@ class iwebadmin extends rcube_plugin
     $login_result = $this->http_post($url, $params);
 
     if (strpos($login_result, 'Modify User: '.$email) === false) {
-      raise_error(array('code' => 1001, 'type' => 'php', 'message' => 'Unable to login to iwebadmin'), true, true);
+      raise_error(array('code' => 1001, 'type' => 'php', 'message' => rcube::Q($this->gettext('unabletologiniwebadmin'))), true, true);
     }
 
     // Parse the login results, which should be the modify user form
@@ -348,7 +464,7 @@ class iwebadmin extends rcube_plugin
 
     $form_els = $form_doc->getElementsByTagName('form');
     if ($form_els->length != 1) {
-      raise_error(array('code' => 1002, 'type' => 'php', 'message' => 'Unable to find modify user form'), true, true);
+      raise_error(array('code' => 1002, 'type' => 'php', 'message' => rcube::Q($this->gettext('unabletofindmodiyform'))), true, true);
     }
 
     $form = $form_els->item(0);
@@ -429,12 +545,12 @@ class iwebadmin extends rcube_plugin
 
     $fp = fopen($url, 'rb', false, stream_context_create($context_params));
     if (!$fp) {
-      raise_error(array('code' => 1003, 'type' => 'php', 'message' => 'Could not open a connection to '.$url), true, true);
+      raise_error(array('code' => 1003, 'type' => 'php', 'message' => rcube::Q($this->gettext('cantconnect')).' '.$url), true, true);
     }
 
     $response = stream_get_contents($fp);
     if ($response === false) {
-      raise_error(array('code' => 1004, 'type' => 'php', 'message' => 'Could not read data from '.$url), true, true);
+      raise_error(array('code' => 1004, 'type' => 'php', 'message' => rcube::Q($this->gettext('cantreadfrom')).' '.$url), true, true);
     }
 
     fclose($fp);
