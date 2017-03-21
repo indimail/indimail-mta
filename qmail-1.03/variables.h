95891add9a1bbdfbe202766122c279c15432995d
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-03-21 15:40:55 +0530

    added certdir variable

--- qmail-1.03/variables.h
+++ qmail-1.03/variables.h
@@ -1,5 +1,8 @@
 /*
  * $Log: variables.h,v $
+ * Revision 1.4  2017-03-21 15:40:45+05:30  Cprogrammer
+ * added certdir variable
+ *
  * Revision 1.3  2004-06-18 23:02:28+05:30  Cprogrammer
  * added RCS log
  *
@@ -9,6 +12,7 @@
 
 extern char    *queuedir;
 extern char    *controldir;
+extern char    *certdir;
 extern int      use_fsync;
 
 #endif
