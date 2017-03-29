620ccc81770d12fc63f09f0cfa47073adada1dea
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-03-29 14:51:26 +0530

    fix for increase in number of arguments

--- qmail-1.03/envmigrate.sh
+++ qmail-1.03/envmigrate.sh
@@ -1,4 +1,7 @@
 # $Log: envmigrate.sh,v $
+# Revision 1.4  2017-03-29 14:50:48+05:30  Cprogrammer
+# fix for increase in arguments
+#
 # Revision 1.3  2017-03-01 22:53:59+05:30  Cprogrammer
 # secure permission for env variables
 #
@@ -9,8 +12,8 @@
 # Initial revision
 #
 #
-if [ $# -ne 1 ] ; then
-	echo "USAGE: $0 config_file"
+if [ $# -lt 1 ] ; then
+	echo "USAGE: $0 config_file [owner group permission]"
 	exit 1
 fi
 if [ ! -f $1 ] ; then
@@ -19,19 +22,34 @@ if [ ! -f $1 ] ; then
 fi
 dir=`dirname $1`
 file=`basename $1`
-mv $1 $1.tmp
+mkdir=$(which mkdir)
+mv=$(which mv)
+chmod=$(which chmod)
+chown=$(which chown)
+grep=$(which grep)
+$mv $1 $1.tmp
+if [ $# -eq 4 ] ; then
+	owner=$2
+	group=$3
+	perm=$4
+else
+	owner="root"
+	group="root"
+	perm="500"
+fi
 if [ $? = 0 ] ; then
-	mkdir $1
-	chmod 500 $1
+	$mkdir $1
+	$chown "$owner":"$group" $1
+	$chmod $perm $1
 	if [ $? = 0 ] ; then
-		mv $1.tmp $1/."$file"
+		$mv $1.tmp $1/."$file"
 	else
-		mv $1.tmp $1
+		$mv $1.tmp $1
 		echo "Error Creating Directory $1"
 		exit 1
 	fi
 fi
-grep -v "^#" $1/."$file" | while read line
+$grep -v "^#" $1/."$file" | while read line
 do
 	if [ ! " $line" = " " ] ; then
 		file=`echo $line | cut -d= -f1`
