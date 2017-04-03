681d5c02930fb3931679e07c75b605c999b475d3
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-03 15:56:34 +0530

    increase MAX_LANG_STR size

--- iwebadmin-1.3/iwebadmin.h
+++ iwebadmin-1.3/iwebadmin.h
@@ -1,5 +1,5 @@
 /*
- * $Id: iwebadmin.h,v 1.1 2010-04-26 12:08:01+05:30 Cprogrammer Exp mbhangui $
+ * $Id: iwebadmin.h,v 1.2 2017-04-03 15:55:03+05:30 Cprogrammer Exp mbhangui $
  * Copyright (C) 1999-2004 Inter7 Internet Technologies, Inc. 
  *
  * This program is free software; you can redistribute it and/or modify
