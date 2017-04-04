4eaabb8fbdf0713ac978846a8aacb7b0266b5a68
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-05 03:16:54 +0530

    use syscall for setgroups(), setgid(), setuid() to avoid NPTL wrapper for these functions probable BUG with this functions with dlmopen() in current kernel/glibc

--- ucspi-tcp-0.88/prot.c
+++ ucspi-tcp-0.88/prot.c
@@ -1,5 +1,9 @@
 /*
  * $Log: prot.c,v $
+ * Revision 1.3  2017-04-05 03:14:57+05:30  Cprogrammer
+ * use syscall for setgroups(), setgid(), setuid() to avoid NPTL wrapper for these functions
+ * probable BUG with this functions with dlmopen() in current kernel
+ *
  * Revision 1.2  2007-06-10 10:15:28+05:30  Cprogrammer
  * fixed compilation warning
  *
@@ -12,24 +16,29 @@
 #include <unistd.h>
 #include <grp.h>
 
+#include <syscall.h>
+#define SYS_SETGROUPS(NGRUP,MYGIDSET) syscall(SYS_setgroups, NGRUP, MYGIDSET)
+#define SYS_SETGID(MYGID) syscall(SYS_setgid, MYGID)
+#define SYS_SETUID(MYUID) syscall(SYS_setuid, MYUID)
+
 int
-prot_gid(unsigned int gid)
+prot_gid(gid_t gid)
 {
 #ifdef HASSHORTSETGROUPS
 	short           x[2];
 	x[0] = gid;
-	x[1] = 73;					/*- catch errors */
+	x[1] = 73; /*- catch errors */
 	if (setgroups(1, x) == -1)
 		return -1;
 #else
-	if (setgroups(1, &gid) == -1)
+	if (SYS_SETGROUPS(1, &gid) == -1)
 		return -1;
 #endif
-	return setgid(gid);			/*- _should_ be redundant, but on some systems it isn't */
+	return SYS_SETGID(gid);	/*- _should_ be redundant, but on some systems it isn't */
 }
 
 int
-prot_uid(int uid)
+prot_uid(uid_t uid)
 {
-	return setuid(uid);
+	return SYS_SETUID(uid);
 }
