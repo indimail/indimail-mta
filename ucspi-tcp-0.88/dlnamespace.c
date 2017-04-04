944eed2c64c5446498cae6de206e378bf0660b06
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-05 04:37:03 +0530

    replace ':' character after str comparision

--- ucspi-tcp-0.88/dlnamespace.c
+++ ucspi-tcp-0.88/dlnamespace.c
@@ -1,5 +1,8 @@
 /*
  * $Log: dlnamespace.c,v $
+ * Revision 1.3  2017-04-05 04:36:25+05:30  Cprogrammer
+ * replace ':' character after str comparision
+ *
  * Revision 1.2  2017-04-05 04:05:14+05:30  Cprogrammer
  * conditional compilation of dlnamespace()
  *
@@ -37,8 +40,10 @@ dlnamespace(char *fn, unsigned long *id)
 				*ptr = 0;
 				if (!str_diff(ptr + 1, fn)) {
 					scan_ulong(cptr, id);
+					*ptr = ':';
 					return (1);
-				}
+				} else
+					*ptr = ':';
 			}
 		}
 		return (0);
@@ -53,6 +58,7 @@ dlnamespace(char *fn, unsigned long *id)
 	else
 	if (!stralloc_cats(&namespace, fn))
 		return (-1);
+	else
 	if (!stralloc_0(&namespace))
 		return (-1);
 	return (0);
