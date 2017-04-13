6a8c9901d9cb902ab12334d7e40c2679193f739a
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-13 20:05:55 +0530

    Revert "Revert "added rsa2048.pem, dsa2048.pem creation""
    This reverts commit 83978f02de625ad3d26ce3163ee6fc458344a6c4.
    
    # Conflicts:
    #	qmail-1.03/update_tmprsadh.sh

--- indimail-2.1/roundcube/usr/share/roundcubemail/plugins/iwebadmin/onvacation.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    if (!rcmail.env.framed) {
-	$('BODY').append('<div id="onvacation" style="position: absolute; top: 5px; left: 220px; width: 500px; z-index: 2; border: 1px solid #da8080; background-color: #efdeed; padding: 2px 4px; text-align: center;">'+rcmail.gettext('vacationmessagealert', 'iwebadmin')+'. <a href="./?_task=settings">'+rcmail.gettext('change', 'iwebadmin')+'</a></div>');
-    }
-
-});
