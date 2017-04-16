3565432caeba753db36a1307887d8c1820c90b7a
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-16 14:23:13 +0530

    corrected FHS paths

--- qmail-1.03/home+df.sh
+++ qmail-1.03/home+df.sh
@@ -4,6 +4,6 @@
 # Using dot-forward to support sendmail-style ~/.forward files.
 # Using qmail-local to deliver messages to ~/Mailbox by default.
 
-exec env - PATH="QMAIL/bin:$PATH" \
+exec env - PATH="PREFIX/sbin:PREFIX/bin$PATH" \
 qmail-daemon '|dot-forward .forward
 ./Mailbox' splogger qmail
