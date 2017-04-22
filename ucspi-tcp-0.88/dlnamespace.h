fc2219587c3f3ad63303476a92d0ded2466ed86c
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-22 12:13:11 +0530

    added new argument environ list to dlnamespace()

--- ucspi-tcp-0.88/dlnamespace.h
+++ ucspi-tcp-0.88/dlnamespace.h
@@ -1,5 +1,8 @@
 /*
  * $Log: dlnamespace.h,v $
+ * Revision 1.2  2017-04-22 11:55:11+05:30  Cprogrammer
+ * added new argument, environ list to dlnamespace()
+ *
  * Revision 1.1  2017-04-05 03:09:29+05:30  Cprogrammer
  * Initial revision
  *
@@ -8,6 +11,6 @@
 #ifndef DLNAMESPACE_H
 #define DLNAMESPACE_H
 
-int             dlnamespace(char *, unsigned long *);
+int             dlnamespace(char *, char **, unsigned long *);
 
 #endif
