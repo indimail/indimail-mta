b7bb3f1fe63b6eec4ddb1df9d8ddba45300751b1
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-24 15:42:14 +0530

    added upgrade scripts

---
+++ qmail-1.03/local_upgrade.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+# $Log: $
+#
+# $Id:$
+#
+PATH=/bin:/usr/bin:/usr/sbin:/sbin
+chown=$(which chown)
+chgrp=$(which chgrp)
+ln=$(which ln)
+uname=$(which uname)
+chmod=$(which chmod)
+mkdir=$(which mkdir)
+rm=$(which rm)
+mv=$(which mv)
+sed=$(which sed)
+
+do_post_upgrade()
+{
+#
+# certs were in /etc/indimail/control
+# they have been moved to /etc/indimail/certs
+#
+if [ ! -d /etc/indimail/certs ] ; then
+	$mkdir -p /etc/indimail/certs
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+	$chown indimail:qmail /etc/indimail/certs
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+	$chmod 2775 /etc/indimail/certs
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+fi
+# move existing certs in control directory to /etc/indimail/certs
+for i in servercert.pem clientcert.pem dh1024.pem dh512.pem \
+	rsa2048.pem dh2048.pem rsa512.pem couriersslcache servercert.cnf \
+	servercert.rand tlshosts notlshosts
+do
+	if [ -f /etc/indimail/control/$i -a ! -L /etc/indimail/control/$i ] ; then
+		$mv /etc/indimail/control/$i /etc/indimail/certs/$i
+		if [ $? -ne 0 ] ; then
+			exit 1
+		fi
+	fi
+	if [ -d /etc/indimail/control/$i -a ! -L /etc/indimail/control/$i ] ; then
+		$mv /etc/indimail/control/$i /etc/indimail/certs/$i
+		if [ $? -ne 0 ] ; then
+			exit 1
+		fi
+		$ln -rsf /etc/indimail/certs/$i /etc/indimail/control/$i
+	fi
+done
+# remove clientcert.pem link to servercert.pem in control directory
+if [ -f /etc/indimail/control/clientcert.pem ] ; then
+	$rm -f /etc/indimail/control/clientcert.pem
+fi
+if [ -f /etc/indimail/certs/clientcert.pem ] ; then
+	$rm -f /etc/indimail/certs/clientcert.pem
+fi
+for i in servercert.pem dh2048.pem rsa2048.pem dh1024.pem rsa1024.pem dh512.pem rsa512.pem
+do
+	# roundcube (php) will require read access to certs
+	if [ -f /etc/indimail/certs/$i ] ; then
+		$chgrp apache /etc/indimail/certs/$i
+	fi
+done
+$ln -rsf /etc/indimail/certs/servercert.pem /etc/indimail/control/servercert.pem
+$ln -rsf /etc/indimail/certs/servercert.pem /etc/indimail/control/clientcert.pem
+$ln -rsf /etc/indimail/certs/servercert.pem /etc/indimail/certs/clientcert.pem
+# Certificate location changed from /etc/indimail/control to /etc/indimail/certs
+for i in qmail-smtpd.25 qmail-smtpd.465 qmail-smtpd.587 qmail-send.25
+do
+	echo /etc/indimail/certs > /service/$i/variables/CERTDIR
+	# increase for using dlmopen()
+	if [ ! " $i" = " qmail-send.25" ] ; then
+		echo 536870912 > /service/$i/variables/SOFT_MEM
+	fi
+done
+
+# service qmail-spamlog has been renamed to qmail-logfifo
+# fifo is now /tmp/logfifo instead of /tmp/spamfifo
+if [ -d /service/qmail-spamlog ] ; then
+	$mv /service/qmail-spamlog /service/qmail-logfifo
+	$mkdir -p /service/qmail-logfifo/variables
+	$chown root:indimail /service/qmail-logfifo/variables
+	$chmod 775 /service/qmail-logfifo/variables
+	echo /tmp/logfifo > /service/qmail-logfifo/variables/LOGFILTER
+	$sed -e 's{smtpd.25{logfifo{' /service/qmail-logfifo/run > /tmp/logfifo.$$
+	$mv /tmp/logfifo.$$ /service/qmail-logfifo/run
+	$sed -e 's{spamlog{logfifo{' /service/qmail-logfifo/log/run > /tmp/logfifo.$$
+	$mv /tmp/logfifo.$$ /service/qmail-logfifo/log/run
+fi
+
+# for bogofilter to send back X-Bogosity back to qmail-smtpd as well as log entry
+# to /var/log/indimail/logfifo/current
+# for qmail-send it is required if you run bogofilter during remote/local delivery,
+# in which case it will be logged to /var/log/indimail/logfifo/current
+for i in qmail-smtpd.25 qmail-smtpd.465 qmail-send.25
+do
+	if [ -d /service/$i -a -s /service/$i/variables/LOGFILTER ] ; then
+		echo /tmp/logfifo > /service/$i/variables/LOGFILTER
+	fi
+done
+if [ -s /etc/indimail/control/defaultqueue/LOGFILTER ] ; then
+echo /tmp/logfifo > /etc/indimail/control/defaultqueue/LOGFILTER
+fi
+#
+# tcpserver uses -c option to set concurrency and uses MAXDAEMON config file
+# on sighup, since tcpserver is no longer root, it is unable to read MAXDAEMON config
+# file. Better solution is to move MAXDAEMON config file out of /service/*/variables
+# directory
+for i in qmail-*qm?pd.* qmail-smtpd.*
+do
+	$chown root:indimail /service/$i/variables
+	$chmod 755 /service/$i/variables
+done
+
+$uname -n > /service/qmail-send.25/variables/DEFAULT_DOMAIN
+$uname -n > /etc/indimail/control/envnoathost
+$uname -n > /etc/indimail/control/defaulthost
+if [ -f /service/fetchmail/variables/QMAILDEFAULTHOST ] ; then
+	$rm -f /service/fetchmamil/variables/QMAILDEFAULTHOST
+fi
+# qmail-greyd, greydaemon path changed to /usr/sbin
+$sed -i 's{/bin/qmail-greyd{/sbin/qmail-greyd{' /service/greylist.1999/run
+}
+
+case $1 in
+	post)
+	do_post_upgrade
+	;;
+esac
---
+++ qmail-1.03/upgrade.sh
@@ -0,0 +1,164 @@
+#!/bin/sh
+# $Log: upgrade.sh,v $
+# Revision 2.4  2017-03-29 22:37:06+05:30  Cprogrammer
+# added case for pretrans
+#
+# Revision 2.3  2017-03-29 14:49:56+05:30  Cprogrammer
+# fixes for v2.1
+#
+# Revision 2.2  2017-03-28 19:15:10+05:30  Cprogrammer
+# added do_upgrade scriptlet
+#
+# Revision 2.1  2017-03-28 19:12:18+05:30  Cprogrammer
+# generic upgrade script for indimail
+#
+#
+# $Id: upgrade.sh,v 2.4 2017-03-29 22:37:06+05:30 Cprogrammer Exp mbhangui $
+
+do_upgrade()
+{
+	if [ -f /usr/libexec/indimail/local_upgrade.sh ] ; then
+		echo "Running upgrade script for $1"
+		sh /usr/libexec/indimail/local_upgrade.sh
+		#if [ $? -eq 0 ] ; then
+		#	/bin/rm -f /usr/libexec/indimail/local_upgrade.sh
+		#fi
+	fi
+}
+
+do_pre()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	echo "RPM Version  $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		install)
+		echo do_pre install
+		;;
+		upgrade)
+		echo do_pre upgrade
+		do_upgrade pre
+		;;
+	esac
+}
+
+do_post()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	echo "RPM Version  $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		install)
+		echo do_post install
+		;;
+		upgrade)
+		echo do_post upgrade
+		do_upgrade post
+		;;
+	esac
+}
+
+do_preun()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	echo $RPM_VER > /tmp/indimail-rpm.old
+	echo "RPM Version  $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		upgrade)
+		echo do_preun upgrade
+		;;
+		uninstall)
+		echo do_preun uninstall
+		;;
+		## debian ###
+		remove)
+		echo do_preun remove
+		;;
+		deconfigure)
+		echo do_preun deconfigure
+		;;
+	esac
+}
+
+do_postun()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	echo "RPM Version  $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		upgrade)
+		echo do_postun upgrade
+		;;
+		uninstall|remove)
+		echo do_postun uninstall
+		;;
+		## debian ##
+    	purge|failed-upgrade|abort-install|abort-upgrade|disappear)
+		echo do_postun purge
+		;;
+	esac
+}
+
+do_prettrans()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	echo "RPM Version $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		noargs)
+		echo do_prettrans noargs
+		do_upgrade prettrans
+		;;
+	esac
+}
+
+do_posttrans()
+{
+	RPM_VER=`rpm -qf /etc/indimail/indimail-release`
+	if [ -f /tmp/indimail-rpm.old ] ; then
+		OLD_RPM_VER=`cat /tmp/indimail-rpm.old`
+	fi
+	echo "RPM Version old $OLD_RPM_VER new $RPM_VER"
+	if [ -f /etc/indimail/indimail-release ] ; then
+		. /etc/indimail/indimail-release
+	fi
+	case $1 in
+		noargs)
+		echo do_posttrans noargs
+		do_upgrade posttrans
+		;;
+	esac
+}
+
+version=$3
+case $1 in
+	pre)
+	do_pre $2
+	;;
+	post)
+	do_post $2
+	;;
+	preun|prerm)
+	do_preun $2
+	;;
+	postun)
+	do_postun $2
+	;;
+	prettrans)
+	do_prettrans $2
+	;;
+	posttrans)
+	do_posttrans $2
+	;;
+esac
