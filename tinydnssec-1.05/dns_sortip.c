d83087f989998a4fcbc846a62e480d0a0117067d
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-26 13:30:54 +0530

    removed dnscurve code

--- tinydnssec-1.05/dns_sortip.c
+++ tinydnssec-1.05/dns_sortip.c
@@ -18,22 +18,3 @@ void dns_sortip(char *s,unsigned int n)
     byte_copy(s + (n << 2),4,tmp);
   }
 }
-
-#ifdef DNSCURVE
-void dns_sortip2(char *s,char *t,unsigned int n)
-{
-  unsigned int i;
-  char tmp[32];
-
-  while (n > 1) {
-    i = dns_random(n);
-    --n;
-    byte_copy(tmp,4,s + (i << 2));
-    byte_copy(s + (i << 2),4,s + (n << 2));
-    byte_copy(s + (n << 2),4,tmp);
-    byte_copy(tmp,32,t + (i << 5));
-    byte_copy(t + (i << 5),32,t + (n << 5));
-    byte_copy(t + (n << 5),32,tmp);
-  }
-}
-#endif
