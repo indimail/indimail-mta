4e17167f70b4a070ade81fc0b54d0dd4b1ca563c
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-04-26 18:47:33 +0530

    dqcache - recursive DNS server with DNSCurve support

---
+++ tinydnssec-1.05/dq-20161210/dq/iptostr.h
@@ -0,0 +1,9 @@
+#ifndef _IPTOSTR_H____
+#define _IPTOSTR_H____
+
+#define IPTOSTR_LEN 46
+
+extern char *iptostr(char *, const unsigned char *);
+extern char *iptostrx(char *, const unsigned char *);
+
+#endif
