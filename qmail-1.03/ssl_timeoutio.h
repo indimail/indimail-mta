ae3a8f6205e40b6a295e0c752fac3863622291e9
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-08-08 17:39:22 +0530

    removed extra space

--- qmail-1.03/ssl_timeoutio.h
+++ qmail-1.03/ssl_timeoutio.h
@@ -16,11 +16,11 @@
 # error "Need OpenSSL version at least 0.9.6"
 #endif
 
-int             ssl_timeoutconn(long t, int rfd, int wfd, SSL * ssl);
-int             ssl_timeoutaccept(long t, int rfd, int wfd, SSL * ssl);
-int             ssl_timeoutrehandshake(long t, int rfd, int wfd, SSL * ssl);
-int             ssl_timeoutread(long t, int rfd, int wfd, SSL * ssl, char *buf, int len);
-int             ssl_timeoutwrite(long t, int rfd, int wfd, SSL * ssl, char *buf, int len);
-int             ssl_timeoutio(int (*fun) (), long t, int rfd, int wfd, SSL * ssl, char *buf, int len);
+int             ssl_timeoutconn(long t, int rfd, int wfd, SSL *ssl);
+int             ssl_timeoutaccept(long t, int rfd, int wfd, SSL *ssl);
+int             ssl_timeoutrehandshake(long t, int rfd, int wfd, SSL *ssl);
+int             ssl_timeoutread(long t, int rfd, int wfd, SSL *ssl, char *buf, int len);
+int             ssl_timeoutwrite(long t, int rfd, int wfd, SSL *ssl, char *buf, int len);
+int             ssl_timeoutio(int (*fun) (), long t, int rfd, int wfd, SSL *ssl, char *buf, int len);
 
 #endif
