2d11fe241d7bcd7aab6215fc048760837520380c
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2017-11-20 23:23:08 +0530

    header for binary search in inlookup

---
+++ indimail-2.2/in_bsearch.h
@@ -0,0 +1,33 @@
+/*
+ * $Log: in_bsearch.h,v $
+ * Revision 2.1  2017-11-20 23:22:32+05:30  Cprogrammer
+ * header for binary search
+ *
+ */
+/*
+ * __USE_GNU exposes the GNU tdestroy() function, a
+ * function that is not mentioned by the Single Unix
+ * Specification. 
+ */
+#define __USE_GNU 1
+#define _GNU_SOURCE
+#include <search.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+typedef struct
+{
+	char           *in_key;
+	struct passwd   in_pw;
+	char            pwStat;
+	char           *aliases;
+	char           *mdahost;
+	/*-
+	 *  0: User is fine
+	 *  1: User is not present
+	 *  2: User is Inactive
+	 *  3: User is overquota
+	 * -1: System Error
+	 */
+	int             in_userStatus;
+} INENTRY;
