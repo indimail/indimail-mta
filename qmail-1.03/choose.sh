f70233cab849e5224a7d03fdb721c772017cdf1b
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-01-09 12:51:14 +0530

    added R option for mysql_config

--- qmail-1.03/choose.sh
+++ qmail-1.03/choose.sh
@@ -13,6 +13,12 @@ case "$1" in
   *r*) ./$2 >/dev/null 2>&1 || result="$3" ;;
 esac
 
-rm -f $2.o $2
+case "$1" in
+  *R*) ./$2 >/dev/null 2>&1 || result="$3" ;;
+esac
+
+case "$1" in
+	*c* | *l* | *L* | *r*) rm -f $2.o $2 ;;
+esac
 
 exec cat "$result"
