1a9c52de5eb805a1c54b7597303e0332fe6b5249
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-01-27 21:10:18 +0530

    indimail v2.5

--- indimail-2.4/eps-1.2/eps_email.h
+++
@@ -1,7 +0,0 @@
-#ifndef _EMAIL_H_
-#define _EMAIL_H_
-
-void            email_content_type(struct eps_t *, struct header_t *, void *);
-void            email_header_internal(struct eps_t *);
-
-#endif
--- indimail-2.4/eps-1.2/eps_int_buffer.h
+++
@@ -1,7 +0,0 @@
-#ifndef _INT_BUFFER_H_
-#define _INT_BUFFER_H_
-
-int             int_buffer_init(struct eps_t *, struct line_t *);
-void            int_buffer_restart(struct eps_t *, struct line_t *);
-
-#endif
