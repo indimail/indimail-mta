b7219ac248472ed8762c5ad6d04c7813fecb4dd6
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-03-19 23:09:29 +0530

    added ability to set SSLTLS options

--- indimail-2.5/set_mysql_options.c
+++ indimail-2.5/set_mysql_options.c
@@ -1,5 +1,8 @@
 /*
  * $Log: set_mysql_options.c,v $
+ * Revision 2.11  2018-03-19 23:08:51+05:30  Cprogrammer
+ * added ability to set SSLTLS options
+ *
  * Revision 2.10  2018-02-18 16:08:51+05:30  Cprogrammer
  * use mysql_optionsv() with mariadb
  *
@@ -35,7 +38,7 @@
 #include "indimail.h"
 
 #ifndef	lint
-static char     sccsid[] = "$Id: set_mysql_options.c,v 2.10 2018-02-18 16:08:51+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsid[] = "$Id: set_mysql_options.c,v 2.11 2018-03-19 23:08:51+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 int
@@ -52,11 +55,11 @@ int
 set_mysql_options(MYSQL *mysql, char *file, char *group, unsigned int *flags)
 {
 	char           *default_file, *default_group, *c_timeout, 
-				   *r_timeout, *w_timeout, *init_cmd,
+				   *r_timeout, *w_timeout, *init_cmd, *ptr,
 				   *set_client_ip, *opt_reconnect, *opt_protocol;
 	char            temp[4];
-	char            o_reconnect;
-	unsigned int    protocol, connect_timeout, read_timeout, write_timeout;
+	char            o_reconnect, tmpv_c;
+	unsigned int    protocol, connect_timeout, read_timeout, write_timeout, ssl_mode;
 
 	*flags = 0;
 	if (getenv("CLIENT_COMPRESS"))
@@ -103,6 +106,102 @@ set_mysql_options(MYSQL *mysql, char *file, char *group, unsigned int *flags)
 	 * MYSQL_PROTOCOL_PIPE, MYSQL_PROTOCOL_MEMORY
 	 */
 	if (int_mysql_options(mysql, MYSQL_OPT_PROTOCOL, (char *) &protocol))
+		return (1);
+
+	/*- SSL options */
+	
+	/*-
+	 * MYSQL_OPT_SSL_CA - The path name of the Certificate Authority (CA) certificate file.
+	 * This option, if used, must specify the same certificate used by the server.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CA")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CA, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_CAPATH
+	 * The path name of the directory that contains trusted SSL CA certificate files.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CAPATH")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CAPATH, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_CERT
+	 * The path name of the client public key certificate file.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CERT")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CERT, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_CIPHER
+	 * The list of permitted ciphers for SSL encryption.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CIPHER")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CIPHER, ptr))
+		return (1);
+	/*
+	 * MYSQL_OPT_SSL_CRL (argument type: char *)
+	 * The path name of the file containing certificate revocation lists.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CRL")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CRL, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_CRLPATH (argument type: char *)
+	 * The path name of the directory that contains files containing certificate revocation lists.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_CRLPATH")) && int_mysql_options(mysql, MYSQL_OPT_SSL_CRLPATH, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_ENFORCE (argument type: my_bool *)
+	 *
+	 * Whether to require the connection to use SSL. If enabled and an encrypted
+	 * connection cannot be established, the connection attempt fails.
+	 * This option is deprecated as of MySQL 5.7.11 and is removed in MySQL 8.0.
+	 * Instead, use MYSQL_OPT_SSL_MODE with a value of SSL_MODE_REQUIRED.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_ENFORCE")) && int_mysql_options(mysql, MYSQL_OPT_SSL_ENFORCE, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_SSL_VERIFY_SERVER_CERT (argument type: my_bool *)
+	 * Enable or disable verification of the server's Common Name identity in its certificate
+	 * against the host name used when connecting to the server. The connection is rejected
+	 * if there is a mismatch. For encrypted connections, this feature can be used to prevent
+	 * man-in-the-middle attacks. Identity verification is disabled by default.
+	 * This option does not work with self-signed certificates, which do not contain the
+	 * server name as the Common Name value.
+	 *
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_VERIFY_SERVER_CERT"))) {
+		tmpv_c = atoi(ptr) ? 1 : 0;
+		if (int_mysql_options(mysql, MYSQL_OPT_SSL_VERIFY_SERVER_CERT, tmpv_c))
+			return (1);
+	}
+	/*-
+	 * MYSQL_OPT_SSL_MODE (argument type: unsigned int *)
+	 *
+	 * The security state to use for the connection to the server: 
+	 * SSL_MODE_DISABLED, SSL_MODE_PREFERRED, SSL_MODE_REQUIRED, SSL_MODE_VERIFY_CA,
+	 * SSL_MODE_VERIFY_IDENTITY.
+	 * The default is SSL_MODE_PREFERRED. These modes are the permitted values of
+	 * the mysql_ssl_mode enumeration defined in mysql.h.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_MODE"))) {
+		ssl_mode = atoi(ptr);
+		if (int_mysql_options(mysql, MYSQL_OPT_SSL_MODE, &ssl_mode))
+			return (1);
+	}
+	/*-
+	 * MYSQL_OPT_SSL_KEY (argument type: char *)
+	 * The path name of the client private key file.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_SSL_KEY")) && int_mysql_options(mysql, MYSQL_OPT_SSL_KEY, ptr))
+		return (1);
+	/*-
+	 * MYSQL_OPT_TLS_VERSION (argument type: char *)
+	 * The protocols permitted by the client for encrypted connections.
+	 * The value is a comma-separated list containing one or more protocol names.
+	 * The protocols that can be named for this option depend on the SSL library
+	 * used to compile MySQL. 
+	 *
+	 * When compiled using OpenSSL 1.0.1 or higher, MySQL supports the TLSv1, TLSv1.1, and TLSv1.2 protocols.
+	 * When compiled using the bundled version of yaSSL, MySQL supports the TLSv1 and TLSv1.1 protocols.
+	 */
+	if ((ptr = getenv("MYSQL_OPT_TLS_VERSION")) && int_mysql_options(mysql, MYSQL_OPT_TLS_VERSION, ptr))
 		return (1);
 	return (0);
 }
