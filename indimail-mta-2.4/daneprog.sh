7a658f95d8dadd5aff2f6abc5451df8c55a58175
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-04-25 22:58:03 +0530

    DNS resource record checker

---
+++ indimail-mta-2.4/daneprog.sh
@@ -0,0 +1,34 @@
+#
+# $Log: daneprog.sh,v $
+# Revision 1.1  2018-04-25 22:57:23+05:30  Cprogrammer
+# Initial revision
+#
+#
+# check DNS resource records for the DANE protocol.
+#
+# $ID: $
+#
+if [ ! -x /usr/bin/host ] ; then
+	echo "/usr/bin/host from bind-utils package not found" 1>&2
+	exit 111
+elif [ ! -x /usr/bin/danetool ] ; then
+	echo "/usr/bin/danetool from gnutls-utils package not found" 1>&2
+	exit 111
+fi
+if [ -x /usr/bin/ipcalc ] ; then
+	/usr/bin/ipcalc -c $1 >/dev/null 2>&1
+	if [ $? -eq 0 ] ; then
+		mx=$1
+	else
+		mx=`/usr/bin/host -tmx $1 | sort -g -k 6 | head -1 | awk '{print $7}' | sed 's/.$//'`
+	fi
+else
+	if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+		mx=$1
+	else
+		mx=`/usr/bin/host -tmx $1 | sort -g -k 6 | head -1 | awk '{print $7}' | sed 's/.$//'`
+	fi
+fi
+echo $mx
+cd /tmp
+exec /usr/bin/danetool --quiet --check $mx --proto tcp --starttls-proto=smtp
