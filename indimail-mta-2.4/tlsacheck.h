1415fb34d09f16cf4cc0289f3f887c3b44c596d3
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-04-26 11:40:42 +0530

    changed DNATIMEOUT to 10 sec

--- indimail-mta-2.4/tlsacheck.h
+++ indimail-mta-2.4/tlsacheck.h
@@ -1,5 +1,8 @@
 /*
  * $Log: tlsacheck.h,v $
+ * Revision 1.2  2018-04-26 11:40:23+05:30  Cprogrammer
+ * changed DANETIMEOUT to 10
+ *
  * Revision 1.1  2018-04-26 01:31:31+05:30  Cprogrammer
  * Initial revision
  *
@@ -10,7 +13,7 @@
 #define MAXDANEDATASIZE 2000
 #define DEFAULTDANEPORT 1998
 #define DEFAULTDANEIP   "127.0.0.1"
-#define DANETIMEOUT     3
+#define DANETIMEOUT     10
 
 int             tlsacheck(char *, char *, void (*)(), void (*)());
 
