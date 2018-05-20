27e779ed6bb450b730f53adfa97d64a561233574
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-05-20 23:11:07 +0530

    added mx query

--- danetlsa-1.0/tlsarr.c
+++ danetlsa-1.0/tlsarr.c
@@ -2,55 +2,183 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <getopt.h>
 #include "query-getdns.h"
 
 void
 cleanup(int status)
 {
-	freeaddrinfo(addresses);
-	free_tlsa(tlsa_rdata_list);
+	if (addresses) {
+		freeaddrinfo(addresses);
+		addresses = (addrinf *) 0;
+	}
+	if (tlsa_rdata_list) {
+		free_tlsa(tlsa_rdata_list);
+		tlsa_rdata_list = (tlsa_rdata *) 0;
+	}
+	if (mx_rdata_list) {
+		free_mx(mx_rdata_list);
+		mx_rdata_list = (mx_rdata *) 0;
+	}
 	exit (status);
+}
+
+void
+usage()
+{
+	fprintf(stderr, "usage: tlsarr [-m] [-p port] host\n");
+	fprintf(stderr, "         -m       query MX records for host\n");
+	fprintf(stderr, "         -p port  port to connect to\n");
+	fprintf(stderr, "         -s       stub mode\n");
+	exit (1);
 }
 
 int
 main(int argc, char **argv)
 {
-    char *cp;
-    tlsa_rdata *rp;
+    char           *cp, *host, *mxhost;
+    tlsa_rdata     *rp;
+    mx_rdata       *mp;
+	int             i, c, err, verbose = 0, recursive = 1, query_mx = 0, port = 25;
 
-	if (argc != 2) {
-		fprintf(stderr, "tlsarr mxhost\n");
-		return (1);
+	while ((c = getopt(argc, argv, "svmp:")) != -1) {
+		switch (c)
+		{
+		case 'v':
+			verbose = 1;
+			break;
+		case 's':
+			recursive = 0;
+			break;
+		case 'p':
+			port = atoi(optarg);
+			break;
+		case 'm':
+			query_mx = 1;
+			break;
+		default:
+			usage();
+		}
 	}
-	do_dns_queries(argv[1], 25, 1);
-	if (dns_bogus_or_indeterminate) {
-		fprintf(stderr, "DNSSEC status of responses is bogus or indeterminate.\n");
-		cleanup(1);
+	if (optind < argc)
+		host = argv[optind++];
+	else
+		usage();
+	if (query_mx) {
+		if (verbose) {
+			printf("doing MX query for %s\n", host);
+			fflush(stdout);
+		}
+		if ((err = do_mx_queries(host, recursive)))  {
+			printf("do_mx_query error %d\n", err);
+			print_stack(0);
+			return (1);
+		}
+		if (verbose) {
+			printf("MX query finished\n");
+			fflush(stdout);
+		}
+		if (mx_rdata_list) {
+			fprintf(stdout, "MX records found: %ld\n", mx_count);
+			fflush(stdout);
+			for (i = 1, mp = mx_rdata_list; mp != NULL; mp = mp->next, i++) {
+				if (!(cp = bin2txt(mp->data, mp->data_len))) {
+					fprintf(stderr, "out of memory\n");
+					continue;
+				}
+				fprintf(stdout, "MX[%d]: %d %s\n", i, mp->pref, cp);
+				fflush(stdout);
+				if (verbose) {
+					printf("doing TLSA query for %s\n", cp);
+					fflush(stdout);
+				}
+				if ((err = do_dns_queries(cp, port, recursive))) {
+					free(cp);
+					if (dns_bogus_or_indeterminate) {
+						fprintf(stderr, "DNSSEC status of responses is bogus or indeterminate.\n");
+						cleanup(1);
+					}
+					if (!addresses) {
+						fprintf(stderr, "No address records found\n");
+						cleanup(1);
+					}
+					print_stack(0);
+					return (err);
+				} 
+				free(cp);
+				if (verbose) {
+					printf("TLSA query finished\n");
+					fflush(stdout);
+				}
+			}
+			if (tlsa_rdata_list) {
+				if (!tlsa_authenticated) {
+					fprintf(stderr, "Insecure TLSA records.\n");
+					cleanup(1);
+				} else
+				if (!v4_authenticated || !v6_authenticated) {
+					fprintf(stderr, "Insecure Address records.\n");
+					cleanup(1);
+				}
+				fprintf(stdout, "TLSA records found: %ld\n", tlsa_count);
+				fflush(stdout);
+				for (i = 1, rp = tlsa_rdata_list; rp != NULL; rp = rp->next, i++) {
+					if (!(cp = bin2hexstring(rp->data, rp->data_len))) {
+						fprintf(stderr, "out of memory\n");
+						continue;
+					}
+					fprintf(stdout, "TLSA[%d][%s]: %d %d %d %s\n", i, rp->host, rp->usage, rp->selector, rp->mtype, cp);
+					fflush(stdout);
+					free(cp);
+				}
+			} else
+				fprintf(stderr, "No TLSA records found.\n");
+		}
+		return (0);
+	} else
+		mxhost = host;
+	if (verbose) {
+		printf("doing TLSA query for %s\n", mxhost);
+		fflush(stdout);
 	}
-	if (!addresses) {
-		fprintf(stderr, "No address records found, exiting[%d].\n", tlsa_count);
-		cleanup(0);
-	}
-	if (!tlsa_rdata_list) {
+	if ((err = do_dns_queries(mxhost, port, recursive))) {
+		if (dns_bogus_or_indeterminate) {
+			fprintf(stderr, "DNSSEC status of responses is bogus or indeterminate.\n");
+			cleanup(1);
+		}
+		if (!addresses) {
+			fprintf(stderr, "No address records found\n");
+			cleanup(1);
+		}
+		print_stack(0);
+		return (err);
+	} else
+	if (tlsa_rdata_list) {
+		if (!tlsa_authenticated) {
+			fprintf(stderr, "Insecure TLSA records.\n");
+			cleanup(1);
+		} else
+		if (!v4_authenticated || !v6_authenticated) {
+			fprintf(stderr, "Insecure Address records.\n");
+			cleanup(1);
+		}
+		fprintf(stdout, "TLSA records found: %ld\n", tlsa_count);
+		fflush(stdout);
+		for (i = 1, rp = tlsa_rdata_list; rp != NULL; rp = rp->next, i++) {
+			if (!(cp = bin2hexstring(rp->data, rp->data_len))) {
+				fprintf(stderr, "out of memory\n");
+				continue;
+			}
+			fprintf(stdout, "TLSA[%d][%s]: %d %d %d %s\n", i, rp->host, rp->usage, rp->selector, rp->mtype, cp);
+			fflush(stdout);
+			free(cp);
+		}
+	} else
 		fprintf(stderr, "No TLSA records found.\n");
-		cleanup(0);
-	} else
-	if (!tlsa_authenticated) {
-		fprintf(stderr, "Insecure TLSA records.\n");
-		cleanup(0);
-	} else
-	if (!v4_authenticated || !v6_authenticated) {
-		fprintf(stderr, "Insecure Address records.\n");
-		cleanup(0);
+	if (verbose) {
+		printf("TLSA query finished\n");
+		fflush(stdout);
 	}
-    if (tlsa_rdata_list) {
-        fprintf(stdout, "TLSA records found: %ld\n", tlsa_count);
-        for (rp = tlsa_rdata_list; rp != NULL; rp = rp->next) {
-            fprintf(stdout, "TLSA: %d %d %d %s\n", rp->usage, rp->selector,
-                    rp->mtype, (cp = bin2hexstring(rp->data, rp->data_len)));
-            free(cp);
-        }
-    }
 	cleanup(0);
 	return 0;
 }
