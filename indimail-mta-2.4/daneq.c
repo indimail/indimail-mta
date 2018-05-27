5f200f5a2758fa8c6ed018d2ab8d43f1a052fd95
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-05-27 17:47:50 +0530

    added option for qmail-remote to query/update records

--- indimail-mta-2.4/daneq.c
+++ indimail-mta-2.4/daneq.c
@@ -1,5 +1,8 @@
 /*
  * $Log: daneq.c,v $
+ * Revision 1.2  2018-05-27 17:47:16+05:30  Cprogrammer
+ * added option for qmail-remote to query/update records
+ *
  * Revision 1.1  2018-04-26 11:37:56+05:30  Cprogrammer
  * Initial revision
  *
@@ -48,17 +51,61 @@ err_tmpfail(char *arg)
 	return;
 }
 
-char           *usage = "usage: daneq -d domain ipaddr";
+void
+print_status(char status)
+{
+	switch (status)
+	{
+	case RECORD_NEW:
+		out("RECORD NEW");
+		break;
+	case RECORD_WHITE:
+		out("RECORD WHITE");
+		break;
+	case RECORD_NOVRFY:
+		out("RECORD NOVERIFY");
+		break;
+	case RECORD_OK:
+		out("RECORD OK");
+		break;
+	case RECORD_OLD:
+		out("RECORD OLD");
+		break;
+	case RECORD_FAIL:
+		out("RECORD FAIL");
+		break;
+	default:
+		out("RECORD UNKNOWN");
+		break;
+	}
+	return;
+}
+
+char           *usage =
+				"usage: daneq [qsf] -d domain ipaddr\n"
+				"        -q (query mode for record check)\n"
+				"        -S (update mode success)\n"
+				"        -F (update mode failure)";
 
 int
 main(int argc, char **argv)
 {
 	char           *domain = 0;
-	int             opt;
+	int             opt, query_or_update = 0;
+	char            rbuf[2];
 
-	while ((opt = getopt(argc, argv, "d:")) != opteof) {
+	while ((opt = getopt(argc, argv, "qSFd:")) != opteof) {
 		switch (opt)
 		{
+		case 'q':
+			query_or_update = 1;
+			break;
+		case 'S':
+			query_or_update = 2;
+			break;
+		case 'F':
+			query_or_update = 3;
+			break;
 		case 'd':
 			domain = optarg;
 			break;
@@ -66,16 +113,32 @@ main(int argc, char **argv)
 	}
 	if (optind + 1 != argc)
 		strerr_die1x(100, usage);
-	switch (tlsacheck(argv[optind], domain, timeoutfn, err_tmpfail))
+	switch (tlsacheck(argv[optind], domain, query_or_update, rbuf, timeoutfn, err_tmpfail))
 	{
-		case 1:/*- success */
-			out("success\n");
-			flush();
-			return (0);
 		case 0:
-			out("tlsa verification failed\n");
+			out(query_or_update ? "does not exist " : "tlsa verification failed ");
+			print_status(rbuf[1]);
+			out("\n");
 			flush();
 			return (1);
+		case 1:/*- success */
+			out(query_or_update ? "exists " : "success ");
+			print_status(rbuf[1]);
+			out("\n");
+			flush();
+			return (0);
+		case 2: /*- update failed */
+			out("update failed ");
+			print_status(rbuf[1]);
+			out("\n");
+			flush();
+			return (1);
+		case 3: /*- update succeeded */
+			out("update succeeded ");
+			print_status(rbuf[1]);
+			out("\n");
+			flush();
+			break;
 		case -1:
 			out("system error\n");
 			flush();
@@ -93,7 +156,7 @@ main(int argc, char **argv)
 void
 getversion_daneq_c()
 {
-	static char    *x = "$Id: daneq.c,v 1.1 2018-04-26 11:37:56+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: daneq.c,v 1.2 2018-05-27 17:47:16+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
