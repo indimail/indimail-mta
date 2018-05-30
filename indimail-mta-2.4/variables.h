40f6faf8b7c8dc023ad1cd0852c8784ffabc38fa
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-05-30 23:28:59 +0530

    moved noipv6 to variables.c

--- indimail-mta-2.4/variables.h
+++ indimail-mta-2.4/variables.h
@@ -1,5 +1,8 @@
 /*
  * $Log: variables.h,v $
+ * Revision 1.5  2018-05-30 23:26:52+05:30  Cprogrammer
+ * moved noipv6 variable to variables.c
+ *
  * Revision 1.4  2017-03-21 15:40:45+05:30  Cprogrammer
  * added certdir variable
  *
@@ -14,5 +17,6 @@ extern char    *queuedir;
 extern char    *controldir;
 extern char    *certdir;
 extern int      use_fsync;
+extern int      noipv6;
 
 #endif
