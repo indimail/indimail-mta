cfeebda6bdc219f846b8515cf5715b4a1dff734f
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-09-02 14:25:39 +0530

    fixed syntax error

--- indimail-mta-2.7/qlocal_upgrade.sh
+++ indimail-mta-2.7/qlocal_upgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # $Log: qlocal_upgrade.sh,v $
+# Revision 1.15  2018-09-02 14:25:02+05:30  Cprogrammer
+# fixed syntax error
+#
 # Revision 1.14  2018-07-15 13:58:08+05:30  Cprogrammer
 # create env variable ROUTE_NULL_USER, LOCK_LOGS for qmail-send
 #
@@ -40,7 +43,7 @@
 # Initial revision
 #
 #
-# $Id: qlocal_upgrade.sh,v 1.14 2018-07-15 13:58:08+05:30 Cprogrammer Exp mbhangui $
+# $Id: qlocal_upgrade.sh,v 1.15 2018-09-02 14:25:02+05:30 Cprogrammer Exp mbhangui $
 #
 PATH=/bin:/usr/bin:/usr/sbin:/sbin
 chown=$(which chown)
@@ -65,7 +68,7 @@ check_update_if_diff()
 do_post_upgrade()
 {
 date
-echo "Running $1 - $Id: qlocal_upgrade.sh,v 1.14 2018-07-15 13:58:08+05:30 Cprogrammer Exp mbhangui $"
+echo "Running $1 - $Id: qlocal_upgrade.sh,v 1.15 2018-09-02 14:25:02+05:30 Cprogrammer Exp mbhangui $"
 if [ -x /bin/systemctl -o -x /usr/bin/systemctl ] ; then
   systemctl is-enabled svscan >/dev/null 2>&1
   if [ $? -ne 0 ] ; then
@@ -148,7 +151,7 @@ do
 		check_update_if_diff /service/$i/variables/SOFT_MEM 536870912
 	fi
 	if [ "$i" = "qmail-send.25" ] ; then
-		if [ -f /etc/indimail/control/virtualdomains && ! -f /service/$i/variables/ROUTE_NULL_USER ] ; then
+		if [ -f /etc/indimail/control/virtualdomains -a ! -f /service/$i/variables/ROUTE_NULL_USER ] ; then
 			echo > /service/$i/variables/ROUTE_NULL_USER
 		fi
 		if [ ! -f /service/$i/variables/LOCK_LOGS ] ; then
