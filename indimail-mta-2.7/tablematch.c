2de93b5988dcc806100df80e5b2d20c11b91e1a1
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2018-11-29 15:24:09 +0530

    use HOSTACCESS environment variable as alternate filename for hostaccess control file

--- indimail-mta-2.7/tablematch.c
+++ indimail-mta-2.7/tablematch.c
@@ -1,5 +1,8 @@
 /*
  * $Log: tablematch.c,v $
+ * Revision 1.8  2018-11-29 15:23:28+05:30  Cprogrammer
+ * use HOSTACCESS environment variable as alternate filename for hostaccess control file
+ *
  * Revision 1.7  2018-05-29 22:15:34+05:30  Cprogrammer
  * removed call to gethostbyname() in ipv6 code
  *
@@ -84,8 +87,7 @@ matchinet(char *ip, char *token, char flag)
 	if (inet_addr(token) != INADDR_NONE)
 		return (0);
 	else
-	for (match = idx1 = 0, ptr1 = token, ptr2 = ip; idx1 < 4 && match == idx1; idx1++)
-	{
+	for (match = idx1 = 0, ptr1 = token, ptr2 = ip; idx1 < 4 && match == idx1; idx1++) {
 		/*- IP Address in control file */
 		for (cptr = field1; *ptr1 && *ptr1 != '.'; *cptr++ = *ptr1++);
 		*cptr = 0;
@@ -100,8 +102,7 @@ matchinet(char *ip, char *token, char flag)
 		/*-
 		 * Network address wildcard match (i.e. "192.86.28.*")
 		 */
-		if (!str_diff(field1, "*"))
-		{
+		if (!str_diff(field1, "*")) {
 			match++;
 			continue;
 		}
@@ -109,21 +110,16 @@ matchinet(char *ip, char *token, char flag)
 		 * Network address wildcard match (i.e.
 		 * "192.86.2?.12?")
 		 */
-		for (; (ptr = my_strchr(field1, '?'));)
-		{
+		for (; (ptr = my_strchr(field1, '?'));) {
 			lnum = ptr - field1;
 			*ptr = field2[lnum];
 		}
-		if (!str_diff(field1, field2))
-		{
+		if (!str_diff(field1, field2)) {
 			match++;
 			continue;
 		}
-		/*
-		 * Range match (i.e. "190-193.86.22.11") 
-		 */
-		if ((ptr = my_strchr(field1, '-')))
-		{
+		/*- Range match (i.e. "190-193.86.22.11") */
+		if ((ptr = my_strchr(field1, '-'))) {
 			*ptr = 0;
 			ptr++;
 			scan_int(field1, &lnum);
@@ -132,10 +128,8 @@ matchinet(char *ip, char *token, char flag)
 			else
 				scan_int(ptr, &hnum);
 			scan_int(field2, &tmp);
-			for (idx2 = lnum; idx2 <= hnum; idx2++)
-			{
-				if (idx2 == tmp)
-				{
+			for (idx2 = lnum; idx2 <= hnum; idx2++) {
+				if (idx2 == tmp) {
 					match++;
 					break;
 				}
@@ -146,8 +140,7 @@ matchinet(char *ip, char *token, char flag)
 	} /*- for (match = idx1 = 0, ptr1 = token, ptr2 = ip; idx1 < 4 && match == idx1; idx1++) */
 	if (match == 4)
 		return (1);
-	if (flag)
-	{
+	if (flag) {
 		/*
 		 * If our token is a host name then translate it and compare internet
 		 * addresses
@@ -194,12 +187,12 @@ matchinet(char *ip, char *token, char flag)
  * *:202.133.22.1
  * yahoo.com:202.144.77.5
  * abc.com:202.10-33.54.5
- * indi.com:*
+ * indimail.org:*
  * - All domains allwed from 202.133.22.1 (no domain restriction for
  *   202.133.22.1)
  * - yahoo.com allowed from 202.144.77.5
  * - abc.com is allowed from a ip range 202.10.54.5 to 202.33.54.5
- * - indi.com is allowed from any ip address (no IP restriction for indi.com)
+ * - indimail.org is allowed from any ip address (no IP restriction for indimail.org)
  */
 int
 tablematch(char *filename, char *ip, char *domain)
@@ -208,27 +201,13 @@ tablematch(char *filename, char *ip, char *domain)
 	char           *ptr, *cptr;
 	static stralloc hostacc = { 0 };
 
-	if ((ptr = env_get("HOSTACCESS")))
-	{
-		len = str_len(ptr);
-		if (*(ptr + len - 1) == '\n')
-			len--;
-		for (;*ptr;ptr++)
-		{
-			if (*ptr == '\n')
-				*ptr = 0;
-		}
-		if (!stralloc_copyb(&hostacc, ptr, len))
-			return(-2);
-	} else
-	{
-		if ((count = control_readfile(&hostacc, filename, 0)) == -1)
-			return (-1);
-		if (!count) /*- allow access if control file is absent */
-			return (1);
-	}
-	for (domain_match = ip_match = len = 0, ptr = hostacc.s; len < hostacc.len;)
-	{
+	if (!(ptr = env_get("HOSTACCESS")))
+		ptr = filename;
+	if ((count = control_readfile(&hostacc, ptr, 0)) == -1)
+		return (-1);
+	if (!count) /*- allow access if control file is absent */
+		return (1);
+	for (domain_match = ip_match = len = 0, ptr = hostacc.s; len < hostacc.len;) {
 		len += (str_len(ptr) + 1);
 		for (cptr = ptr; *cptr && *cptr != ':'; cptr++);
 		if (*cptr == ':') /*- cptr + 1 is the IP address token */
@@ -241,17 +220,15 @@ tablematch(char *filename, char *ip, char *domain)
 			nullflag = 0;
 		/*
 		 * If a sender domain comes from specific IPs then
-		 * It also means that yahoo.com cannot come from those
-		 * specific IPs.
+		 * It also means that some other domain cannot come
+		 * from those specific IPs.
 		 */
 		dmatch = imatch = 0;
-		if (nullflag || (*domain && (!str_diff("*", ptr) || !str_diff(domain + 1, ptr))))
-		{
+		if (nullflag || (*domain && (!str_diff("*", ptr) || !str_diff(domain + 1, ptr)))) {
 			dmatch = 1;
 			domain_match = str_diff(ptr, "*") ? 1 : 0; /* matched on domain & not on wildcard */
 		}
-		if (!str_diff(cptr + 1, "*") || !str_diff(cptr + 1, "*.*.*.*") || matchinet(ip, cptr + 1, 0))
-		{
+		if (!str_diff(cptr + 1, "*") || !str_diff(cptr + 1, "*.*.*.*") || matchinet(ip, cptr + 1, 0)) {
 			imatch = 1;
 			ip_match = (str_diff(cptr + 1, "*") && str_diff(cptr + 1, "*.*.*.*")) ? 1 : 0;
 		}
@@ -278,7 +255,7 @@ tablematch(char *filename, char *ip, char *domain)
 void
 getversion_tablematch_c()
 {
-	static char    *x = "$Id: tablematch.c,v 1.7 2018-05-29 22:15:34+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: tablematch.c,v 1.8 2018-11-29 15:23:28+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
