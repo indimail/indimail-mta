32c309201eb427c4290fe4bcebefcb4e3a729f75
Manvendra Bhangui <mbhangui@users.sourceforge.net>
2019-03-07 00:52:49 +0530

    added regcomp error cases for documentation

--- indimail-mta-2.7/matchregex.c
+++ indimail-mta-2.7/matchregex.c
@@ -1,5 +1,8 @@
 /*
  * $Log: matchregex.c,v $
+ * Revision 1.3  2019-03-07 00:52:18+05:30  Cprogrammer
+ * added regcomp error cases for documentation
+ *
  * Revision 1.2  2015-07-23 15:01:42+05:30  Cprogrammer
  * fixed comparision of constant '-1' with 0
  *
@@ -25,8 +28,7 @@ matchregex(char *text, char *regex, char **errStr)
 	if (errStr)
 		*errStr = 0;
 	/*- build the regex */
-	if ((retval = REGCOMP(qreg, regex)) != 0)
-	{
+	if ((retval = REGCOMP(qreg, regex)) != 0) {
 		regerror(retval, &qreg, errbuf, sizeof(errbuf));
 		regfree(&qreg);
 		if (!stralloc_copys(&err_str, text))
@@ -43,7 +45,25 @@ matchregex(char *text, char *regex, char **errStr)
 			return (AM_MEMORY_ERR);
 		if (errStr)
 			*errStr = err_str.s;
-		return (AM_REGEX_ERR);
+		switch (retval)
+		{
+			case REG_BADBR:
+			case REG_BADPAT:
+			case REG_BADRPT:
+			case REG_EBRACE:
+			case REG_EBRACK:
+			case REG_ECOLLATE:
+			case REG_ECTYPE:
+			case REG_EEND:
+			case REG_EESCAPE:
+			case REG_EPAREN:
+			case REG_ERANGE:
+			case REG_ESIZE:
+			case REG_ESPACE:
+			case REG_ESUBREG:
+			default:
+				return (AM_REGEX_ERR);
+		}
 	}
 	/*- execute the regex */
 #define REGEXEC(X,Y)    regexec(&X, Y, (size_t) 0, (regmatch_t *) 0, (int) 0)
@@ -55,7 +75,7 @@ matchregex(char *text, char *regex, char **errStr)
 void
 getversion_matchregex_c()
 {
-	static char    *x = "$Id: matchregex.c,v 1.2 2015-07-23 15:01:42+05:30 Cprogrammer Stab mbhangui $";
+	static char    *x = "$Id: matchregex.c,v 1.3 2019-03-07 00:52:18+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
