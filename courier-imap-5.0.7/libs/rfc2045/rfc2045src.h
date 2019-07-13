4cd3a25dc5e16f9ea5e435bdf193b340765df3e3
Manvendra Bhangui <mbhangui@gmail.com>
2019-07-14 00:13:08 +0530

    moved to indimail-virt repo

--- courier-imap-5.0.7/libs/rfc2045/rfc2045src.h
+++
@@ -1,2 +0,0 @@
-#define SRC_SEEK(src,pos) ((*(src)->seek_func)((pos), (src)->arg))
-#define SRC_READ(src,buf,cnt) ((*(src)->read_func)((buf),(cnt),(src)->arg))
--- courier-imap-5.0.7/libs/waitlib/testwait2.c
+++
@@ -1,9 +0,0 @@
-/*
-** Copyright 1998 - 1999 Double Precision, Inc.
-** See COPYING for distribution information.
-*/
-
-
-void foobar()
-{
-}
