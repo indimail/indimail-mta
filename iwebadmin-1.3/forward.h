44377da1e37efe0b96b9eddb7419db6d97d84458
Manvendra Bhangui <mbhangui@gmail.com>
2019-07-14 00:12:35 +0530

    moved to indimail-virt repo

--- iwebadmin-1.3/forward.h
+++
@@ -1,6 +0,0 @@
-/*
- * $Id: forward.h,v 1.1 2010-04-26 12:07:49+05:30 Cprogrammer Exp mbhangui $
- */
-
-int             show_forwards(char *user, char *dom, time_t mytime);
-void            count_forwards();
