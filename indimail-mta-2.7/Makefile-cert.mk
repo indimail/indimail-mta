3abb608b3de212957e8a09355456cc9f9bf18445
Manvendra Bhangui <mbhangui@gmail.com>
2019-07-16 00:59:18 +0530

    fixed certificate paths

--- indimail-mta-2.7/Makefile-cert.mk
+++ indimail-mta-2.7/Makefile-cert.mk
@@ -1,21 +1,19 @@
 cert-req: req.pem
-cert cert-req: SYSCONFDIR/control/clientcert.pem
+cert cert-req: SYSCONFDIR/certs/clientcert.pem
 	@:
 
-SYSCONFDIR/control/clientcert.pem: SYSCONFDIR/control/servercert.pem
-	cd SYSCONFDIR/control;ln -s servercert.pem clientcert.pem
+SYSCONFDIR/certs/clientcert.pem: SYSCONFDIR/certs/servercert.pem
+	cd SYSCONFDIR/certs;ln -s servercert.pem clientcert.pem
 
-SYSCONFDIR/control/servercert.pem:
-	PATH=$$PATH:/usr/local/ssl/bin \
-		openssl req -new -x509 -nodes -days 366 -out $@ -keyout $@
+SYSCONFDIR/certs/servercert.pem:
+	openssl req -new -x509 -nodes -days 366 -out $@ -keyout $@
 	chmod 640 $@
 	chown indimail:indimail $@
 
 req.pem:
-	PATH=$$PATH:/usr/local/ssl/bin openssl req \
-		-new -nodes -out $@ -keyout SYSCONFDIR/control/servercert.pem
-	chmod 640 SYSCONFDIR/control/servercert.pem
-	chown indimail:indimail SYSCONFDIR/control/servercert.pem
+	openssl req -new -nodes -out $@ -keyout SYSCONFDIR/certs/servercert.pem
+	chmod 640 SYSCONFDIR/certs/servercert.pem
+	chown indimail:indimail SYSCONFDIR/certs/servercert.pem
 	@echo
 	@echo "Send req.pem to your CA to obtain signed_req.pem, and do:"
-	@echo "cat signed_req.pem >> SYSCONFDIR/control/servercert.pem"
+	@echo "cat signed_req.pem >> SYSCONFDIR/certs/servercert.pem"
