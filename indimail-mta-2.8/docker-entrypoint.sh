3de6ea17a4b6fb8062973ab571dad0c3fea9020c
mbhangui <mbhangui@gmail.com>
2020-03-20 15:09:27 +0530

    docker-entrypoint.sh: display command being run during docker entrypoint

--- indimail-mta-2.8/docker-entrypoint.sh
+++ indimail-mta-2.8/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #
-# $Id: docker-entrypoint.sh,v 1.1 2019-12-08 18:07:36+05:30 Cprogrammer Exp mbhangui $
+# $Id: docker-entrypoint.sh,v 1.2 2020-03-20 15:08:32+05:30 Cprogrammer Exp mbhangui $
 #
 # $Log: docker-entrypoint.sh,v $
+# Revision 1.2  2020-03-20 15:08:32+05:30  Cprogrammer
+# display command being run during docker entrypoint
+#
 # Revision 1.1  2019-12-08 18:07:36+05:30  Cprogrammer
 # Initial revision
 #
@@ -12,12 +15,15 @@ indimail)
 	if [ -f /service/mysql.3306/down -a -d /var/indimail/mysqldb/data/indimail ] ; then
 		/bin/rm -f /service/mysql.3306/down
 	fi
+   	echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
    	exec PREFIX/sbin/svscan /service
 ;;
 indimail-mta|svscan)
+   	echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
    	exec PREFIX/sbin/svscan /service
 ;;
 *)
+	echo "docker-entrypoint: executing $@"
 	exec "$@"
 ;;
 esac
