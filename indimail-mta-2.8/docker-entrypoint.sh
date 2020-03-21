211b618c706244401207a7c3f7fff34a2e2e918d
mbhangui <mbhangui@gmail.com>
2020-03-21 23:41:48 +0530

    use envdir to set env variables in /service/.svscan/variables

--- indimail-mta-2.8/docker-entrypoint.sh
+++ indimail-mta-2.8/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #
-# $Id: docker-entrypoint.sh,v 1.2 2020-03-20 15:08:32+05:30 Cprogrammer Exp mbhangui $
+# $Id: docker-entrypoint.sh,v 1.3 2020-03-21 23:41:17+05:30 Cprogrammer Exp mbhangui $
 #
 # $Log: docker-entrypoint.sh,v $
+# Revision 1.3  2020-03-21 23:41:17+05:30  Cprogrammer
+# use envdir to set env variables in /service/.svscan/variables
+#
 # Revision 1.2  2020-03-20 15:08:32+05:30  Cprogrammer
 # display command being run during docker entrypoint
 #
@@ -15,12 +18,22 @@ indimail)
 	if [ -f /service/mysql.3306/down -a -d /var/indimail/mysqldb/data/indimail ] ; then
 		/bin/rm -f /service/mysql.3306/down
 	fi
-   	echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
-   	exec PREFIX/sbin/svscan /service
+	if [ -d /service/.svscan/variables ] ; then
+   		echo "docker-entrypoint: [$1] PREFIX/bin/envdir /service/.svscan/variables PREFIX/sbin/svscan /service"
+   		exec PREFIX/bin/envdir /service/.svscan/variables PREFIX/sbin/svscan /service
+	else
+   		echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
+   		exec PREFIX/sbin/svscan /service
+	fi
 ;;
 indimail-mta|svscan)
-   	echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
-   	exec PREFIX/sbin/svscan /service
+	if [ -d /service/.svscan/variables ] ; then
+   		echo "docker-entrypoint: [$1] PREFIX/bin/envdir /service/.svscan/variables PREFIX/sbin/svscan /service"
+   		exec PREFIX/bin/envdir /service/.svscan/variables PREFIX/sbin/svscan /service
+	else
+   		echo "docker-entrypoint: [$1] executing PREFIX/sbin/svscan /service"
+   		exec PREFIX/sbin/svscan /service
+	fi
 ;;
 *)
 	echo "docker-entrypoint: executing $@"
