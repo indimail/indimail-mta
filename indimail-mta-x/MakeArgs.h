f2e3b6102d83fe452bba97e38af756a68472f73d
mbhangui <mbhangui@gmail.com>
2020-04-01 16:08:33 +0530

    header for MakeArgs() function

---
+++ indimail-mta-x/MakeArgs.h
@@ -0,0 +1,13 @@
+/*
+ * $Log: MakeArgs.h,v $
+ * Revision 1.1  2020-04-01 16:07:53+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef MAKEARGS_H
+#define MAKEARGS_H
+
+char          **MakeArgs(char *);
+void            FreeMakeArgs(char **);
+
+#endif
