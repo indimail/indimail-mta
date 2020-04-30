09d4bc29378f1920c9c5824ca09f13d8ec214af0
mbhangui <mbhangui@gmail.com>
2020-04-30 18:09:37 +0530

    removed rwhconfig_err variable to prevent variable getting defined multiple times on inclusion

--- indimail-mta-x/rwhconfig.h
+++ indimail-mta-x/rwhconfig.h
@@ -1,5 +1,8 @@
 /*
  * $Log: rwhconfig.h,v $
+ * Revision 1.3  2020-04-30 18:08:52+05:30  Cprogrammer
+ * removed rwhconfig_err variable.
+ *
  * Revision 1.2  2004-10-11 14:01:35+05:30  Cprogrammer
  * added function prototypes
  *
@@ -12,8 +15,6 @@
 
 #include "strerr.h"
 #include "sconfig.h"
-
-struct strerr   rwhconfig_err;
 
 int             rwhconfig(config_str *, stralloc *);
 
