5bd44c1bb62d7b1ad75e3c512dfcd95d69bd419b
Manvendra Bhangui <mbhangui@gmail.com>
2020-07-05 08:55:14 +0530

    make-load: use LDFLAGS when linking

--- indimail-mta-x/make-load.sh
+++ indimail-mta-x/make-load.sh
@@ -1,10 +1,11 @@
 echo 'main="$1"; shift'
 set $LD
+ld_args=$1
 shift
 echo "if [ -n \"\$CC\" ] ; then"
-echo "  " echo \""\$CC" $* '-o "$main" "$main".o ${1+"$@"}'\"
-echo "  " exec "\$CC" $* '-o "$main" "$main".o ${1+"$@"}'
+echo "  " echo \""\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'\"
+echo "  " exec   "\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'
 echo "else"
-echo "  " echo \""$LD" '-o "$main" "$main".o ${1+"$@"}'\"
-echo "  " exec "$LD" '-o "$main" "$main".o ${1+"$@"}'
+echo "  " echo \""$ld_args" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'\"
+echo "  " exec   "$ld_args" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'
 echo "fi"
