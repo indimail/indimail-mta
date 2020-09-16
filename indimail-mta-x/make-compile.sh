ca83e3e3717d88cdee8d8c6f555b84f8d33487fc
Manvendra Bhangui <mbhangui@gmail.com>
2020-09-16 19:01:56 +0530

    use conf-cc-SYSTEM for extra compiler flags

--- indimail-mta-x/make-compile.sh
+++ indimail-mta-x/make-compile.sh
@@ -1,17 +1,18 @@
 DEFS=`uname -s | tr "[:lower:]" "[:upper:]"`
 if [ " $DEFS" = " DARWIN" ] ; then
-	exec "$CC" -Wno-deprecated-declarations -D$DEFS -c '${1+"$@"}'
+	echo "" exec "$CC" -Wno-deprecated-declarations -D$DEFS -c '${1+"$@"}'
 else
+	extra=$(cat conf-cc-$DEFS)
 	set $CC
 	cc_arg=$1
 	shift
 	(
 	echo "if [ -n \"\$CC\" ] ; then"
-	echo "  " echo \""\$CC" \$CFLAGS $* -D$DEFS -c '${1+"$@"}'\"
-	echo "  " exec   "\$CC" \$CFLAGS $* -D$DEFS -c '${1+"$@"}'
+	echo "  " echo \""\$CC" \$CFLAGS $* $extra -D$DEFS -c '${1+"$@"}'\"
+	echo "  " exec   "\$CC" \$CFLAGS $* $extra -D$DEFS -c '${1+"$@"}'
 	echo "else"
-	echo "  " echo \""$cc_arg" \$CFLAGS $* -D$DEFS -c '${1+"$@"}'\"
-	echo "  " exec   "$cc_arg" \$CFLAGS $* -D$DEFS -c '${1+"$@"}'
+	echo "  " echo \""$cc_arg" \$CFLAGS $* $extra -D$DEFS -c '${1+"$@"}'\"
+	echo "  " exec   "$cc_arg" \$CFLAGS $* $extra -D$DEFS -c '${1+"$@"}'
 	echo "fi"
 	)
 fi
