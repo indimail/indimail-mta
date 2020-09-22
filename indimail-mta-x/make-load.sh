7fb14865983b00f53ca0c9373b0995150dc625ff
Manvendra Bhangui <mbhangui@gmail.com>
2020-09-22 20:56:32 +0530

    use conf-ld-SYSTEM for system specific ld flags

--- indimail-mta-x/make-load.sh
+++ indimail-mta-x/make-load.sh
@@ -1,11 +1,13 @@
+DEFS=`uname -s | tr "[:lower:]" "[:upper:]"`
+extra=$(cat conf-ld-$DEFS)
 echo 'main="$1"; shift'
 set $LD
 ld_args=$1
 shift
 echo "if [ -n \"\$CC\" ] ; then"
-echo "  " echo \""\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'\"
-echo "  " exec   "\$CC" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'
+echo "  " echo \""\$CC" \$LDFLAGS $extra $* '-o "$main" "$main".o ${1+"$@"}'\"
+echo "  " exec   "\$CC" \$LDFLAGS $extra $* '-o "$main" "$main".o ${1+"$@"}'
 echo "else"
-echo "  " echo \""$ld_args" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'\"
-echo "  " exec   "$ld_args" \$LDFLAGS $* '-o "$main" "$main".o ${1+"$@"}'
+echo "  " echo \""$ld_args" \$LDFLAGS $extra $* '-o "$main" "$main".o ${1+"$@"}'\"
+echo "  " exec   "$ld_args" \$LDFLAGS $extra $* '-o "$main" "$main".o ${1+"$@"}'
 echo "fi"
