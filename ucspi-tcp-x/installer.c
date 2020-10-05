987e706b8922d4f5381efc6bb2bde7279c686925
Manvendra Bhangui <mbhangui@gmail.com>
2020-10-05 15:09:46 +0530

    installer new features 1. uninstall option (-u, -i) 2. create devices 3. create fifo

--- ucspi-tcp-x/installer.c
+++ ucspi-tcp-x/installer.c
@@ -1,5 +1,11 @@
 /*
  * $Log: installer.c,v $
+ * Revision 1.5  2020-10-05 14:57:42+05:30  Cprogrammer
+ * added new features
+ * 1. uninstall option (-u, -i)
+ * 2. create devices
+ * 3. create fifo
+ *
  * Revision 1.4  2020-10-02 17:00:54+05:30  Cprogrammer
  * set permissions for directory only if it is a new directory
  *
@@ -16,9 +22,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fifo.h>
 #include <pwd.h>
 #include <grp.h>
 #include <substdio.h>
+#include <sgetopt.h>
 #include <subfd.h>
 #include <stralloc.h>
 #include <getln.h>
@@ -32,6 +40,7 @@
 stralloc        target = { 0 };
 char           *user, *group, *to;
 const char      FATAL[] = "installer: fatal: ";
+const char      WARN[] = "installer: warning: ";
 static char     strnum[FMT_ULONG];
 char            inbuf[SUBSTDIO_INSIZE], outbuf[SUBSTDIO_OUTSIZE];
 substdio        ssin, ssout;
@@ -68,8 +77,6 @@ print_info(char *str, char *source, char *dest, int mode, int uid, int gid)
 			strerr_die4sys(111, FATAL, "unable to get uid entry for ", strnum, ": ");
 		}
 		user = pw->pw_name;
-	}
-	if (uid != -1) {
 		substdio_puts(subfdout, " -user ");
 		substdio_puts(subfdout, user);
 	}
@@ -100,8 +107,16 @@ print_info(char *str, char *source, char *dest, int mode, int uid, int gid)
 	substdio_flush(subfdout);
 }
 
+/*
+ * anyline starting with #, newline or spaace is a  comment
+ * d:owner:group:mode:target_dir::
+ * l:owner:group:mode:link:target_dir:
+ * f:owner:group:mode:target_file:name:
+ * p:ower:group:mode:target_fifo::
+ * c:ower:group:mode:target_device:devnum:
+ */
 void
-doit(stralloc *line)
+doit(stralloc *line, int uninstall, int ign_dir)
 {
 	char           *x;
 	char           *type, *uidstr, *gidstr, *modestr, *mid, *name;
@@ -109,9 +124,18 @@ doit(stralloc *line)
 	unsigned long   m;
 	unsigned int    xlen, i;
 	int             uid, gid, mode;
+	dev_t           dev;
+	struct stat     st;
 	struct passwd  *pw;
 	struct group   *gr;
 
+	switch (line->s[0])
+	{
+		case '#':
+		case '\n':
+		case ' ':
+			return;
+	}
 	x = line->s;
 	xlen = line->len;
 	opt = (*x == '?');
@@ -181,6 +205,47 @@ doit(stralloc *line)
 	if (xlen > 0)
 		name = x;
 
+	if (uninstall) {
+		switch (*type)
+		{
+		case 'l':
+			print_info("ulink", 0, name, -1, -1, -1);
+			if (lstat(name, &st) == -1) {
+				if (errno == error_noent)
+					return;
+				strerr_die4sys(111, FATAL, "lstat: ", name, ": ");
+			}
+			if ((st.st_mode & S_IFMT) == S_IFDIR)
+				strerr_die3x(111, FATAL, name, ": is a directory");
+			if (unlink(name) == -1 && errno != error_noent)
+				strerr_die4sys(111, FATAL, "unable to unlink ", name, ": ");
+			break;
+		case 'd':
+			if (ign_dir)
+				return;
+			print_info("rmdir", 0, target.s, -1, -1, -1);
+			if (rmdir(target.s) == -1) {
+				if (errno == error_noent)
+					return;
+				strerr_warn4(WARN, "rmdir ", target.s, ": ", &strerr_sys);
+			}
+			break;
+		case 'f':
+			print_info("delete", 0, target.s, -1, -1, -1);
+			if (lstat(target.s, &st) == -1) {
+				if (errno == error_noent)
+					return;
+				strerr_die4sys(111, FATAL, "lstat: ", name, ": ");
+			}
+			if ((st.st_mode & S_IFMT) == S_IFDIR)
+				strerr_die3x(111, FATAL, name, ": is a directory");
+			if (unlink(target.s) == -1 && errno != error_noent)
+				strerr_die4sys(111, FATAL, "unable to unlink ", name, ": ");
+			break;
+		}
+		return;
+	}
+
 	if (*uidstr) {
 		if (!(pw = getpwnam(uidstr)))
 			scan_uint(uidstr, (unsigned int *) &uid);
@@ -202,9 +267,35 @@ doit(stralloc *line)
 		mode = -1;
 	switch (*type)
 	{
-	case 'l':
+	case 'c':
+		print_info("mknod", 0, target.s, mode, uid, gid);
+		scan_ulong(name, (unsigned long *) &dev);
+		if (mknod(target.s, mode, dev) == -1) {
+			if (errno != error_exist)
+				strerr_die4sys(111, FATAL, "mknod ", target.s, ": ");
+		} else {
+			if (uid != -1 && gid != -1 && !my_uid && chown(target.s, (uid_t) uid, (gid_t) gid) == -1)
+				strerr_die4sys(111, FATAL, "unable to chown ", target.s, ": ");
+			if (mode != -1 && !my_uid && chmod(target.s, (mode_t) mode) == -1)
+				strerr_die4sys(111, FATAL, "unable to chmod ", target.s, ": ");
+		}
+	case 'p':
+		print_info("mkfifo", 0, target.s, mode, uid, gid);
+		if (my_uid)
+			mode = 0755;
+		if (fifo_make(target.s, mode) == -1) {
+			if (errno != error_exist)
+				strerr_die4sys(111, FATAL, "fifo_make: ", target.s, ": ");
+		} else {
+			if (uid != -1 && gid != -1 && !my_uid && chown(target.s, (uid_t) uid, (gid_t) gid) == -1)
+				strerr_die4sys(111, FATAL, "unable to chown ", target.s, ": ");
+			if (mode != -1 && !my_uid && chmod(target.s, (mode_t) mode) == -1)
+				strerr_die4sys(111, FATAL, "unable to chmod ", target.s, ": ");
+		}
+	case 'l': /*- here name is target */
+		print_info("make link", name, target.s, -1, -1, -1);
 		if (symlink(name, target.s) == -1 && errno != error_exist)
-			strerr_die6sys(111, FATAL, "unable to symlink ", name, " to ", target.s, ": ");
+			strerr_die6sys(111, FATAL, "unable to symlink ", target.s, " to ", name, ": ");
 		break;
 	case 'd':
 		print_info("makedir", 0, target.s, mode, uid, gid);
@@ -221,7 +312,7 @@ doit(stralloc *line)
 		}
 		break;
 
-	case 'c':
+	case 'f':
 		print_info("install file", name, target.s, mode, uid, gid);
 		if ((fdin = open_read(name)) == -1) {
 			if (opt)
@@ -270,15 +361,23 @@ main(argc, argv)
 	int             argc;
 	char          **argv;
 {
-	int             match;
+	int             opt, match, uninstall = 0, ignore_dirs = 0;;
 
-	if (argc == 1)
+	while ((opt = getopt(argc, argv, "ui")) != opteof) {
+		switch (opt)
+		{
+		case 'u':
+			uninstall = 1;
+			break;
+		case 'i':
+			ignore_dirs = 1;
+			break;
+		}
+	}
+	if (optind < argc)
+		to = argv[optind];
+	else
 		to = (char *) 0;
-	else
-	if (argc == 2)
-		to = argv[1];
-	else
-		strerr_die2x(100, FATAL, "usage: installer [install_dir]");
 	if ((my_uid = getuid()))
 		umask(077);
 	for (;;) {
@@ -286,7 +385,7 @@ main(argc, argv)
 			strerr_die2sys(111, FATAL, "unable to read input");
 		if (line.len > 0)
 			line.s[--line.len] = 0;
-		doit(&line);
+		doit(&line, uninstall, ignore_dirs);
 		if (!match)
 			_exit(0);
 	}
@@ -295,7 +394,7 @@ main(argc, argv)
 void
 getversion_installer_c()
 {
-	static char    *x = "$Id: installer.c,v 1.4 2020-10-02 17:00:54+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: installer.c,v 1.5 2020-10-05 14:57:42+05:30 Cprogrammer Exp mbhangui $";
 
 	if (x)
 		x++;
