169bcb9ad15a7b573512f2280b06940917a45e78
Manvendra Bhangui <mbhangui@gmail.com>
2020-11-30 22:55:43 +0530

    svok: exit if run_init() returns non-zero exit status

--- daemontools-x/svok.c
+++ daemontools-x/svok.c
@@ -1,5 +1,8 @@
 /*
  * $Log: svok.c,v $
+ * Revision 1.4  2020-11-30 22:54:49+05:30  Cprogrammer
+ * exit if run_init() returns non-zero exit status
+ *
  * Revision 1.3  2020-10-08 18:34:12+05:30  Cprogrammer
  * use /run, /var/run if system supports it
  *
@@ -19,6 +22,7 @@
 #endif
 
 #define FATAL "svok: fatal: "
+#define WARN  "svok: warning: "
 
 int
 main(int argc, char **argv)
@@ -30,7 +34,17 @@ main(int argc, char **argv)
 	if (chdir(argv[1]) == -1)
 		strerr_die4sys(111, FATAL, "unable to chdir to ", argv[1], ": ");
 #ifdef USE_RUNFS
-	run_init(argv[1], FATAL);
+	switch (run_init(argv[1]))
+	{
+	case 0:
+		break;
+	case -1:
+		strerr_warn2(WARN, "unable to get current working directory: ", &strerr_sys);
+		_exit(111);
+	case -2:
+		strerr_warn4(WARN, "unable to chdir to ", argv[1], ": ", &strerr_sys);
+		_exit(111);
+	}
 #endif
 	if ((fd = open_write("supervise/ok")) == -1) {
 		if (errno == error_noent)
@@ -39,14 +53,13 @@ main(int argc, char **argv)
 			_exit(100);
 		strerr_die4sys(111, FATAL, "unable to open ", argv[1], "/supervise/ok: ");
 	}
-
 	_exit(0);
 }
 
 void
 getversion_svok_c()
 {
-	static char    *x = "$Id: svok.c,v 1.3 2020-10-08 18:34:12+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: svok.c,v 1.4 2020-11-30 22:54:49+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
