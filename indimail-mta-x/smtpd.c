8413d05b360f0037cf3eee3d466885b635fd962f
Manvendra Bhangui <mbhangui@gmail.com>
2020-12-07 12:07:47 +0530

    refactored mailfrom_parms()

--- indimail-mta-x/smtpd.c
+++ indimail-mta-x/smtpd.c
@@ -106,7 +106,7 @@ int             secure_auth = 0;
 int             ssl_rfd = -1, ssl_wfd = -1;	/*- SSL_get_Xfd() are broken */
 char           *servercert, *clientca, *clientcrl;
 #endif
-char           *revision = "$Revision: 1.232 $";
+char           *revision = "$Revision: 1.233 $";
 char           *protocol = "SMTP";
 stralloc        proto = { 0 };
 static stralloc Revision = { 0 };
@@ -6116,6 +6116,9 @@ addrrelay()
 
 /*
  * $Log: smtpd.c,v $
+ * Revision 1.233  2020-12-07 12:06:27+05:30  Cprogrammer
+ * refactored mailfrom_parms()
+ *
  * Revision 1.232  2020-12-03 17:29:59+05:30  Cprogrammer
  * added EAI - RFC 6530-32 - unicode address support
  *
@@ -6250,7 +6253,7 @@ addrrelay()
 void
 getversion_smtpd_c()
 {
-	static char    *x = "$Id: smtpd.c,v 1.232 2020-12-03 17:29:59+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: smtpd.c,v 1.233 2020-12-07 12:06:27+05:30 Cprogrammer Exp mbhangui $";
 
 	if (x)
 		x++;
