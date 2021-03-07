b986d43eb83dfdc400171f1bf6de68a7697c06d8
Manvendra Bhangui <mbhangui@gmail.com>
2021-03-07 18:11:28 +0530

    added opportunistic TLS for pop3

--- ucspi-tcp-x/dotls.c
+++ ucspi-tcp-x/dotls.c
@@ -1,5 +1,8 @@
 /*
  * $Log: dotls.c,v $
+ * Revision 1.2  2021-03-07 18:11:04+05:30  Cprogrammer
+ * added opportunistic TLS for pop3
+ *
  * Revision 1.1  2021-03-07 08:13:57+05:30  Cprogrammer
  * Initial revision
  *
@@ -25,22 +28,34 @@
 #include <alloc.h>
 #include <gen_alloc.h>
 #include <gen_allocdefs.h>
+#include <getln.h>
 #include <timeoutwrite.h>
 #include "pathexec.h"
 #include "tls.h"
 
 #define FATAL         "sslerator: fatal: "
-#define HUGESMTPTEXT  5000
+#define HUGECAPATEXT  5000
 
 #ifndef	lint
-static char     sccsid[] = "$Id: dotls.c,v 1.1 2021-03-07 08:13:57+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsid[] = "$Id: dotls.c,v 1.2 2021-03-07 18:11:04+05:30 Cprogrammer Exp mbhangui $";
 #endif
+
+int             do_bulk();
+int             smtp_ehlo(char *, char *, int);
+int             func_unimpl(char *, char *, int);
+int             do_tls();
+int             do_quit();
+int             pop3_capa(char *, char *, int);
+void            flush_data();
+void            flush_io();
+void            flush();
 
 struct substdio ssin, ssto, smtpin, smtpto;
 unsigned long   dtimeout = 60;
 stralloc        certfile = { 0 };
-stralloc        smtptext = { 0 };
+stralloc        capatext = { 0 };
 stralloc        sauninit = { 0 };
+stralloc        line = { 0 };
 char            strnum[FMT_ULONG];
 int             linemode = 1;
 struct scommd
@@ -59,20 +74,48 @@ usage(void)
 		 " [ -t timeoutdata ]\n"
 		 " [ -n clientcert ]\n"
 		 " [ -c cafile ] \n"
-		 " [ -s starttlsType (smpt|pop3|imap) ]\n"
+		 " [ -s starttlsType (smpt|pop3) ]\n"
 		 " program");
 }
 
+struct scommd smtpcommands[] = {
+	{ "data", do_bulk, flush_data },
+	{ "ehlo", smtp_ehlo, flush },
+	{ "starttls", do_tls, flush_io },
+	{ "quit", do_quit, flush},
+	{ 0, func_unimpl, flush }
+};
+
+struct scommd pop3commands[] = {
+	{ "retr", do_bulk, flush_data },
+	{ "capa", pop3_capa, flush },
+	{ "stls", do_tls, flush_io },
+	{ "quit", do_quit, flush},
+	{ 0, func_unimpl, flush }
+};
+
 int
-smtp_commands(SSL *ssl, substdio *ss, struct scommd *c, int rfd, int wfd)
+do_commands(enum starttls stls, SSL *ssl, substdio *ss, int rfd, int wfd)
 {
 	int             i, j, found;
 	char           *arg;
 	char            ch;
-	static stralloc smtp_cmd = { 0 };
+	struct scommd   *c;
+	static stralloc cmd = { 0 };
 
-	if (!stralloc_copys(&smtp_cmd, ""))
+	if (!stralloc_copys(&cmd, ""))
 		return -1;
+	switch (stls)
+	{
+	case smtp:
+		c = smtpcommands;
+		break;
+	case pop3:
+		c = pop3commands;
+		break;
+	default:
+		return -1;
+	}
 	for (;;) {
 		if ((i = substdio_get(ss, &ch, 1)) != 1) {
 			return i;
@@ -81,40 +124,51 @@ smtp_commands(SSL *ssl, substdio *ss, struct scommd *c, int rfd, int wfd)
 			break;
 		if (!ch)
 			ch = '\n';
-		if (!stralloc_append(&smtp_cmd, &ch))
+		if (!stralloc_append(&cmd, &ch))
 			return -1;
 	}
-	if (smtp_cmd.len > 0 && smtp_cmd.s[smtp_cmd.len - 1] == '\r')
-		--smtp_cmd.len;
-	if (!stralloc_0(&smtp_cmd))
+	if (cmd.len > 0 && cmd.s[cmd.len - 1] == '\r')
+		--cmd.len;
+	if (!stralloc_0(&cmd))
 		return -1;
-	smtp_cmd.len--;
-	i = str_chr(smtp_cmd.s, ' ');
-	arg = smtp_cmd.s + i;
+	cmd.len--;
+	i = str_chr(cmd.s, ' ');
+	arg = cmd.s + i;
 	found = 0;
 	while (*arg == ' ') {
 		found = 1;
 		++arg;
 	}
-	smtp_cmd.s[i] = 0;
+	cmd.s[i] = 0;
 	for (j = 0; c[j].text; ++j) {
-		if (case_equals(c[j].text, smtp_cmd.s))
+		if (case_equals(c[j].text, cmd.s))
 			break;
 	}
 	if (found) /*- if we had replaced space, put it back */
-		smtp_cmd.s[i] = ' ';
-	i = c[j].fun(arg, smtp_cmd.s, smtp_cmd.len);
+		cmd.s[i] = ' ';
+	i = c[j].fun(arg, cmd.s, cmd.len);
 	if (c[j].flush)
 		c[j].flush();
-	if (i) { /*- only smtp_tls() returns 1 */
-		if (substdio_put(&ssto, "220 ready for tls\r\n", 19) || substdio_flush(&ssto))
-			strerr_die2sys(111, FATAL, "write: ");
+	if (i) { /*- only do_tls() returns 1 */
+		switch (stls)
+		{
+		case smtp:
+			if (substdio_put(&ssto, "220 ready for tls\r\n", 19) == -1 || substdio_flush(&ssto) == -1)
+				strerr_die2sys(111, FATAL, "write: ");
+			break;
+		case pop3:
+			if (substdio_put(&ssto, "+OK Begin SSL/TLS negotiation now.\r\n", 37) == -1 ||
+					substdio_flush(&ssto) == -1)
+			break;
+		default:
+			break;
+		}
 		if (tls_accept(ssl))
 			strerr_die2x(111, FATAL, "unable to accept SSL connection");
-		i = translate(ssl, wfd, rfd, dtimeout);
+		i = translate(ssl, wfd, rfd, dtimeout); /*- returns only when one side closes */
 		ssl_free();
-		return i;
-	} 
+		_exit(i);
+	}
 	return 0;
 }
 
@@ -122,8 +176,8 @@ void
 get1(char *ch)
 {
 	substdio_get(&smtpin, ch, 1);
-	if (*ch != '\r' && smtptext.len < HUGESMTPTEXT &&
-			!stralloc_append(&smtptext, ch))
+	if (*ch != '\r' && capatext.len < HUGECAPATEXT &&
+			!stralloc_append(&capatext, ch))
 		strerr_die2x(111, FATAL, "out of memory");
 }
 
@@ -139,8 +193,8 @@ get3()
 	for (i = 0; i < 3; i++) {
 		if (str[i] == '\r')
 			continue;
-		if (smtptext.len < HUGESMTPTEXT &&
-				!stralloc_append(&smtptext, str + i))
+		if (capatext.len < HUGECAPATEXT &&
+				!stralloc_append(&capatext, str + i))
 			strerr_die2x(111, FATAL, "out of memory");
 	}
 	scan_ulong(str, &code);
@@ -148,13 +202,13 @@ get3()
 }
 
 unsigned long
-smtpcode(unsigned int *len)
+smtpcode(unsigned int *ilen)
 {
 	char            ch;
 	unsigned long   code;
 	int             err = 0;
 
-	if (!stralloc_copys(&smtptext, ""))
+	if (!stralloc_copys(&capatext, ""))
 		strerr_die2x(111, FATAL, "out of memory");
 	if ((code = get3()) < 200)
 		err = 1;
@@ -163,7 +217,7 @@ smtpcode(unsigned int *len)
 		if (ch != ' ' && ch != '-')
 			err = 1;
 		if (ch == ' ')
-			*len = smtptext.len - 1;
+			*ilen = capatext.len - 1;
 		if (ch != '-')
 			break;
 		while (ch != '\n')
@@ -191,48 +245,36 @@ flush_io()
 
 GEN_ALLOC_typedef(saa, stralloc, sa, len, a)
 GEN_ALLOC_readyplus(saa, stralloc, sa, len, a, 10, saa_readyplus)
-saa             ehlokw = { 0 };	/*- list of EHLO keywords and parameters */
-int             maxehlokwlen = 0;
+saa             capakw = { 0 };	/*- list of EHLO keywords and parameters */
 
-unsigned long
-ehlo(char *host, int len, unsigned int *ilen)
+int             maxcapakwlen = 0;
+
+void
+extract_kw(enum starttls stls)
 {
 	stralloc       *saptr;
 	char           *s, *e, *p;
-	unsigned long   code;
 
-	if (ehlokw.len > maxehlokwlen)
-		maxehlokwlen = ehlokw.len;
-	ehlokw.len = 0;
-	if (substdio_puts(&smtpto, "EHLO ") == -1)
-		strerr_die2sys(111, FATAL, "write: ");
-	else
-	if (substdio_put(&smtpto, host, len) == -1)
-		strerr_die2sys(111, FATAL, "write: ");
-	else
-	if (substdio_puts(&smtpto, "\r\n") == -1)
-		strerr_die2sys(111, FATAL, "write: ");
-	else
-	if (substdio_flush(&smtpto) == -1)
-		strerr_die2sys(111, FATAL, "write: ");
-	if ((code = smtpcode(ilen)) != 250)
-		return code;
-	s = smtptext.s;
-	while (*s++ != '\n'); /*- skip the first line: contains the domain */
-	e = smtptext.s + smtptext.len - 6;	/*- 250-?\n */
+	if (capakw.len > maxcapakwlen)
+		maxcapakwlen = capakw.len;
+	capakw.len = 0;
+	s = capatext.s;
+	if (stls == smtp)
+		while (*s++ != '\n'); /*- skip the first line: contains the domain */
+	e = capatext.s + capatext.len - (stls == smtp ? 6 : 0);	/*- 250-?\n */
 	while (s <= e) {
 		int             wasspace = 0;
 
-		if (!saa_readyplus(&ehlokw, 1))
+		if (!saa_readyplus(&capakw, 1))
 			strerr_die2x(111, FATAL, "out of memory");
-		saptr = ehlokw.sa + ehlokw.len++;
-		if (ehlokw.len > maxehlokwlen)
+		saptr = capakw.sa + capakw.len++;
+		if (capakw.len > maxcapakwlen)
 			*saptr = sauninit;
 		else
 			saptr->len = 0;
 
-		/*- smtptext is known to end in a '\n' */
-		for (p = (s += 4);; ++p) {
+		/*- capatext is known to end in a '\n' */
+		for (p = (s += (stls == smtp ? 4 : 0));; ++p) {
 			if (*p == '\n' || *p == ' ' || *p == '\t') {
 				if (!wasspace)
 					if (!stralloc_catb(saptr, s, p - s) || !stralloc_0(saptr))
@@ -252,18 +294,42 @@ ehlo(char *host, int len, unsigned int *ilen)
 		 * keyword should consist of alpha-num and '-'
 		 * broken AUTH might use '=' instead of space
 		 */
-		for (p = saptr->s; *p; ++p) {
-			if (*p == '=') {
-				*p = 0;
-				break;
+		if (stls == smtp) {
+			for (p = saptr->s; *p; ++p) {
+				if (*p == '=') {
+					*p = 0;
+					break;
+				}
 			}
 		}
 	}
+	return;
+}
+
+unsigned long
+ehlo(char *host, int len, unsigned int *ilen)
+{
+	unsigned long   code;
+
+	if (substdio_puts(&smtpto, "EHLO ") == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	else
+	if (substdio_put(&smtpto, host, len) == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	else
+	if (substdio_puts(&smtpto, "\r\n") == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	else
+	if (substdio_flush(&smtpto) == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	if ((code = smtpcode(ilen)) != 250)
+		return code;
+	extract_kw(smtp);
 	return 250;
 }
 
 int
-smtp_data()
+do_bulk()
 {
 	linemode = 0;
 	return 0;
@@ -287,56 +353,98 @@ smtp_ehlo(char *arg, char *cmmd, int cmmdlen)
 
 	if ((code = ehlo(arg, str_len(arg), &ilen)) != 250)
 		strerr_die2x(111, FATAL, "Greeting to server failed");
-	len = ehlokw.len;
-	saptr = ehlokw.sa;
+	len = capakw.len;
+	saptr = capakw.sa;
 	for (; len && case_diffs(saptr->s, "STARTTLS"); ++saptr, --len);
 	if (!len && ilen) {
-		if (!stralloc_copyb(&save, smtptext.s + ilen, smtptext.len - ilen) ||
+		if (!stralloc_copyb(&save, capatext.s + ilen, capatext.len - ilen) ||
 				!stralloc_0(&save))
 			strerr_die2x(111, FATAL, "out of memory");
-		smtptext.len = ilen;
-		if (!stralloc_catb(&smtptext, "-STARTTLS\n250", 14) ||
-				!stralloc_cat(&smtptext, &save))
+		capatext.len = ilen;
+		if (!stralloc_catb(&capatext, "-STARTTLS\n250", 14) ||
+				!stralloc_cat(&capatext, &save))
 			strerr_die2x(111, FATAL, "out of memory");
 	}
-	if (substdio_put(&ssto, smtptext.s, smtptext.len) || substdio_flush(&ssto))
+	if (substdio_put(&ssto, capatext.s, capatext.len) == -1 || substdio_flush(&ssto) == -1)
 		strerr_die2sys(111, FATAL, "write: ");
 	return 0;
 }
 
 int
-smtp_tls()
+do_tls()
 {
 	return 1;
 }
 
 int
-smtp_quit()
+do_quit()
 {
-	if (substdio_put(&smtpto, "quit\r\n",  6) || substdio_flush(&smtpto))
+	if (substdio_put(&smtpto, "quit\r\n",  6) == -1 || substdio_flush(&smtpto) == -1)
 		strerr_die2sys(111, FATAL, "write: ");
 	ssl_free();
 	return 0;
 }
 
 int
-smtp_unimpl(char *arg1, char *cmd, int cmdlen)
+func_unimpl(char *arg1, char *cmd, int cmdlen)
 {
 	if (!*cmd)
 		return 0;
-	if (substdio_put(&smtpto, cmd, cmdlen) ||
-			substdio_put(&smtpto, "\r\n", 2) || substdio_flush(&smtpto))
+	if (substdio_put(&smtpto, cmd, cmdlen) == -1 ||
+			substdio_put(&smtpto, "\r\n", 2) == -1 || substdio_flush(&smtpto) == -1)
 		strerr_die2sys(111, FATAL, "write: ");
 	return 0;
 }
 
-struct scommd smtpcommands[] = {
-	{ "data", smtp_data, flush_data },
-	{ "ehlo", smtp_ehlo, flush },
-	{ "starttls", smtp_tls, flush_io },
-	{ "quit", smtp_quit, flush},
-	{ 0, smtp_unimpl, flush }
-};
+int
+pop3_capa(char *arg, char *cmmd, int cmmdlen)
+{
+	int             i, match, len;
+	stralloc       *saptr;
+
+	if (!stralloc_copys(&capatext, ""))
+		strerr_die2x(111, FATAL, "out of memory");
+	if (substdio_put(&smtpto, "CAPA\n", 5) == -1 || substdio_flush(&smtpto) == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	if (getln(&smtpin, &line, &match, '\n') == -1)
+		strerr_die2sys(111, FATAL, "getln: read-smtpd: ");
+	if (!line.len || !match)
+		strerr_die2x(100, FATAL, "CAPA command failed");
+	if (!case_startb(line.s, 3, "+OK")) {
+		if (!stralloc_catb(&capatext, "STLS\n", 5))
+			strerr_die2x(111, FATAL, "out of memory");
+		/*- capatext.len--; -*/
+	} else
+	for (;;) {
+		if (getln(&smtpin, &line, &match, '\n') == -1)
+			strerr_die2sys(111, FATAL, "getln: read-smtpd: ");
+		if (!line.len || !match)
+			break;
+		i = str_chr(line.s, '\n');
+		if (line.s[i]) {
+			line.s[i] = 0;
+			line.len--;
+		}
+		if (line.s[0] == '.')
+			break;
+		if (!stralloc_cat(&capatext, &line))
+			strerr_die2x(111, FATAL, "out of memory");
+	}
+	extract_kw(pop3);
+	len = capakw.len;
+	saptr = capakw.sa;
+	for (; len && case_diffs(saptr->s, "STLS"); ++saptr, --len) ;
+	if (!len) {
+		if (!stralloc_catb(&capatext, "STLS\n", 5))
+			strerr_die2x(111, FATAL, "out of memory");
+	}
+	if (substdio_put(&ssto, "+OK Here's what I can do:\n", 26) == -1 ||
+			substdio_put(&ssto, capatext.s, capatext.len) == -1 ||
+			substdio_put(&ssto, ".\n", 2) == -1 ||
+			substdio_flush(&ssto) == -1)
+		strerr_die2sys(111, FATAL, "write: ");
+	return 0;
+}
 
 ssize_t
 do_read(int fd, char *buf, size_t len)
@@ -351,7 +459,7 @@ do_write(int fd, char *buf, size_t len)
 }
 
 int
-starttls_smtp(SSL *ssl, int rfd, int wfd)
+do_starttls(enum starttls stls, SSL *ssl, int rfd, int wfd)
 {
 	fd_set          rfds;	/*- File descriptor mask for select -*/
 	char            inbuf[512], outbuf[1024], smtp_inbuf[512],
@@ -387,7 +495,7 @@ starttls_smtp(SSL *ssl, int rfd, int wfd)
 			_exit(111);
 		}
 		if (FD_ISSET(0, &rfds))
-			smtp_commands(ssl, &ssin, smtpcommands, rfd, wfd);
+			do_commands(stls, ssl, &ssin, rfd, wfd);
 		if (FD_ISSET(rfd, &rfds)) {
 			if ((n = saferead(rfd, buf, sizeof(buf), dtimeout)) < 0)
 				strerr_die2sys(111, FATAL, "unable to read from smtpd: ");
@@ -452,20 +560,13 @@ main(int argc, char **argv)
 			cafile = optarg;
 			break;
 		case 's':
-			if (case_diffs(optarg, "smtp") && case_diffs(optarg, "pop3")
-					&& case_diffs(optarg, "imap"))
+			if (case_diffs(optarg, "smtp") && case_diffs(optarg, "pop3"))
 				usage();
 			if (!case_diffs(optarg, "smtp"))
 				stls = smtp;
 			else
-			if (!case_diffs(optarg, "pop3")) {
+			if (!case_diffs(optarg, "pop3"))
 				stls = pop3;
-				strerr_die2x(100, FATAL, "starttls pop3 code not yet ready");
-			} else
-			if (!case_diffs(optarg, "imap")) {
-				stls = imap;
-				strerr_die2x(100, FATAL, "starttls imap code not yet ready");
-			}
 			break;
 		default:
 			usage();
@@ -513,11 +614,8 @@ main(int argc, char **argv)
 		switch (stls)
 		{
 		case smtp:
-			starttls_smtp(ssl, pi2[0], pi1[1]);
-			break;
 		case pop3:
-			break;
-		case imap:
+			do_starttls(stls, ssl, pi2[0], pi1[1]);
 			break;
 		default:
 			if (tls_accept(ssl))
