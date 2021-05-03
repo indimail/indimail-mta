daa70cba00693ec1391b9cb07a01d3ddff4b550e
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-03 09:32:15 +0530

    fix function prototype for timestamp()

--- daemontools-x/timestamp.h
+++ daemontools-x/timestamp.h
@@ -1,5 +1,8 @@
 /*
  * $Log: timestamp.h,v $
+ * Revision 1.2  2021-05-03 09:29:24+05:30  Cprogrammer
+ * fix compiler warnings
+ *
  * Revision 1.1  2004-09-19 18:54:21+05:30  Cprogrammer
  * Initial revision
  *
@@ -9,6 +12,6 @@
 
 #define TIMESTAMP 25
 
-void            timestamp(char *);
+void            timestamp(char [25]);
 
 #endif
