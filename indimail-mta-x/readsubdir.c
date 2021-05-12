846e2cb2fffdd30180792711c228b13a13b54fca
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-12 15:46:00 +0530

    improve readsubdir
    1. reduce calls required to get next entry
    2. make conf-split configurable using environment variable CONFSPLIT

--- indimail-mta-x/readsubdir.c
+++ indimail-mta-x/readsubdir.c
@@ -1,7 +1,7 @@
 /*
  * $Log: readsubdir.c,v $
- * Revision 1.4  2021-05-12 15:30:10+05:30  Cprogrammer
- * converted to c99 function prototypes
+ * Revision 1.4  2021-05-12 15:43:18+05:30  Cprogrammer
+ * reduce calls required to get next entry
  *
  * Revision 1.3  2004-10-22 20:29:54+05:30  Cprogrammer
  * added RCS id
@@ -14,7 +14,6 @@
 #include "fmt.h"
 #include "scan.h"
 #include "str.h"
-#include "auto_split.h"
 
 void
 readsubdir_init(readsubdir *rs, char *name, void (*pause)())
@@ -26,6 +25,22 @@ readsubdir_init(readsubdir *rs, char *name, void (*pause)())
 }
 
 static char     namepos[FMT_ULONG + 4 + READSUBDIR_NAMELEN];
+extern int      conf_split;
+
+void
+opensubdir(readsubdir *rs)
+{
+	unsigned int    len;
+
+	len = 0;
+	len += fmt_str(namepos + len, rs->name);
+	namepos[len++] = '/';
+	len += fmt_ulong(namepos + len, (unsigned long) rs->pos);
+	namepos[len] = 0;
+	while (!(rs->dir = opendir(namepos)))
+		rs->pause(namepos);
+	return;
+}
 
 int
 readsubdir_next(readsubdir *rs, unsigned long *id)
@@ -33,43 +48,32 @@ readsubdir_next(readsubdir *rs, unsigned long *id)
 	direntry       *d;
 	unsigned int    len;
 
-	if (!rs->dir) {
-		if (rs->pos >= auto_split)
-			return 0; /*- end */
-		if (str_len(rs->name) > READSUBDIR_NAMELEN) {
+	if (str_len(rs->name) > READSUBDIR_NAMELEN)
+		return -1;
+	for (;;) {
+		if (!rs->dir)
+			opensubdir(rs);
+		if (!(d = readdir(rs->dir))) {
+			closedir(rs->dir);
+			rs->dir = 0;
 			rs->pos++;
-			return -1;
+			if (rs->pos == conf_split)
+				return 0; /*- no more files in any directory */
+			continue;
 		}
-		len = 0;
-		len += fmt_str(namepos + len, rs->name);
-		namepos[len++] = '/';
-		len += fmt_ulong(namepos + len, (unsigned long) rs->pos);
-		namepos[len] = 0;
-		while (!(rs->dir = opendir(namepos)))
-			rs->pause(namepos);
-		rs->pos++;
-		return -1;
+		if (str_equal(d->d_name, ".") || str_equal(d->d_name, ".."))
+			continue;
+		len = scan_ulong(d->d_name, id);
+		if (!len || d->d_name[len])
+			return -2; /*- invalid filename */
+		return 1;
 	}
-	if (!(d = readdir(rs->dir))) {
-		closedir(rs->dir);
-		rs->dir = 0;
-		return -1;
-	}
-
-	if (str_equal(d->d_name, "."))
-		return -1;
-	if (str_equal(d->d_name, ".."))
-		return -1;
-	len = scan_ulong(d->d_name, id);
-	if (!len || d->d_name[len])
-		return -2;
-	return 1;
 }
 
 void
 getversion_readsubdir_c()
 {
-	static char    *x = "$Id: readsubdir.c,v 1.4 2021-05-12 15:30:10+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: readsubdir.c,v 1.4 2021-05-12 15:43:18+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
