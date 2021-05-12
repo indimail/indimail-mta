b576f0bde99005383b0631ea294146da14dd374d
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-12 21:04:34 +0530

    define arguments as array subscripts to fix gcc 11 warnings

--- ucspi-tcp-x/socket.h
+++ ucspi-tcp-x/socket.h
@@ -1,5 +1,8 @@
 /*
  * $Log: socket.h,v $
+ * Revision 1.4  2021-05-12 21:04:07+05:30  Cprogrammer
+ * define arguments as array subscripts to fix gcc 11 warnings
+ *
  * Revision 1.3  2005-06-10 09:13:16+05:30  Cprogrammer
  * added ipv6 support
  *
@@ -19,10 +22,10 @@
 
 int             socket_tcp(void);
 int             socket_udp(void);
-int             socket_connect4(int, char *, uint16);
+int             socket_connect4(int, char ip[4], uint16);
 int             socket_connected(int);
-int             socket_bind4(int, char *, uint16);
-int             socket_bind4_reuse(int, char *, uint16);
+int             socket_bind4(int, char ip[4], uint16);
+int             socket_bind4_reuse(int, char ip[4], uint16);
 int             socket_listen(int, int);
 int             socket_accept4(int, char *, uint16 *);
 int             socket_recv4(int, char *, int, char *, uint16 *);
@@ -33,14 +36,14 @@ void            socket_tryreservein(int, int);
 #ifdef LIBC_HAS_IP6
 int             socket_tcp6(void);
 int             socket_udp6(void);
-int             socket_connect6(int, char *, uint16, uint32);
-int             socket_bind6(int, char *, uint16, uint32);
-int             socket_bind6_reuse(int, char *, uint16, uint32);
-int             socket_accept6(int, char *, uint16 *, uint32 *);
-int             socket_recv6(int, char *, unsigned int, char *, uint16 *, uint32 *);
-int             socket_send6(int, char *, unsigned int, char *, uint16, uint32);
-int             socket_local6(int, char *ip, uint16 *, uint32 *);
-int             socket_remote6(int, char *ip, uint16 *, uint32 *);
+int             socket_connect6(int, char ip[16], uint16, uint32);
+int             socket_bind6(int, char ip[16], uint16, uint32);
+int             socket_bind6_reuse(int, char ip[16], uint16, uint32);
+int             socket_accept6(int, char ip[16], uint16 *, uint32 *);
+int             socket_recv6(int, char *, unsigned int, char ip[16], uint16 *, uint32 *);
+int             socket_send6(int, char *, unsigned int, char ip[16], uint16, uint32);
+int             socket_local6(int, char ip[16], uint16 *, uint32 *);
+int             socket_remote6(int, char ip[16], uint16 *, uint32 *);
 
 /*- enable sending udp packets to the broadcast address */
 int             socket_broadcast(int);
