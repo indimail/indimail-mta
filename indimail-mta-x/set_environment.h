9c27f7a891adfed109735590baf86791cd17e281
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-13 14:51:18 +0530

    set_environment function to set env variables

---
+++ indimail-mta-x/set_environment.h
@@ -0,0 +1,11 @@
+/*
+ * $Log: set_environment.h,v $
+ * Revision 1.1  2021-05-13 12:36:28+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef SET_ENVIRONMENT_H
+#define SET_ENVIRONMENT_H
+
+void            set_environment(char *, char *);
+#endif
