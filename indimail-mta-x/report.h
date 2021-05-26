7d61f81dbd246532b31ffe7de779c487fb6ed05c
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-26 07:44:45 +0530

    added RCS log

--- indimail-mta-x/report.h
+++ indimail-mta-x/report.h
@@ -1,5 +1,8 @@
 /*
  * $Log: report.h,v $
+ * Revision 1.2  2021-05-26 07:37:41+05:30  Cprogrammer
+ * made delivery variable extern
+ *
  * Revision 1.1  2021-05-23 06:35:03+05:30  Cprogrammer
  * Initial revision
  *
@@ -8,7 +11,7 @@
 #define _REPORT_H
 
 #ifndef	lint
-static char     sccsidreporth[] = "$Id: report.h,v 1.1 2021-05-23 06:35:03+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsidreporth[] = "$Id: report.h,v 1.2 2021-05-26 07:37:41+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 typedef enum {
