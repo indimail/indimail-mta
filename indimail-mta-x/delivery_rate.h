49e3df64c2602be9769a2d58035056bd8035b33a
Manvendra Bhangui <mbhangui@gmail.com>
2021-06-01 01:57:28 +0530

    source for delivery_rate()

---
+++ indimail-mta-x/delivery_rate.h
@@ -0,0 +1,17 @@
+/*
+ * $Log: delivery_rate.h,v $
+ * Revision 1.1  2021-06-01 01:48:07+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef _DELIVERY_RATE_H
+#define _DELIVERY_RATE_H
+
+#ifndef	lint
+static char     sccsiddelivery_rateh[] = "$Id: delivery_rate.h,v 1.1 2021-06-01 01:48:07+05:30 Cprogrammer Exp mbhangui $";
+#endif
+
+int             delivery_rate(char *domain);
+
+#endif
+
