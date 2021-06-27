d18257e2cc968fb7a9d3a654481ebf9b0925fdc1
Manvendra Bhangui <mbhangui@gmail.com>
2021-06-27 11:03:06 +0530

    moved conf_split to fmtqfn.h

--- indimail-mta-x/fmtqfn.h
+++ indimail-mta-x/fmtqfn.h
@@ -1,5 +1,8 @@
 /*
  * $Log: fmtqfn.h,v $
+ * Revision 1.4  2021-06-27 10:44:51+05:30  Cprogrammer
+ * moved conf_split variable to fmtqfn.c
+ *
  * Revision 1.3  2004-10-11 13:53:56+05:30  Cprogrammer
  * added function prototypes
  *
@@ -13,5 +16,7 @@
 #define FMTQFN 40	/*- maximum space needed, if len(dirslash) <= 10 */
 
 unsigned int    fmtqfn(char *, char *, unsigned long, int);
+
+extern int      conf_split;
 
 #endif
