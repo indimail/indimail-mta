ab0b1a48fcf6fdafde6c43868288bab02a90d6d7
Manvendra Bhangui <mbhangui@gmail.com>
2021-08-03 22:37:36 +0530

    use control file control/qfilters to configure qmail-qfilter filters

--- indimail-mta-x/qfrontend.sh
+++ indimail-mta-x/qfrontend.sh
@@ -1,16 +1,23 @@
 #
 # $Log: qfrontend.sh,v $
+# Revision 1.2  2021-08-03 22:35:32+05:30  Cprogrammer
+# use control file control/qfilters to configure qmail-qfilter filters
+#
 # Revision 1.1  2019-02-21 14:17:16+05:30  Cprogrammer
 # Initial revision
 #
 #
 qf=""
-for i in `echo LIBEXEC/qfilters/qf-*`
+if [ ! -f @controldir@/qfilters ] ; then
+	exec qmail-multi
+fi
+for i in `cat @controldir@/qfilters`
 do
+	j=@libexecdir@/qfilters/$i
 	if [ -n "$qf" ] ; then
-		qf="$qf -- $i"
+		qf="$qf -- $j"
 	else
-		qf="$i"
+		qf="$j"
 	fi
 done
 eval qmail-qfilter $qf
