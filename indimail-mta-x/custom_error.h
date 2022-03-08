ba63aa183f59ec042540bc167790df4a40fdfd60
Manvendra Bhangui <mbhangui@gmail.com>
2022-03-08 23:09:02 +0530

    added custom_error function

---
+++ indimail-mta-x/custom_error.h
@@ -0,0 +1,13 @@
+/*
+ * $Log: custom_error.h,v $
+ * Revision 1.1  2022-03-08 22:56:39+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef _CUSTOM_ERROR_H
+#define _CUSTOM_ERROR_H
+#include <noreturn.h>
+
+no_return void  custom_error(char *, char *, char *, char *, char *);
+
+#endif
