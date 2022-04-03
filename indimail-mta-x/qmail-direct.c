dbbcb0341625fb8f9f18d9c18ddf464a13963cea
Manvendra Bhangui <mbhangui@gmail.com>
2022-04-03 18:42:16 +0530

    use custom_error() for error messages

--- indimail-mta-x/qmail-direct.c
+++ indimail-mta-x/qmail-direct.c
@@ -1,5 +1,8 @@
 /*
  * $Log: qmail-direct.c,v $
+ * Revision 1.11  2022-04-03 18:41:49+05:30  Cprogrammer
+ * use custom_error() for error messages
+ *
  * Revision 1.10  2021-09-11 19:00:55+05:30  Cprogrammer
  * replace qmail with indimail-mta in received header
  *
@@ -50,13 +53,18 @@
 #include <substdio.h>
 #include <datetime.h>
 #include <now.h>
+#include <scan.h>
 #include <date822fmt.h>
 #include <qgetpwgr.h>
 #include <noreturn.h>
 #include "pidopen.h"
+#include "custom_error.h"
+#ifdef USE_FSYNC
+#include "syncdir.h"
+#endif
 
 #define DEATH 86400				/* 24 hours; _must_ be below q-s's OSSIFIED (36 hours) */
-#define ADDR 1003
+#define ADDR   1003
 
 static char     inbuf[2048];
 static char     outbuf[256];
@@ -69,53 +77,44 @@ static unsigned int    receivedlen;
 static struct stat pidst;
 static char    *messfn, *intdfn, *tmp_fn;
 static int      intdfd, mailfd, flagmademess = 0, flagmadeintd = 0;
+static int      qm_custom_err = 0;
 static char    *received;
-
-no_return void
-die(int e)
-{
-	_exit(e);
-}
+static stralloc err_str = { 0 };
 
 void
 cleanup()
 {
+	int             e;
+
 	if (tmp_fn) {
+		e = errno;
 		seek_trunc(mailfd, 0);
 		if (unlink(tmp_fn) == -1)
 			return;
+		errno = e;
 	}
 }
 
 no_return void
-die_write()
-{
-	die(53);
-}
-
-no_return void
-die_read()
-{
-	die(54);
-}
-
-no_return void
-die_nomem()
+die(int e, char *str)
 {
 	cleanup();
-	die(51);
+	if (qm_custom_err)
+		custom_error("qmail-direct", "Z", str, errno ? error_str(errno) : 0, "X.3.0");
+	else
+		_exit(e);
 }
 
 no_return void
 sigalrm()
-{								/* thou shalt not clean up here */
-	die(52);
+{	/* thou shalt not clean up here */
+	die(52, "timer expired");
 }
 
 no_return void
 sigbug()
 {
-	die(81);
+	die(81, "internal bug");
 }
 
 /*
@@ -164,7 +163,7 @@ received_setup()
 {
 	receivedlen = receivedfmt((char *) 0);
 	if (!(received = alloc(receivedlen + 1)))
-		die(51);
+		die(51, "out of memory");
 	receivedfmt(received);
 }
 
@@ -199,7 +198,7 @@ fnnum(char *dirslash, char *suffix)
 	char           *s;
 
 	if (!(s = alloc(fmtqfn((char *) 0, dirslash, messnum, suffix))))
-		die(51);
+		die(51, "out of memory");
 	fmtqfn(s, dirslash, messnum, suffix);
 	return s;
 }
@@ -224,19 +223,19 @@ mailopen(uid_t uid, gid_t gid)
 	for (loop = 0; loop < str_len(host_a); ++loop) {
 		if (host_a[loop] == '/') {
 			if (!stralloc_cats(&hostname, "057"))
-				die_nomem();
+				die(51, "out of memory");
 			continue;
 		}
 		if (host_a[loop] == ':') {
 			if (!stralloc_cats(&hostname, "072"))
-				die_nomem();
+				die(51, "out of memory");
 			continue;
 		}
 		if (!stralloc_append(&hostname, s + loop))
-			die_nomem();
+			die(51, "out of memory");
 	}
 	if (!stralloc_copyb(&fntmptph, "tmp/", 4))
-		die_nomem();
+		die(51, "out of memory");
 	for (loop = 0;; ++loop) {
 		gettimeofday(&cur, 0);
 		strnum[fmt_ulong(strnum, cur.tv_sec)] = 0;
@@ -244,17 +243,18 @@ mailopen(uid_t uid, gid_t gid)
 		if (!stralloc_cats(&fntmptph, strnum)
 				|| !stralloc_append(&fntmptph, ".")
 				|| !stralloc_append(&fntmptph, "M"))
-			die_nomem();
+			die(51, "out of memory");
 		strnum[fmt_ulong(strnum, cur.tv_usec)] = 0;
 		if (!stralloc_cats(&fntmptph, strnum)
 				|| !stralloc_append(&fntmptph, "P"))
-			die_nomem();
+			die(51, "out of memory");
 		strnum[fmt_ulong(strnum, pid)] = 0;
 		if (!stralloc_cats(&fntmptph, strnum)
 				|| !stralloc_append(&fntmptph, ".")
 				|| !stralloc_cat(&fntmptph, &hostname)
 				|| !stralloc_0(&fntmptph))
-			die_nomem();
+			die(51, "out of memory");
+		fntmptph.len--;
 		if ((mailfd = open_exclr(fntmptph.s)) >= 0) {
 			tmp_fn = fntmptph.s;
 			break;
@@ -262,77 +262,122 @@ mailopen(uid_t uid, gid_t gid)
 		if (errno == error_exist) {
 			/*- really should never get to this point */
 			if (loop == 2) {
-				cleanup();
-				die(61);
+				if (!stralloc_copyb(&err_str, "open: ", 6) ||
+						!stralloc_cat(&err_str, &fntmptph) ||
+						!stralloc_0(&err_str))
+					die(51, "out of memory");
+				die(68, err_str.s);
 			}
 			usleep(100);
-		} 
-		cleanup();
-		die(61);
+		}
+		if (!stralloc_copyb(&err_str, "open: ", 6) ||
+				!stralloc_cat(&err_str, &fntmptph) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(68, err_str.s);
 	}
 	if (fstat(mailfd, &st) == -1) {
-		cleanup();
-		die(61);
+		if (!stralloc_copyb(&err_str, "fstat: ", 7) ||
+				!stralloc_cats(&err_str, pidfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(68, err_str.s);
 	}
 	if (fchown(mailfd, uid, gid)) {
-		cleanup();
-		die(50);
+		if (!stralloc_copyb(&err_str, "fchown: ", 8) ||
+				!stralloc_cat(&err_str, &fntmptph) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(68, err_str.s);
 	}
 	if (!stralloc_copyb(&fnnewtph, "new/", 4))
-		die_nomem();
+		die(51, "out of memory");
 	strnum[fmt_ulong(strnum, cur.tv_sec)] = 0;
 	if (!stralloc_cats(&fnnewtph, strnum)
 			|| !stralloc_append(&fnnewtph, "."))
-		die_nomem();
+		die(51, "out of memory");
 	/*- in hexadecimal */
 	if (!stralloc_append(&fnnewtph, "I"))
-		die_nomem();
+		die(51, "out of memory");
 	s = alloc(fmt_xlong(0, st.st_ino) + fmt_xlong(0, st.st_dev));
 	i = fmt_xlong(s, st.st_ino);
 	if (!stralloc_catb(&fnnewtph, s, i)
 			|| !stralloc_append(&fnnewtph, "V"))
-		die_nomem();
+		die(51, "out of memory");
 	i = fmt_xlong(s, st.st_dev);
 	if (!stralloc_catb(&fnnewtph, s, i))
-		die_nomem();
+		die(51, "out of memory");
 	alloc_free(s);
 	/*- in decimal */
 	if (!stralloc_append(&fnnewtph, "M"))
-		die_nomem();
+		die(51, "out of memory");
 	strnum[fmt_ulong(strnum, cur.tv_usec)] = 0;
 	if (!stralloc_cats(&fnnewtph, strnum)
 			|| !stralloc_append(&fnnewtph, "P"))
-		die_nomem();
+		die(51, "out of memory");
 	strnum[fmt_ulong(strnum, pid)] = 0;
 	if (!stralloc_cats(&fnnewtph, strnum)
 			|| !stralloc_append(&fnnewtph, ".")
 			|| !stralloc_cat(&fnnewtph, &hostname)
 			|| !stralloc_0(&fnnewtph))
-		die_nomem();
+		die(51, "out of memory");
 	return;
 }
 
 int
 main(int argc, char **argv)
 {
-	unsigned int    ret, len, rcptcount, use_pwgr;
-	char           *ptr;
+	unsigned int    ret, len, rcptcount, use_pwgr, fastqueue;
+	uid_t           uid;
+	gid_t           gid;
+	char           *ptr, *home;
 	struct passwd  *pw;
 	char            ch;
 
 	sig_blocknone();
 	umask(033);
-	if (!(ptr = env_get("USER")))
-		die(67);
-	use_pwgr = env_get("USE_QPWGR") ? 1 : 0;
-	if (!(pw = (use_pwgr ? qgetpwnam : getpwnam) (ptr)))
-		die(67);
-	if (!stralloc_copys(&fntmptph, pw->pw_dir) ||
+	if (!(ptr = env_get("FASTQUEUE")))
+		fastqueue = 0;
+	else {
+		scan_uint(ptr, &fastqueue);
+		uid = fastqueue;
+		gid = -1;
+	}
+#ifdef USE_FSYNC
+	if (fastqueue)
+		use_fsync = use_syncdir = 0;
+	else {
+		use_fsync = use_syncdir = 0;
+		if ((ptr = env_get("USE_FSYNC")) && *ptr)
+			use_fsync = 1;
+		if ((ptr = env_get("USE_SYNCDIR")) && *ptr)
+			use_syncdir = 1;
+	}
+#endif
+	qm_custom_err = env_get("QMAIL_QUEUE_CUSTOM_ERROR") ? 1 : 0;
+	if (fastqueue) {
+		if (!(home = env_get("HOME")))
+			die(78, "trouble getting home directory for user");
+	} else {
+		if (!(ptr = env_get("USER")))
+			die(78, "trouble getting user");
+		use_pwgr = env_get("USE_QPWGR") ? 1 : 0;
+		if (!(pw = (use_pwgr ? qgetpwnam : getpwnam) (ptr)))
+			die(78, "trouble getting uid/gid");
+		home = pw->pw_dir;
+		uid = pw->pw_uid;
+		gid = pw->pw_gid;
+	}
+	if (!stralloc_copys(&fntmptph, home) ||
 			!stralloc_catb(&fntmptph, "/Maildir", 8) ||
 			!stralloc_0(&fntmptph))
-		die_nomem();
-	if (chdir(fntmptph.s) == -1)
-		die(61);
+		die(51, "out of memory");
+	if (chdir(fntmptph.s) == -1) {
+		if (!stralloc_copyb(&err_str, "chdir: $HOME/Maildir", 22) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(61, err_str.s);
+	}
 	myuid = getuid();
 	mypid = getpid();
 	starttime = now();
@@ -345,148 +390,189 @@ main(int argc, char **argv)
 	alarm(DEATH);
 
 	/*- set pidfn and open with fd = messfd */
-	if ((ret = pidopen(starttime, "tmp")))
-		die(ret);
-	if (fstat(messfd, &pidst) == -1)
-		die(63);
+	if ((ret = pidopen(starttime, "tmp"))) {
+		if (!stralloc_copyb(&err_str, "open: ", 6) ||
+				!stralloc_cats(&err_str, pidfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(ret, err_str.s);
+	}
+	if (fstat(messfd, &pidst) == -1) {
+		if (!stralloc_copyb(&err_str, "fstat: ", 7) ||
+				!stralloc_cats(&err_str, pidfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(70, err_str.s);
+	}
 	messnum = pidst.st_ino;
 	messfn = fnnum("tmp/", ".h");
 	intdfn = fnnum("tmp/", ".m");
 	/*- link mess file to pid file */
 	if (link(pidfn, messfn) == -1) {
+		if (!stralloc_copyb(&err_str, "link: ", 6) ||
+				!stralloc_cats(&err_str, messfn) ||
+				!stralloc_catb(&err_str, " -> ", 4) ||
+				!stralloc_cats(&err_str, pidfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
 		unlink(pidfn);
-		die(64);
+		die(67, err_str.s);
 	}
 	/*- remove pid file */
 	if (unlink(pidfn) == -1) {
+		if (!stralloc_copyb(&err_str, "unlink: ", 8) ||
+				!stralloc_cats(&err_str, pidfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
 		unlink(pidfn);
-		die(63);
+		die(70, err_str.s);
 	}
 	flagmademess = 1;
-	if (unlink(messfn) == -1)
-		die(63);
+	if (unlink(messfn) == -1) {
+		if (!stralloc_copyb(&err_str, "unlink: ", 8) ||
+				!stralloc_cats(&err_str, messfn) ||
+				!stralloc_0(&err_str))
+			die(51, "out of memory");
+		die(70, err_str.s);
+	}
 	substdio_fdbuf(&ssout, write, messfd, outbuf, sizeof (outbuf));
 	/*- read the message body */
 	substdio_fdbuf(&ssin, read, 0, inbuf, sizeof (inbuf)); /*- message is read from fd 0 */
 	if (substdio_bput(&ssout, received, receivedlen) == -1)
-		die_write();
+		die(53, "trouble writing message");
 	switch (substdio_copy(&ssout, &ssin)) /*- copy message body to messfn */
 	{
 	case -2:
-		die_read();
+		die(54, "trouble reading message");
 	case -3:
-		die_write();
+		die(53, "trouble writing message");
 	}
 	if (substdio_flush(&ssout) == -1)
-		die_write();
-	if (fsync(messfd) == -1)
-		die_write();
+		die(53, "trouble writing message");
 	/*- write the envelope */
 	if ((intdfd = open_exclr(intdfn)) == -1)
-		die(65);
+		die(65, "trouble creating files in intd");
 	flagmadeintd = 1;
 	if (unlink(intdfn) == -1)
-		die(63);
+		die(80, "trouble removing intdfn");
 	substdio_fdbuf(&ssout, write, intdfd, outbuf, sizeof (outbuf));
 	substdio_fdbuf(&ssin, read, 1, inbuf, sizeof (inbuf)); /*- envelope is read from fd 1 */
 
 	/*- Return-Path */
 	if (substdio_get(&ssin, &ch, 1) < 1)
-		die_read();
+		die(54, "trouble reading envelope");
 	if (ch != 'F')
-		die(91);
+		die(79, "envelope format error");
 	if (substdio_bput(&ssout, "Return-Path: ", 13) == -1)
-		die_write();
+		die(53, "trouble writing envelope");
 	for (len = 0; len < ADDR; ++len) {
 		if (substdio_get(&ssin, &ch, 1) < 1)
-			die_read();
+			die(54, "trouble reading envelope");
 		if (!ch) {
 			if (substdio_put(&ssout, "\n", 1) == -1)
-				die_write();
+				die(53, "trouble writing envelope");
 			break;
 		} else
 		if (substdio_put(&ssout, &ch, 1) == -1)
-			die_write();
+			die(53, "trouble writing envelope");
 	}
 	if (len >= ADDR)
-		die(11);
+		die(11, "envelope address too long");
 	/*- get recipients */
 	if (substdio_bput(&ssout, "X-Recipients: ", 14) == -1)
-		die_write();
+		die(53, "trouble writing envelope");
 	for (rcptcount = 0;;) {
 		if (substdio_get(&ssin, &ch, 1) < 1)
-			die_read();
+			die(54, "trouble reading recipients");
 		if (!ch) {
 			if (substdio_put(&ssout, "\n", 1) == -1)
-				die_write();
+				die(53, "trouble writing envelope");
 			break;
 		}
 		if (rcptcount) {
 			if (substdio_put(&ssout, ", ", 2) == -1)
-				die_write();
+				die(53, "trouble writing envelope");
 		}
 		/*- new recipient */
 		if (ch != 'T')
-			die(91);
+			die(79, "envelope format error");
 		rcptcount++;
 		for (len = 0; len < ADDR; ++len) {
 			if (substdio_get(&ssin, &ch, 1) < 1)
-				die_read();
+				die(54, "trouble reading envelope");
 			if (!ch)
 				break;
 			if (substdio_bput(&ssout, &ch, 1) == -1)
-				die_write();
+				die(53, "trouble writing envelope");
 		}
 		if (len >= ADDR)
-			die(11);
+			die(11, "envelope address too long");
 	} /*- for (rcptcount = 0;;) */
-	if (substdio_flush(&ssout) == -1 || fsync(intdfd) == -1)
-		die_write();
-	if (seek_set(messfd, 0) == -1 || seek_set(intdfd, 0) == -1)
-		die_read();
-	if (chdir(pw->pw_dir) == -1 || chdir("Maildir") == -1)
-		die(61);
+	if (substdio_flush(&ssout) == -1)
+		die(53, "trouble writing envelope");
+	if (seek_set(messfd, 0) == -1)
+		die(54, "trouble reading messfd");
+	if (seek_set(intdfd, 0) == -1)
+		die(54, "trouble seeking intdfd");
+	if (chdir(home) == -1 || chdir("Maildir") == -1)
+		die(61, "trouble doing cd to Maildir");
 	/*- write the mail */
-	mailopen(pw->pw_uid, pw->pw_gid);
+	mailopen(uid, gid);
 	substdio_fdbuf(&ssout, write, mailfd, outbuf, sizeof (outbuf));
 	substdio_fdbuf(&ssin, read, intdfd, inbuf, sizeof (inbuf)); /*- envelope read earlier from fd 1 */
 	switch (substdio_copy(&ssout, &ssin))
 	{
 	case -2:
-		die_read();
+		die(54, "trouble reading envelope");
 	case -3:
-		die_write();
+		die(53, "trouble writing message");
 	}
 	if (substdio_flush(&ssout) == -1)
-		die_write();
+		die(53, "trouble writing message");
 	substdio_fdbuf(&ssin, read, messfd, inbuf, sizeof (inbuf)); /*- body read earlier from fd 0 */
 	switch (substdio_copy(&ssout, &ssin))
 	{
 	case -2:
-		die_read();
+		die(54, "trouble reading message body");
 	case -3:
-		die_write();
+		die(53, "trouble writing message");
 	}
-	if (fsync(mailfd) == -1)
-		die_write();
+#ifdef USE_FSYNC
+	if (use_fsync && fdatasync(mailfd) == -1)
+		die(53, "trouble syncing message to disk");
+#else
+	if (fdatasync(mailfd) == -1)
+		die(53, "trouble syncing message to disk");
+#endif
 	close(mailfd);
 	close(messfd);
 	close(intdfd);
 	/*- link new/file to tmp/file */
-	if (link(fntmptph.s, fnnewtph.s) == -1) {
-		unlink(fntmptph.s);
-		die(61);
-	}
+	if (link(fntmptph.s, fnnewtph.s) == -1)
+		die(68, "trouble linking files in Maildir/new to Maildir/tmp");
 	/*- remove tmp_fn file */
 	if (unlink(fntmptph.s) == -1)
-		die(61);
-	die(0);
+		die(68, "trouble removing file in Maildir/tmp");
+#if defined(USE_FSYNC) && defined(LINUX) /*- asynchronous nature of link() happens only on Linux */
+	if (use_syncdir > 0) {
+		if ((mailfd = open(fnnewtph.s, O_RDONLY)) == -1 ||
+				fdatasync(mailfd) == -1 || close(mailfd) == -1) {
+			if (!stralloc_copyb(&err_str, "trouble syncing ", 16) ||
+					!stralloc_cats(&err_str, home) ||
+					!stralloc_catb(&err_str, " directory", 10) ||
+					!stralloc_0(&err_str))
+				die(51, "out of memory");
+			die(69, err_str.s);
+		}
+	}
+#endif
+	_exit(0);
 }
 
 void
 getversion_qmail_direct_c()
 {
-	static char    *x = "$Id: qmail-direct.c,v 1.10 2021-09-11 19:00:55+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: qmail-direct.c,v 1.11 2022-04-03 18:41:49+05:30 Cprogrammer Exp mbhangui $";
 
 	x = sccsidpidopenh;
 	if (x)
