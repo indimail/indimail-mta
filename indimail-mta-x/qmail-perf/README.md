5cfde8b5fb671b190051b1b64ae497bc67167a1b
Manvendra Bhangui <mbhangui@gmail.com>
2022-04-10 20:56:26 +0530

    updated qmail-perf scripts

---
+++ indimail-mta-x/qmail-perf/README.md
@@ -0,0 +1,3 @@
+## Performance Graphs for indimail
+
+https://docs.google.com/spreadsheets/d/1WsyASGBrFPERX9am2Cgce4IMjOGrxK-77sNTCruZAHE/edit?usp=sharing
--- indimail-mta-x/qmail-perf/sub.c
+++ indimail-mta-x/qmail-perf/sub.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -32,14 +33,16 @@ main(int argc, char **argv)
 {
 	int count, i, j, k, s, fd, sub;
 	char fn[82];
+	struct timeval  tmval1, tmval2;
 
 	if (argc != 4) {
 		fprintf(stderr, "sub sync conf-split no-of-files-to-create\n");
 		return 1;
 	}
-	s = atoi(argv[1]);
-	j = atoi(argv[2]);
-	k = atoi(argv[3]);
+	gettimeofday(&tmval1, 0);
+	s = atoi(argv[1]); /*- sync */
+	j = atoi(argv[2]); /*- conf split */
+	k = atoi(argv[3]); /*- no of files */
 	for (i = 0; i < j; i++) {
 		sprintf(fn, "%d", i);
 		if (access(fn, F_OK) && mkdir(fn, 0755) == -1) {
@@ -61,10 +64,19 @@ main(int argc, char **argv)
 			fprintf(stderr, "write: %s: %s\n", fn, strerror(errno));
 			return 1;
 		}
-		if ((s && fsync(fd) == -1) || close(fd) == -1) {
-			fprintf(stderr, "fsync/close: %s: %s\n", fn, strerror(errno));
-			return 1;
+		if (s) {
+			if (fsync(fd) == -1 || close(fd) == -1) {
+				fprintf(stderr, "fsync/close: %s: %s\n", fn, strerror(errno));
+				return 1;
+			}
+		} else {
+			if (close(fd) == -1) {
+				fprintf(stderr, "close: %s: %s\n", fn, strerror(errno));
+				return 1;
+			}
 		}
 	}
+	gettimeofday(&tmval2, 0);
+	printf ("%f\n", (tmval2.tv_sec + ((double) tmval2.tv_usec / 1000000)) - (tmval1.tv_sec + ((double) tmval1.tv_usec / 1000000)));
 	return 0;
 }
