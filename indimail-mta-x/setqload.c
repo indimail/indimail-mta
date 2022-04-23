32fec0e6248ca72e3f42c72242c0e33c42288fe5
Manvendra Bhangui <mbhangui@gmail.com>
2022-04-23 22:47:34 +0530

    argument for loads can be mathemtical expresseions

--- indimail-mta-x/setqload.c
+++ indimail-mta-x/setqload.c
@@ -1,10 +1,14 @@
 /*
- * $Log: $
+ * $Log: setqload.c,v $
+ * Revision 1.1  2022-04-23 22:46:40+05:30  Cprogrammer
+ * Initial revision
+ *
  */
 #include <substdio.h>
 #include "haslibrt.h"
 #ifdef HASLIBRT
 #include <unistd.h>
+#include <sgetopt.h>
 #include <fcntl.h>
 #include <subfd.h>
 #include <sys/stat.h>
@@ -14,13 +18,17 @@
 #include <strerr.h>
 #include <scan.h>
 #include <fmt.h>
+#include <evaluate.h>
 #include "send_qload.h"
 
 #define FATAL "setqload: fatal: "
 #define WARN  "setqload: warning: "
 
+char           *usage1 = "-q queue_number total_load_pct";
+char           *usage2 = "-q queue_number local_load_pct remote_load_pct";
+
 void
-get_queue_number(int *q)
+get_queue_details(int *q)
 {
 	int             shm;
 
@@ -32,27 +40,87 @@ get_queue_number(int *q)
 }
 
 int
+compute_expression(struct vartable *vt, char *s, double *x)
+{
+	struct val      result;
+
+	switch (evaluate(s, &result, vt))
+	{
+	case ERROR_SYNTAX:
+		strerr_warn2(s, ": syntax error", 0);
+		return -1;
+	case ERROR_VARNOTFOUND:
+		strerr_warn2(s, ": variable not found", 0);
+		return -1;
+	case ERROR_NOMEM:
+		strerr_warn2(s, ": out of memory", 0);
+		return -1;
+	case ERROR_DIV0:
+		strerr_warn2(s, ": division by zero", 0);
+		return -1;
+	case RESULT_OK:
+		*x = (double) ((result.type == T_INT) ? result.ival : result.rval);
+		return 0;
+	default:
+		strerr_warn2(s, ": unknown error", 0);
+		return -1;
+	}
+}
+
+int
 main(int argc, char **argv)
 {
-	int             i, ret, qnum, qcount, qconf, load;
+	int             i, opt, ret, qnum = 0, qcount, qconf;
 	int             q[3];
+	double          load, load_l, load_r;
 	char            strnum[FMT_ULONG];
 	char           *argv0;
+	struct vartable *vt = {0};
 
-	if (argc != 3) {
-		i = str_rchr(argv[0], '/');
-		argv0 = (argv[0][i] && argv[0][i + 1]) ? argv[0] + i + 1 : argv[0];
-		strerr_die4x(100, FATAL, "USAGE: ", argv0, " queue_no load");
+	i = str_rchr(argv[0], '/');
+	argv0 = (argv[0][i] && argv[0][i + 1]) ? argv[0] + i + 1 : argv[0];
+	load_l = load_r = 0.0;
+	while ((opt = getopt(argc, argv, "q:")) != opteof) {
+		switch(opt)
+		{
+		case 'q':
+			scan_int(optarg, &qnum);
+			break;
+		default:
+			strerr_warn4("usage: ", argv0, " ", usage1, 0);
+			strerr_warn1("       or", 0);
+			strerr_warn4("usage: ", argv0, " ", usage2, 0);
+			_exit(100);
+		}
 	}
-	get_queue_number(q);
+	if (!qnum || optind == argc) {
+		strerr_warn4("usage: ", argv0, " ", usage1, 0);
+		strerr_warn1("       or", 0);
+		strerr_warn4("usage: ", argv0, " ", usage2, 0);
+		_exit(100);
+	}
+	get_queue_details(q);
 	qcount = q[0];
 	qconf = q[1];
-	scan_int(argv[1], &qnum);
 	if (qnum > qcount) {
 		strnum[fmt_int(strnum, qnum)] = 0;
 		strerr_die3x(100, WARN, "queue number cannot be > ", strnum);
 	}
-	scan_int(argv[2], &load);
+	if (!(vt = create_vartable()))
+		strerr_die2x(111, argv0, ": out of memory");
+	if (optind == argc - 1) {
+		if (compute_expression(vt, argv[optind++], &load_l))
+			_exit(111);
+	} else
+	if (optind == argc - 2) {
+		if (compute_expression(vt, argv[optind++], &load_l))
+			_exit(111);
+		if (compute_expression(vt, argv[optind++], &load_r))
+			_exit(111);
+	}
+	if (vt)
+		free_vartable(vt);
+	load = 100 * (load_l + load_r);
 	ret = (load > 0 ? send_qload("/qscheduler", qnum, load, 10) : 0);
 	if (!ret) {
 		strnum[i = fmt_int(strnum, qcount)] = 0;
@@ -91,7 +159,8 @@ main(argc, argv)
 void
 getversion_setqload_c()
 {
-	static char    *x = "$Id: $";
+	static char    *x = "$Id: setqload.c,v 1.1 2022-04-23 22:46:40+05:30 Cprogrammer Exp mbhangui $";
 
+	x = sccsidevalh;
 	x++;
 }
