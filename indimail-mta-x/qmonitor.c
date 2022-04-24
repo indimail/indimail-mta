2de07f3f6619303f0e1bdbed7f73ad502319f2fb
Manvendra Bhangui <mbhangui@gmail.com>
2022-04-24 10:06:57 +0530

    removed display code

--- indimail-mta-x/qmonitor.c
+++ indimail-mta-x/qmonitor.c
@@ -1,5 +1,5 @@
 /*
- * $Id: qmonitor.c,v 1.1 2022-04-24 08:51:15+05:30 Cprogrammer Exp mbhangui $
+ * $Id: qmonitor.c,v 1.2 2022-04-24 10:06:37+05:30 Cprogrammer Exp mbhangui $
  */
 #include "haslibrt.h"
 #ifdef HASLIBRT
@@ -10,7 +10,6 @@
 #include <sgetopt.h>
 #include <strerr.h>
 #include <scan.h>
-#include <qprintf.h>
 #include <getEnvConfig.h>
 #include "queue_load.h"
 #include "set_environment.h"
@@ -22,92 +21,20 @@
 char           *usage =
 	"usage: qmonitor [-t threshold ] [-i interval] [-n]\n"
 	"        -t threshold (load threshold)\n"
-	"        -i interval\n"
-	"        -n           (Display Mode)";
-
-void
-display(QDEF *queue, int queue_count, int queue_conf)
-{
-	char            strnum[FMT_DOUBLE];
-	int             i, j, x, lcur = 0, rcur = 0, min = -1;
-	double          load_l = 0.0, load_r = 0.0;
-
-	for (j = 0; j < queue_count; j++) {
-		x = queue[j].lcur + queue[j].rcur;
-		if (min == -1 || x < min)
-			min = x; /*- minimum concurrency */
-	}
-	for (j = 0; j < queue_count; j++) {
-		if (!j)
-			substdio_put(subfdout, "queue      local  Remote   +/- flag\n", 36);
-		if (!queue[j].lmax || !queue[j].rmax) {
-			substdio_put(subfderr, "invalid concurrency = 0 ", 24);
-			if (!queue[j].lmax)
-				substdio_put(subfderr, "[local] ", 8);
-			if (!queue[j].rmax)
-				substdio_put(subfderr, "[remote] ", 9);
-			substdio_put(subfderr, "for queue ", 10);
-			substdio_put(subfderr, queue[j].queue.s, queue[j].queue.len);
-			substdio_put(subfderr, "\n", 1);
-			substdio_flush(subfderr);
-			continue;
-		}
-		lcur += queue[j].lcur;
-		rcur += queue[j].rcur;
-		load_l += (double) queue[j].lcur / queue[j].lmax;
-		load_r += (double) queue[j].rcur / queue[j].rmax;
-		qprintf(subfdout, queue[j].queue.s, "%-08s");
-		substdio_put(subfdout, " ", 1);
-		strnum[i = fmt_ulong(strnum, queue[j].lcur)] = 0;
-		qprintf(subfdout, strnum, "%+3s");
-		substdio_put(subfdout, "/", 1);
-		strnum[i = fmt_ulong(strnum, queue[j].lmax)] = 0;
-		qprintf(subfdout, strnum, "%-4s");
-		substdio_put(subfdout, " ", 1);
-		strnum[i = fmt_ulong(strnum, queue[j].rcur)] = 0;
-		qprintf(subfdout, strnum, "%+3s");
-		substdio_put(subfdout, "/", 1);
-		strnum[i = fmt_ulong(strnum, queue[j].rmax)] = 0;
-		qprintf(subfdout, strnum, "%-4s");
-		x = queue[j].lcur + queue[j].rcur;
-		substdio_put(subfdout, x == min ? " - " : " + ", 3);
-		substdio_put(subfdout, queue[j].flag ? " disabled\n" : "  enabled\n", 10);
-	}
-	substdio_put(subfdout, "queue count = ", 14);
-	strnum[i = fmt_ulong(strnum, queue_count)] = 0;
-	substdio_put(subfdout, strnum, i);
-
-	substdio_put(subfdout, ", queue configured = ", 21);
-	strnum[i = fmt_ulong(strnum, queue_conf)] = 0;
-	substdio_put(subfdout, strnum, i);
-
-	substdio_put(subfdout, ", Total local = ", 16);
-	strnum[i = fmt_ulong(strnum, lcur)] = 0;
-	substdio_put(subfdout, strnum, i);
-
-	substdio_put(subfdout, ", Total remote = ", 17);
-	strnum[i = fmt_ulong(strnum, rcur)] = 0;
-	substdio_put(subfdout, strnum, i);
-
-	substdio_put(subfdout, ", qload_avg = ", 14);
-	strnum[i = fmt_double(strnum, 50 * (load_l +  load_r)/queue_count, 2)] = 0;
-	qprintf(subfdout, strnum, "%+6s");
-	substdio_put(subfdout, "\n", 1);
-	substdio_flush(subfdout);
-}
+	"        -i interval";
 
 int
 main(int argc, char **argv)
 {
 	int             i, j, opt, qcount, qconf, interval = 60,
-					display_mode = 0, verbose = 0;
+					verbose = 0;
 	double          threshold, total_load[2], qload;
 	char            strnum[FMT_DOUBLE];
 	QDEF           *queue = (QDEF *) NULL;
 
 	set_environment(WARN, FATAL, 0);
 	getEnvConfigDouble(&threshold,   "QUEUE_LOAD",   QUEUE_LOAD);
-	while ((opt = getopt(argc, argv, "i:t:nv")) != opteof) {
+	while ((opt = getopt(argc, argv, "i:t:v")) != opteof) {
 		switch (opt)
 		{
 		case 't':
@@ -115,9 +42,6 @@ main(int argc, char **argv)
 			break;
 		case 'i':
 			scan_int(optarg, &interval);
-			break;
-		case 'n':
-			display_mode = 1;
 			break;
 		case 'v':
 			verbose = 1;
@@ -128,10 +52,6 @@ main(int argc, char **argv)
 	}
 	for (;;) {
 		queue_load("qmonitor", &qcount, &qconf, total_load, &queue);
-		if (display_mode) {
-			display(queue, qcount, qconf);
-			break;
-		}
 		if (verbose || ((total_load[0] + total_load[1]) > 2 * qcount * threshold)) {
 			substdio_put(subfdout, "queue loads local[", 18);
 			strnum[i = fmt_double(strnum, total_load[0], 2)] = 0;
@@ -167,6 +87,7 @@ main(int argc, char **argv)
 		}
 		sleep(interval);
 	}
+	return 0;
 }
 #else
 #warning "not compiled with -DHASLIBRT"
@@ -191,13 +112,16 @@ main(argc, argv)
 void
 getversion_qmonitor_c()
 {
-	static char    *x = "$Id: qmonitor.c,v 1.1 2022-04-24 08:51:15+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: qmonitor.c,v 1.2 2022-04-24 10:06:37+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
 
 /*-
  * $Log: qmonitor.c,v $
+ * Revision 1.2  2022-04-24 10:06:37+05:30  Cprogrammer
+ * removed display code
+ *
  * Revision 1.1  2022-04-24 08:51:15+05:30  Cprogrammer
  * Initial revision
  *
