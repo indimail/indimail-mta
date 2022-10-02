ca21669a9c21b3040de5f258d85c78dc5b3e1e3b
Manvendra Bhangui <mbhangui@gmail.com>
2022-10-02 21:56:18 +0530

    refactored dknewkey

--- indimail-mta-x/dknewkey.sh
+++ indimail-mta-x/dknewkey.sh
@@ -1,41 +1,5 @@
 #
-# $Log: dknewkey.sh,v $
-# Revision 1.12  2022-03-06 18:49:17+05:30  Cprogrammer
-# fix for FreeBSD (getopt usage).
-#
-# Revision 1.11  2021-09-11 18:55:36+05:30  Cprogrammer
-# changed owner/permissions of dkim private/public key pairs
-#
-# Revision 1.10  2021-08-24 11:29:21+05:30  Cprogrammer
-# check if domain exists in rcpthosts
-#
-# Revision 1.9  2021-08-23 17:34:19+05:30  Cprogrammer
-# fixed default_domain variable
-#
-# Revision 1.8  2021-08-21 21:33:14+05:30  Cprogrammer
-# fixed syntax error
-#
-# Revision 1.7  2021-08-19 19:54:39+05:30  Cprogrammer
-# added options to print, remove, generate DKIM keys
-#
-# Revision 1.6  2019-02-15 17:11:18+05:30  Cprogrammer
-# fixed checking of argument count
-#
-# Revision 1.5  2017-03-09 16:38:24+05:30  Cprogrammer
-# use full path of openssl
-#
-# Revision 1.4  2010-05-16 19:59:48+05:30  Cprogrammer
-# fix for Mac OS X
-#
-# Revision 1.3  2004-11-02 20:48:31+05:30  Cprogrammer
-# fixed error when dknewkey was called without arguments
-#
-# Revision 1.2  2004-10-21 21:54:25+05:30  Cprogrammer
-# create public key file
-#
-# Revision 1.1  2004-10-20 20:40:56+05:30  Cprogrammer
-# Initial revision
-#
+# $Id: dknewkey.sh,v 1.13 2022-10-02 21:55:50+05:30 Cprogrammer Exp mbhangui $
 #
 
 usage()
@@ -80,16 +44,54 @@ split_str()
 	done
 }
 
-print_key()
+gen_pub_key()
 {
+	local selector=$(basename $1)
+	local domain=$2
+	local pubkey=$3
 	if [ $bits -lt 2048 ] ; then
-		printf "%s._domainkey.%s. IN TXT (\"v=DKIM1; k=rsa; t=y; p=%s\")\n" "$1" "$2" "$3" > $1.pub
+		printf "%s._domainkey.%s. IN TXT (\"v=DKIM1; k=rsa; t=y; p=%s\")\n" "$selector" "$domain" "$pubkey" > $selector.pub
 	else
-		printf "%s._domainkey.%s. IN TXT (" "$1" "$2"
-		split_str "v=DKIM1; k=rsa; t=y; p=$3"
+		printf "%s._domainkey.%s. IN TXT (" "$selector" "$domain"
+		split_str "v=DKIM1; k=rsa; t=y; p=$pubkey"
 		printf ")\n"
 	fi
 }
+
+print_key()
+{
+	local domain=$1
+	local selector=$2
+	if [ -n "$selector" ] ; then
+		if [ -f $selector ] ; then
+			echo "DKIM Private key for $domain file $dir/$selector"
+			cat $selector
+			echo "------------------------------------------------------"
+		else
+			echo "DKIM Private key for $domain does not exist in $dir/$selector"
+			exit 2
+		fi
+		if [ -f $selector.pub ] ; then
+			echo "DKIM TXT record for $domain with selector=$selector file $dir/$selector.pub"
+			cat $selector.pub
+			echo "------------------------------------------------------"
+		else
+			echo "DKIM TXT record for $domain with selector=$selector does not exist in $dir/$selector.pub"
+			exit 2
+		fi
+	else
+		for i in $(find . -name '*'.pub -print)
+		do
+			selector=$(basename $i | cut -d. -f1)
+			t=$(echo $i | cut -c3-)
+			echo "DKIM TXT record for $domain with selector=$selector file $dir/$t"
+			cat $i
+			echo "------------------------------------------------------"
+		done
+		exit 0
+	fi
+}
+
 controldir=@qsysconfdir@/control
 SYSTEM=$(uname -s)
 if [ "$SYSTEM" = "FreeBSD" ] ; then
@@ -134,10 +136,6 @@ while :; do
 	domain="$2"
 	shift 2
 	;;
-	-s | --selector)
-	selector="$2"
-	shift 2
-	;;
 	-b | --bits)
 	bits="$2"
 	shift 2
@@ -167,40 +165,18 @@ if [ -z "$domain" ] ; then
 else
 	dir=$controldir/domainkeys/$domain
 fi
-if [ ! -d $dir ] ; then
-	if (! mkdir -p $dir || ! chown root:qmail $dir || ! chmod 755 $dir) ; then
-		exit 1
-	fi
-fi
-cd $dir
-if [ $? -ne 0 ] ; then
-	exit 2
-fi
+
 if [ $do_print -eq 1 ] ; then
-	if [ -n "$selector" ] ; then
-		if [ -f $selector.pub ] ; then
-			echo
-			echo "DKIM TXT record for $domain with selector=$selector file $dir/$selector.pub"
-			cat $selector.pub
-			echo "------------------------------------------------------"
-			exit 0
-		else
-			echo "DKIM TXT record for $domain with selector=$selector does not exist"
-			exit 2
-		fi
-	else
-		for i in $(find . -name '*'.pub -print)
-		do
-			selector=$(basename $i | cut -d. -f1)
-			t=$(echo $i | cut -c3-)
-			echo
-			echo "DKIM TXT record for $domain with selector=$selector file $dir/$t"
-			cat $i
-			echo "------------------------------------------------------"
-		done
-		exit 0
+	cd $dir
+	if [ $? -ne 0 ] ; then
+		exit 2
 	fi
+	print_key "$domain" "$selector"
 elif [ $remove -eq 1 ] ; then
+	cd $dir
+	if [ $? -ne 0 ] ; then
+		exit 2
+	fi
 	echo "Removing DKIM Keys $selector, $selector.pub"
 	/bin/rm -f $selector $selector.pub
 	files=$(ls)
@@ -209,13 +185,33 @@ elif [ $remove -eq 1 ] ; then
 		echo "Removing empty directory $domain"
 		rmdir --ignore-fail-on-non-empty $dir
 	fi
-	exit 0
 else
+	t=$(dirname $selector)
+	if [ "$t" != "$dir" -a "$t" != "." ] ; then
+		echo "WARNING!!!. Generating DKIM keys outside $dir"
+		dir=$t
+		selector=$(basename $selector)
+	fi
+	if [ ! -d $dir ] ; then
+		if (! mkdir -p $dir || ! chown root:qmail $dir || ! chmod 755 $dir) ; then
+			exit 1
+		fi
+	fi
+	cd $dir
+	if [ $? -ne 0 ] ; then
+		exit 2
+	fi
 	if [ -f $selector -a $force -eq 0 ] ; then
 		echo "DKIM private key $selector exists. Skipping private key generation" 1>&2
 		skip_priv_key=1
 	else
 		skip_priv_key=0
+	fi
+	if [ -f $selector -a $force -eq 0 ] ; then
+		echo "DKIM public  key $selector.pub exists. Skipping public  key generation" 1>&2
+		skip_pub_key=1
+	else
+		skip_pub_key=0
 	fi
 	err=$(mktemp -t dkkeyXXXXXXXXXX)
 	if [ $? -ne 0 ] ; then
@@ -226,35 +222,74 @@ else
 	exec 2>$err
 	exec 0<$err
 	/bin/rm -f $err
-	if [ $skip_priv_key -ne 1 ] ; then
-		echo "Generating DKIM private key selector=$selector keysize=$bits in $dir"
+	if [ $skip_priv_key -eq 0 ] ; then
+		echo "Generating DKIM private key keysize=$bits, file $dir/$selector"
 		/usr/bin/openssl genrsa -out $selector $bits
 		if [ $? -ne 0 ] ; then
 			/bin/cat
 			exit 1
 		fi
 	fi
-	pubkey=$(/usr/bin/openssl rsa -in $selector -pubout -outform PEM | grep -v '^--' | tr -d '\n')
-	if [ $? -ne 0 ] ; then
-		/bin/cat
-		exit 1
+	if [ $skip_pub_key -eq 0 ] ; then
+		pubkey=$(/usr/bin/openssl rsa -in $selector -pubout -outform PEM | grep -v '^--' | tr -d '\n')
+		if [ $? -ne 0 ] ; then
+			/bin/cat
+			exit 1
+		fi
+		echo "Generating DKIM public  key keysize=$bits for $selector.domainkey.$domain, file $dir/$selector.pub"
+		gen_pub_key "$selector" "$domain" "$pubkey" > $selector.pub
+		if [ $? -ne 0 ] ; then
+			/bin/cat
+			exit 1
+		fi
 	fi
 	exec 2>&3 # restore stderr
-	echo "Generating DKIM public key selector=$selector keysize=$bits in $dir"
-	print_key "$selector" "$domain" "$pubkey" > $selector.pub
-	if [ $? -ne 0 ] ; then
-		exit 1
+	if [ $skip_priv_key -eq 0 -o $skip_pub_key -eq 0 ] ; then
+		if ( ! chown root:qmail $selector $selector.pub || ! chmod 640 $selector || ! chmod 644 $selector.pub) ; then
+			exit 1
+		fi
 	fi
-	if ( ! chown root:qmail $selector $selector.pub || ! chmod 640 $selector || ! chmod 644 $selector.pub) ; then
-		exit 1
-	fi
-	if [ -f $selector ] ; then
-		echo "DKIM TXT record for $domain with selector=$selector in $dir"
-		cat $selector.pub
-		echo "------------------------------------------------------"
-		exit 0
-	else
-		echo "DKIM TXT record for $domain with selector=$selector does not exist in $dir"
-		exit 2
-	fi
+	print_key "$domain" "$selector"
 fi
+exit 0
+
+#
+# $Log: dknewkey.sh,v $
+# Revision 1.13  2022-10-02 21:55:50+05:30  Cprogrammer
+# refactored code
+#
+# Revision 1.12  2022-03-06 18:49:17+05:30  Cprogrammer
+# fix for FreeBSD (getopt usage).
+#
+# Revision 1.11  2021-09-11 18:55:36+05:30  Cprogrammer
+# changed owner/permissions of dkim private/public key pairs
+#
+# Revision 1.10  2021-08-24 11:29:21+05:30  Cprogrammer
+# check if domain exists in rcpthosts
+#
+# Revision 1.9  2021-08-23 17:34:19+05:30  Cprogrammer
+# fixed default_domain variable
+#
+# Revision 1.8  2021-08-21 21:33:14+05:30  Cprogrammer
+# fixed syntax error
+#
+# Revision 1.7  2021-08-19 19:54:39+05:30  Cprogrammer
+# added options to print, remove, generate DKIM keys
+#
+# Revision 1.6  2019-02-15 17:11:18+05:30  Cprogrammer
+# fixed checking of argument count
+#
+# Revision 1.5  2017-03-09 16:38:24+05:30  Cprogrammer
+# use full path of openssl
+#
+# Revision 1.4  2010-05-16 19:59:48+05:30  Cprogrammer
+# fix for Mac OS X
+#
+# Revision 1.3  2004-11-02 20:48:31+05:30  Cprogrammer
+# fixed error when dknewkey was called without arguments
+#
+# Revision 1.2  2004-10-21 21:54:25+05:30  Cprogrammer
+# create public key file
+#
+# Revision 1.1  2004-10-20 20:40:56+05:30  Cprogrammer
+# Initial revision
