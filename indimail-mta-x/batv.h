f41be876319da7ce81587e1d49350a2670f944d1
Manvendra Bhangui <mbhangui@gmail.com>
2022-10-13 20:30:17 +0530

    header for batv functions

---
+++ indimail-mta-x/batv.h
@@ -0,0 +1,16 @@
+/*
+ * $Log: batv.h,v $
+ * Revision 1.1  2022-10-13 18:35:36+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef _BATV_H
+#define _BATV_H
+
+#define BATVLEN          3 /* number of bytes */
+#define BATV_OK          0
+#define BATV_NOSIGNATURE 1
+#define BATV_BADFORMAT   2
+#define BATV_STALE       3
+#endif
+
