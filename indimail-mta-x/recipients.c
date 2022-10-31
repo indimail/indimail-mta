4965c62bbf8cf5f2e593da1f0d15ff2a469667ef
Manvendra Bhangui <mbhangui@gmail.com>
2022-10-31 23:19:44 +0530

    fix for SRS addresses

--- indimail-mta-x/recipients.c
+++ indimail-mta-x/recipients.c
@@ -1,13 +1,8 @@
 /*
  * $Log: recipients.c,v $
- * Revision 1.10  2022-04-20 23:15:43+05:30  Cprogrammer
- * replaced multiple stralloc_0 with single stralloc_catb
- *
- * Revision 1.9  2020-05-11 11:11:36+05:30  Cprogrammer
- * fixed shadowing of global variables by local variables
- *
- * Revision 1.8  2017-10-05 08:51:20+05:30  Cprogrammer
- * use /etc/indimail/users/recipients instead of /etc/indimail/control/recipients
+ * Revision 1.8  2022-10-31 23:17:19+05:30  Cprogrammer
+ * refactored code to collaps stralloc, substdio statements.
+ * fix for SRS addresses
  *
  * Revision 1.7  2011-01-14 22:19:52+05:30  Cprogrammer
  * upgrade to verion 0.7 of EH's recipients extension
@@ -34,7 +29,6 @@
 #include <unistd.h>
 #include "cdb.h"
 #include "auto_break.h"
-#include "auto_assign.h"
 #include "case.h"
 #include "uint32.h"
 #include "byte.h"
@@ -49,7 +43,8 @@
 #include "wait.h"
 #include "substdio.h"
 #include "fd.h"
-#include "env.h"
+
+#define FDAUTH 3
 
 static stralloc key = { 0 };
 static stralloc domain = { 0 };
@@ -75,29 +70,21 @@ static int      fdrcps;
 extern ssize_t  safewrite(int, char *, int);
 
 char            ssrcptbuf[512];
-substdio        ssrcpt = SUBSTDIO_FDBUF(safewrite, 3, ssrcptbuf, sizeof (ssrcptbuf));
+substdio        ssrcpt = SUBSTDIO_FDBUF(safewrite, FDAUTH, ssrcptbuf, sizeof (ssrcptbuf));
 
 int
 callapam(pam, addr)
 	char           *pam;
 	char           *addr;
 {
-	int             i;
-	int             j = 0;
-	int             wstat;
+	int             i, j = 0, wstat, child;
 	int             pi[2];
-	int             child;
 	char            ch;
 	static stralloc mailaddress = { 0 };
-
 	char           *childargs[7] = { 0, 0, 0, 0, 0, 0, 0 };
-	stralloc        pamarg = { 0 };
-	stralloc        pamname = { 0 };
-	stralloc        pamarg1 = { 0 };
-	stralloc        pamarg2 = { 0 };
-	stralloc        pamarg3 = { 0 };
-	stralloc        pamarg4 = { 0 };
-	stralloc        pamarg5 = { 0 };
+	stralloc        pamarg = {0}, pamname = {0}, pamarg1 = {0},
+					pamarg2 = {0}, pamarg3 = {0}, pamarg4 = {0},
+					pamarg5 = {0};
 
 	for (i = 0; (ch = pam[i]); i++) {
 		if (j < 6) {
@@ -139,19 +126,19 @@ callapam(pam, addr)
 		}
 	}
 	childargs[j] = 0;
-
-	close(3);
+	close(FDAUTH);
 	if (pipe(pi) == -1)
 		return -3;
-	if (pi[0] != 3)
+	if (pi[0] != FDAUTH)
 		return -3;
 
-	switch (child = fork()) {
+	switch (child = fork())
+	{
 	case -1:
 		return -3;
 	case 0:
 		close(pi[1]);
-		if (fd_copy(3, pi[0]) == -1)
+		if (fd_copy(FDAUTH, pi[0]) == -1)
 			return -3;
 		sig_pipedefault();
 		execvp(childargs[0], childargs);
@@ -159,24 +146,19 @@ callapam(pam, addr)
 	}
 	close(pi[0]);
 
-/*- checkpassword compliant form: address\0\0\0 */
-
+	/*- checkpassword compliant form: address\0\0\0 */
 	mailaddress.len = 0;
-	if (!stralloc_copys(&mailaddress, addr))
-		return -2;
-	if (!stralloc_catb(&mailaddress, "\0\0\0", 3))
+	if (!stralloc_copys(&mailaddress, addr) ||
+			!stralloc_catb(&mailaddress, "\0\0\0", 3))
 		return -2;
 
 	substdio_fdbuf(&ssrcpt, write, pi[1], ssrcptbuf, sizeof ssrcptbuf);
-	if (substdio_put(&ssrcpt, mailaddress.s, mailaddress.len) == -1)
-		return -3;
-	if (substdio_flush(&ssrcpt) == -1)
+	if (substdio_put(&ssrcpt, mailaddress.s, mailaddress.len) == -1 ||
+			substdio_flush(&ssrcpt) == -1)
 		return -3;
 	close(pi[1]);
-
-	if (wait_pid(&wstat, child) == -1)
-		return -3;
-	if (wait_crashed(wstat))
+	if (wait_pid(&wstat, child) == -1 ||
+			wait_crashed(wstat))
 		return -3;
 	return wait_exitcode(wstat);
 }
@@ -184,20 +166,7 @@ callapam(pam, addr)
 int
 recipients_init()
 {
-	static stralloc controlfile = {0};
-	char           *assigndir;
-
-	assigndir = (assigndir = env_get("ASSIGNDIR")) ? assigndir : auto_assign;
-	if (!stralloc_copys(&controlfile, assigndir))
-		return(-1);
-	if (controlfile.s[controlfile.len - 1] != '/' && !stralloc_cats(&controlfile, "/"))
-		return(-1);
-	if (!stralloc_catb(&controlfile, "recipients", 10))
-		return(-1);
-	if (!stralloc_0(&controlfile))
-		return(-1);
-	flagrcpts = control_readfile(&rcptline, controlfile.s, 0);
-	if (flagrcpts != 1)
+	if ((flagrcpts = control_readfile(&rcptline, "recipients", 0)) != 1)
 		return flagrcpts;
 	return 0;
 }
@@ -205,22 +174,16 @@ recipients_init()
 int
 recipients_parse(char *rhost, int rlen, char *addr, char *rkey, int klen, char *vaddr, char *v_key, int vlen)
 {
-	int             i;
-	int             r;
-	int             j = 0;
-	int             k = 0;
+	int             i, r, j = 0, k = 0, seenhost = 0;
 	uint32          dlen;
 	static stralloc line = { 0 };
-	int             seenhost = 0;
 
 	wildhost.len = 0;
 	line.len = 0;
 
-	if (!stralloc_copys(&wildhost, "!"))
-		return -2;
-	if (!stralloc_cats(&wildhost, rhost))
-		return -2;
-	if (!stralloc_0(&wildhost))
+	if (!stralloc_copys(&wildhost, "!") ||
+			!stralloc_cats(&wildhost, rhost) ||
+			!stralloc_0(&wildhost))
 		return -2;
 
 	for (i = 0; i < rcptline.len; ++i) {
@@ -233,24 +196,25 @@ recipients_parse(char *rhost, int rlen, char *addr, char *rkey, int klen, char *
 			j = byte_chr(line.s, line.len, ':');
 			k = byte_chr(line.s, line.len, '|');
 			if (!str_diffn(line.s, wildhost.s, wildhost.len - 1))
-				return 3;	/*- wilddomain */
-			if (j > 0 || k > 0)
+				return 3; /*- wilddomain */
+
+			if (j > 0 || k > 0) {
 				if (!str_diffn(line.s, "@", 1))	/*- exact */
 					if (!str_diffn(line.s + 1, rhost, rlen - 1))
 						seenhost = 1;
-			if (!seenhost) {	/*- domain */
-				if (j > 0 && rlen >= j)
-					if (!str_diffn(line.s, rhost + rlen - j - 1, j - 1))
-						seenhost = 2;
-				if (k > 0 && rlen >= k)
-					if (!str_diffn(line.s, rhost + rlen - k - 1, k - 1))
-						seenhost = 3;
 			}
-			if (!seenhost)	/*- pass-thru */
-				if (!str_diffn(line.s, "!*", 2))
-					return 4;
+			if (!seenhost) { /*- domain */
+				if (j > 0 && rlen >= j &&
+						!str_diffn(line.s, rhost + rlen - j - 1, j - 1))
+					seenhost = 2;
+				if (k > 0 && rlen >= k &&
+						!str_diffn(line.s, rhost + rlen - k - 1, k - 1))
+					seenhost = 3;
+			}
+			if (!seenhost && !str_diffn(line.s, "!*", 2)) /*- pass-thru */
+				return 4;
 
-			if (k > 0 && k < line.len)	/*- pam */
+			if (k > 0 && k < line.len) { /*- pam */
 				if (seenhost || !str_diffn(line.s, "*", 1)) {
 					r = callapam(line.s + k + 1, addr);
 					if (vlen > 0 && r != 0)
@@ -260,8 +224,9 @@ recipients_parse(char *rhost, int rlen, char *addr, char *rkey, int klen, char *
 					if (r == 111)
 						return r;
 				}
+			}
 
-			if (j > 0 && j < line.len)	/*- cdb */
+			if (j > 0 && j < line.len) { /*- cdb */
 				if (seenhost || !str_diffn(line.s, "*", 1)) {
 					fdrcps = open_read(line.s + j + 1);
 					if (fdrcps != -1) {
@@ -273,6 +238,7 @@ recipients_parse(char *rhost, int rlen, char *addr, char *rkey, int klen, char *
 							return 1;
 					}
 				}
+			}
 
 			if (!seenhost) {
 				fdrcps = open_read(line.s);	/*- legacy cdb */
@@ -293,70 +259,51 @@ recipients_parse(char *rhost, int rlen, char *addr, char *rkey, int klen, char *
 }
 
 int
-recipients(buf, len)
-	char           *buf;
-	int             len;
+recipients(char *buf, int len)
 {
-	int             at;
-	int             i;
-	int             r;
+	int             at, i, r;
 
 	if (flagrcpts != 1)
 		return 10;
-	address.len = 0;			/*- multiple recipients */
+	address.len = 0; /*- multiple recipients */
 	domain.len = 0;
 	at = byte_rchr(buf, len, '@');
 	if (at < len) {
-		if (!stralloc_copyb(&domain, buf + at + 1, len - at - 1))
-			return -2;
-		if (!stralloc_copyb(&address, buf, len))
+		if (!stralloc_copyb(&domain, buf + at + 1, len - at - 1) ||
+				!stralloc_copyb(&address, buf, len))
 			return -2;
 	} else {
-		if (!stralloc_copyb(&address, buf, len))
-			return -2;
-		if (!stralloc_append(&address, "@"))
-			return -2;
-		if (!stralloc_copys(&domain, "localhost"))
-			return -2;
-		if (!stralloc_cat(&address, &domain))
+		if (!stralloc_copyb(&address, buf, len) ||
+				!stralloc_append(&address, "@") ||
+				!stralloc_copys(&domain, "localhost") ||
+				!stralloc_cat(&address, &domain))
 			return -2;
 	}
-	if (!stralloc_0(&address))
+	if (!stralloc_0(&address) ||
+			!stralloc_0(&domain))
 		return -2;
-	if (!stralloc_0(&domain))
-		return -2;
-	key.len = 0;
-	if (!stralloc_copys(&key, ":"))
-		return -2;
-	if (!stralloc_cat(&key, &address))
-		return -2;
-	if (!stralloc_0(&key))
-		return -2;				/* \0\0 terminated */
+	if (!stralloc_copys(&key, ":") ||
+			!stralloc_cat(&key, &address) ||
+			!stralloc_0(&key))
+		return -2; /* \0\0 terminated */
 	case_lowerb(key.s, key.len);
 	case_lowerb(domain.s, domain.len);
-	vkey.len = 0;
-	verp.len = 0;
+
 	for (i = 0; i < at; i++) {
-		if (buf[i] == *auto_break) {
-			if (!stralloc_copyb(&verp, buf, i + 1))
-				return -2;
-			if (!stralloc_append(&verp, "@"))
-				return -2;
-			if (!stralloc_cat(&verp, &domain))
-				return -2;
-			if (!stralloc_copys(&vkey, ":"))
-				return -2;
-			if (!stralloc_cat(&vkey, &verp))
-				return -2;
-			if (!stralloc_0(&vkey))
+    	if (buf[i] == *auto_break || buf[i] == '=' || buf[i] == '+') { /* SRS delimiter */
+			if (!stralloc_copyb(&verp, buf, i + 1) ||
+					!stralloc_append(&verp, "@") ||
+					!stralloc_cat(&verp, &domain) ||
+					!stralloc_copys(&vkey, ":") ||
+					!stralloc_cat(&vkey, &verp) ||
+					!stralloc_0(&vkey))
 				return -2;		/* \0\0 terminated */
 			case_lowerb(vkey.s, vkey.len);
 			break;
 		}
 	}
 
-	r = recipients_parse(domain.s, domain.len, address.s, key.s, key.len, verp.s, vkey.s, vkey.len);
-	if (r)
+	if ((r = recipients_parse(domain.s, domain.len, address.s, key.s, key.len, verp.s, vkey.s, vkey.len)))
 		return r;
 	return 0;
 }
@@ -364,7 +311,7 @@ recipients(buf, len)
 void
 getversion_recipients_c()
 {
-	static char    *x = "$Id: recipients.c,v 1.10 2022-04-20 23:15:43+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: recipients.c,v 1.8 2022-10-31 23:17:19+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
