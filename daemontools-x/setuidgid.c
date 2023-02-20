d746f14c58df806add54c0fc18a6517ab72655cc
Manvendra Bhangui <mbhangui@gmail.com>
2023-02-20 18:38:24 +0530

    setuidgid.c: moved set_additional_groups function to libqmail

--- daemontools-x/setuidgid.c
+++ daemontools-x/setuidgid.c
@@ -1,23 +1,5 @@
 /*
- * $Log: setuidgid.c,v $
- * Revision 1.6  2021-09-11 10:20:02+05:30  Cprogrammer
- * set additional groups using -g option
- *
- * Revision 1.5  2020-09-16 19:06:56+05:30  Cprogrammer
- * FreeBSD fix
- *
- * Revision 1.4  2020-06-16 23:57:33+05:30  Cprogrammer
- * added option -s to set supplementary groups
- *
- * Revision 1.3  2020-06-16 22:36:48+05:30  Cprogrammer
- * set supplementary group ids
- *
- * Revision 1.2  2004-10-22 20:30:17+05:30  Cprogrammer
- * added RCS id
- *
- * Revision 1.1  2003-12-31 18:37:58+05:30  Cprogrammer
- * Initial revision
- *
+ * $Id: setuidgid.c,v 1.7 2023-02-20 18:37:54+05:30 Cprogrammer Exp mbhangui $
  */
 #include <sys/types.h>
 #include <unistd.h>
@@ -30,53 +12,23 @@
 #include <strerr.h>
 #include <sgetopt.h>
 #include <prot.h>
+#include <isnum.h>
 #include <pathexec.h>
 #include <alloc.h>
 #include <scan.h>
 
 #define FATAL "setuidgid: fatal: "
 
-void
-set_additional_groups(char *groups)
-{
-	struct group   *gr;
-	char           *ptr, *cptr;
-	int             ngroups, i = 0;
-	gid_t          *gidset;
-
-	for (ptr = groups, ngroups = 0; *ptr; ptr++) {
-		if (*ptr == ',')
-			ngroups++;
-	}
-	ngroups++;
-	if (!(gidset = (gid_t *) alloc(ngroups * sizeof(gid_t))))
-		strerr_die2x(111, FATAL, "out of memory");
-	for (ptr = cptr = groups; *ptr; ptr++) {
-		if (*ptr == ',') {
-			*ptr = 0;
-			if (!(gr = getgrnam(cptr)))
-				strerr_die3x(111, FATAL, "unknown group: ", cptr);
-			gidset[i++] = gr->gr_gid;
-			*ptr = ',';
-			cptr = ptr + 1;
-		}
-	}
-	if (!(gr = getgrnam(cptr)))
-		strerr_die3x(111, FATAL, "unknown group: ", cptr);
-	gidset[i++] = gr->gr_gid;
-	if (setgroups(ngroups, gidset))
-		strerr_die2sys(111, FATAL, "unable to setgroups: ");
-	return;
-}
-
 int
 main(int argc, char **argv, char **envp)
 {
 	struct passwd  *pw;
-	gid_t          *gidset;
-	char           *account, *groups = 0, *usage = "usage: setuidgid [-s] account child";
+	struct group   *gr;
+	gid_t          *gidset, g;
+	char           *ptr, *cptr, *account, *groups = 0,
+				   *usage = "usage: setuidgid [-s] [-g gid_list] account child";
 	char          **child;
-	int             ngroups = 0, opt;
+	int             i, t, ngroups = 0, opt, old;
 
 	while ((opt = getopt(argc, argv, "sg:")) != opteof) {
 		switch (opt)
@@ -98,17 +50,51 @@ main(int argc, char **argv, char **envp)
 	if (!(pw = getpwnam(account)))
 		strerr_die3x(111, FATAL, "unknown account ", account);
 	if (prot_gid(pw->pw_gid) == -1)
-		strerr_die2sys(111, FATAL, "unable to setgid: ");
+		strerr_die2sys(111, FATAL, "unable to set group id: ");
 	if (ngroups) {
 		if (!(gidset = grpscan(account, &ngroups)))
-			strerr_die2sys(111, FATAL, "unable to do groupscan: ");
-		if (setgroups(ngroups, gidset))
-			strerr_die2sys(111, FATAL, "unable to setgroups: ");
+			strerr_die2sys(111, FATAL, "unable to get groups: ");
 	}
-	if (groups)
-		set_additional_groups(groups);
+	if (groups) {
+		if ((t = sysconf(_SC_NGROUPS_MAX)) == -1)
+			strerr_die2sys(111, FATAL, "unable to get sysconf NGROUPS_MAX: ");
+		old = ngroups;
+		for (ptr = groups; *ptr; ptr++) {
+			if (*ptr == ',')
+				ngroups++;
+		}
+		ngroups++;
+		if (!alloc_re((char *) &gidset, t * sizeof(gid_t), ngroups * sizeof(gid_t)))
+			return -1;
+		for (i = old, ptr = cptr = groups; *ptr; ptr++) {
+			if (*ptr == ',') {
+				*ptr = 0;
+				if (!isnum(cptr)) {
+					if (!(gr = getgrnam(cptr)))
+						strerr_die3x(111, FATAL, "unknown group ", cptr);
+					g = gr->gr_gid;
+				} else
+					scan_uint(cptr, &g);
+				gidset[i++] = g;
+				*ptr = ',';
+				cptr = ptr + 1;
+			}
+		}
+		if (!isnum(cptr)) {
+			if (!(gr = getgrnam(cptr)))
+				strerr_die3x(111, FATAL, "unknown group ", cptr);
+			g = gr->gr_gid;
+		} else
+			scan_uint(cptr, &g);
+		gidset[i++] = g;
+	}
+	if (ngroups) {
+		if (setgroups(ngroups, gidset))
+			strerr_die2sys(111, FATAL, "unable to add addition group ids: ");
+		alloc_free((char *) gidset);
+	}
 	if (prot_uid(pw->pw_uid) == -1)
-		strerr_die2sys(111, FATAL, "unable to setuid: ");
+		strerr_die2sys(111, FATAL, "unable to set user id: ");
 	pathexec_run(*child, child, envp);
 	strerr_die4sys(111, FATAL, "unable to run ", *child, ": ");
 	/*- Not reached */
@@ -118,7 +104,35 @@ main(int argc, char **argv, char **envp)
 void
 getversion_setuidgid_c()
 {
-	static char    *x = "$Id: setuidgid.c,v 1.6 2021-09-11 10:20:02+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: setuidgid.c,v 1.7 2023-02-20 18:37:54+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
+
+/*
+ * $Log: setuidgid.c,v $
+ * Revision 1.7  2023-02-20 18:37:54+05:30  Cprogrammer
+ * moved set_additional_groups function to libqmail
+ *
+ * Revision 1.7  2023-02-20 09:55:09+05:30  Cprogrammer
+ * moved set_additional_groups function to libqmail
+ *
+ * Revision 1.6  2021-09-11 10:20:02+05:30  Cprogrammer
+ * set additional groups using -g option
+ *
+ * Revision 1.5  2020-09-16 19:06:56+05:30  Cprogrammer
+ * FreeBSD fix
+ *
+ * Revision 1.4  2020-06-16 23:57:33+05:30  Cprogrammer
+ * added option -s to set supplementary groups
+ *
+ * Revision 1.3  2020-06-16 22:36:48+05:30  Cprogrammer
+ * set supplementary group ids
+ *
+ * Revision 1.2  2004-10-22 20:30:17+05:30  Cprogrammer
+ * added RCS id
+ *
+ * Revision 1.1  2003-12-31 18:37:58+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
