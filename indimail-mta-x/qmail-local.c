155f8bc8ee0aea3ab86402160e1e30cf5cb13fe3
Manvendra Bhangui <mbhangui@gmail.com>
2023-09-23 00:24:20 +0530

    updated indimail-mta
    1. svctool: Skip MYSQL_OPT_RECONNECT for mysql version > 8.0.34
    2. qmail-local.c: added MESSID to the environment variables to be preserved

--- indimail-mta-x/qmail-local.c
+++ indimail-mta-x/qmail-local.c
@@ -1,5 +1,5 @@
 /*
- * $Id: qmail-local.c,v 1.48 2023-09-20 21:26:31+05:30 Cprogrammer Exp mbhangui $
+ * $Id: qmail-local.c,v 1.49 2023-09-23 00:05:49+05:30 Cprogrammer Exp mbhangui $
  */
 #include <sys/types.h>
 #include <sys/time.h>
@@ -588,6 +588,7 @@ sanitize_env(char *x)
 		{ "USE_FSYNC", 0 },
 		{ "USE_FDATASYNC", 0 },
 		{ "USE_SYNCDIR", 0 },
+		{ "MESSID", 0 },
 		{ 0, 0 }
 	};
 	struct env_tab *p, *q;
@@ -983,7 +984,7 @@ main(int argc, char **argv)
 void
 getversion_qmail_local_c()
 {
-	static char    *x = "$Id: qmail-local.c,v 1.48 2023-09-20 21:26:31+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: qmail-local.c,v 1.49 2023-09-23 00:05:49+05:30 Cprogrammer Exp mbhangui $";
 
 	x = sccsidmyctimeh;
 	x++;
@@ -991,6 +992,9 @@ getversion_qmail_local_c()
 
 /*
  * $Log: qmail-local.c,v $
+ * Revision 1.49  2023-09-23 00:05:49+05:30  Cprogrammer
+ * added MESSID to the environment variables to be preserved
+ *
  * Revision 1.48  2023-09-20 21:26:31+05:30  Cprogrammer
  * added '%' option in dot-qmail to add env variables from a directory
  *
