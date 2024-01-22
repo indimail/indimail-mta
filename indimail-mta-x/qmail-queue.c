93f1a0096fe1c1ede3bab11f4e0749fc4eca50c1
Manvendra Bhangui <mbhangui@gmail.com>
2024-01-23 01:11:51 +0530

    updated logbuf size

--- indimail-mta-x/qmail-queue.c
+++ indimail-mta-x/qmail-queue.c
@@ -62,7 +62,7 @@ static char    *tcpremoteip;
 static char    *received;
 static char    *pidfn, *messfn, *todofn, *intdfn;
 static char    *origin; /* "X-Originating-IP: 10.0.0.1\n" */
-static char     inbuf[BUFSIZE_IN], outbuf[BUFSIZE_OUT], logbuf[BUFSIZE_IN];
+static char     inbuf[BUFSIZE_IN], outbuf[BUFSIZE_OUT], logbuf[BUFSIZE_OUT];
 static int      messfd, intdfd, match;
 static int      flagmademess = 0;
 static int      flagmadeintd = 0;
