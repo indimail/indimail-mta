ae5ee276d81fff99475f8094f62d39c3dee9d968
Manvendra Bhangui <mbhangui@gmail.com>
2024-02-20 23:16:50 +0530

    add user defined filters in control/filters.d (#63)
    * add user defined filters in control/filters.d
    
    1. indimail-mta.spec, DIRS.in: added control/filters.d for user defined
       filters
    2. qmail-multi (mailfilter.c): added exit code 99 (blackhole) for
       compatibility with qmail-qfilter
    3. qfrontend, qf-log-subject, qf-smtp-ratelimit: make script compatible
       with FILTERARGS as well as qmail-qfilter
    4. qfrontend: use control/filters.d directory for user defined scripts
    5. qmail-qfilter.c: added exit code 2 (blackhole) for compatibility with
       qmail-multi
    6. qmail-showctl.c: added display of user filter directory control/filters.d
    
    * updated ChangeLog

--- indimail-mta-x/qfrontend.sh
+++ indimail-mta-x/qfrontend.sh
@@ -1,5 +1,9 @@
 #
 # $Log: qfrontend.sh,v $
+# Revision 1.5  2024-02-20 22:21:46+05:30  Cprogrammer
+# use control/filters.d directory for user defined filters
+# make script compatible with both FILTERARGS and qmail-qfilter
+#
 # Revision 1.4  2024-02-19 22:46:16+05:30  Cprogrammer
 # skip comments
 #
@@ -14,19 +18,61 @@
 #
 #
 qf=""
+if [ -n "$USE_QMAILQFILTER" ] ; then
+	qqfilter=1
+else
+	qqfilter=0
+fi
+unset SPAMFILTER
+unset FILTERARGS
 [ -n "$CONTROLDIR" ] && controldir=$CONTROLDIR || controldir=@controldir@
 [ -n "$LIBEXECDIR" ] && libexecdir=$LIBEXECDIR || libexecdir=@libexecdir@
-if [ ! -f $controldir/qfilters ] ; then
-	exec qmail-multi
-fi
-for i in $(grep -v "^#" $controldir/qfilters)
-do
-	j=$libexecdir/qfilters/$i
-	if [ -n "$qf" ] ; then
-		qf="$qf -- $j"
-	else
-		qf="$j"
+if [ -d $controldir/filters.d ] ; then
+	for i in $(find $controldir/filters.d -maxdepth 1 -type f)
+	do
+		if [ $qqfilter -eq 1 ] ; then
+			if [ -n "$qf" ] ; then
+				qf="$qf -- $i"
+			else
+				qf="$i"
+			fi
+		else
+			if [ -n "$qf" ] ; then
+				qf="$qf | $i"
+			else
+				qf=$i
+			fi
+		fi
+	done
+else
+	if [ ! -f $controldir/qfilters ] ; then
+		exec @prefix@/sbin/qmail-queue
 	fi
-done
-eval qmail-qfilter $qf
+	for i in $(grep -v "^#" $controldir/qfilters)
+	do
+		j=$libexecdir/qfilters/$i
+		if [ $qqfilter -eq 1 ] ; then
+			if [ -n "$qf" ] ; then
+				qf="$qf -- $j"
+			else
+				qf="$j"
+			fi
+		else
+			if [ -n "$qf" ] ; then
+				qf="$qf | $j"
+			else
+				qf=$j
+			fi
+		fi
+	done
+fi
+if [ -z "$qf" ] ; then
+	exec @prefix@/sbin/qmail-queue
+else
+	if [ $qqfilter -eq 1 ] ; then
+		exec @prefix@/bin/qmail-qfilter $qf
+	else
+		FILTERARGS="$qf" exec @prefix@/sbin/qmail-multi
+	fi
+fi
 exit $?
