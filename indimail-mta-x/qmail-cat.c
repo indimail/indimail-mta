5c05ced65c2b4795518c0ba262b8e11dfb63c4ad
Manvendra Bhangui <mbhangui@gmail.com>
2024-02-21 22:52:59 +0530

    qmail-cat.c: added -e, -t, -v options similar to gnu cat

--- indimail-mta-x/qmail-cat.c
+++ indimail-mta-x/qmail-cat.c
@@ -1,36 +1,5 @@
 /*
- * $Log: qmail-cat.c,v $
- * Revision 1.10  2024-01-23 01:24:23+05:30  Cprogrammer
- * include buffer_defs.h for buffer size definitions
- *
- * Revision 1.9  2021-10-20 23:30:20+05:30  Cprogrammer
- * added SIGTERM handler to flush io
- *
- * Revision 1.8  2021-08-29 23:27:08+05:30  Cprogrammer
- * define functions as noreturn
- *
- * Revision 1.7  2009-10-03 15:02:59+05:30  Cprogrammer
- * process all arguments on command line
- * bug fix in my_error - 2nd arg never printed
- *
- * Revision 1.6  2009-03-31 09:07:55+05:30  Cprogrammer
- * open files other than fifo in O_RDONLY
- *
- * Revision 1.5  2004-10-27 17:52:37+05:30  Cprogrammer
- * stralloc_0() not needed if substdio_bput() is used instead of substdio_bputs()
- *
- * Revision 1.4  2004-10-22 20:28:10+05:30  Cprogrammer
- * added RCS id
- *
- * Revision 1.3  2004-07-15 23:31:56+05:30  Cprogrammer
- * fixed compilation warning
- *
- * Revision 1.2  2004-01-10 09:45:07+05:30  Cprogrammer
- * close file descriptor
- *
- * Revision 1.1  2004-01-07 21:09:03+05:30  Cprogrammer
- * Initial revision
- *
+ * $Id: qmail-cat.c,v 1.11 2024-02-21 20:41:45+05:30 Cprogrammer Exp mbhangui $
  */
 #include <unistd.h>
 #include <fcntl.h>
@@ -38,9 +7,11 @@
 #include <sys/stat.h>
 #include <substdio.h>
 #include <stralloc.h>
+#include <str.h>
 #include <getln.h>
 #include <error.h>
 #include <sig.h>
+#include <sgetopt.h>
 #include <noreturn.h>
 #include "buffer_defs.h"
 
@@ -93,44 +64,150 @@ sigterm()
 	_exit(0);
 }
 
-int
-main(int argc, char **argv)
+void
+convert(char *inbuf, int len, int show_nonprinting, int show_tabs, int show_ends)
+{
+	int             i;
+	char           ch;
+
+	for (i = 0; i < len; i++) {
+		ch = inbuf[i];
+		if (show_nonprinting) {
+			if (ch >= 32) {
+				if (ch < 127)
+					substdio_bput(&ssout, inbuf + i, 1);
+				else
+				if (ch == 127) {
+					substdio_bput(&ssout, "^?", 2);
+				} else {
+					substdio_bput(&ssout, "M-", 2);
+					if (ch >= 128 + 32) {
+						if (ch < 128 + 127) {
+							inbuf[i] -= 128;
+							substdio_bput(&ssout, inbuf + i, 1);
+						} else 
+							substdio_bput(&ssout, "^?", 2);
+					} else {
+						substdio_bput(&ssout, "^", 1);
+						inbuf[i] -= 64;
+						substdio_bput(&ssout, inbuf + i, 1);
+					}
+				}
+			} else
+			if (ch == '\t' && !show_tabs)
+				substdio_bput(&ssout, inbuf + i, 1);
+			else
+			if (ch == '\n') {
+				if (show_ends)
+					substdio_bput(&ssout, "$\n", 2);
+				else
+					substdio_bput(&ssout, "\n", 1);
+			}
+			else {
+				substdio_bput(&ssout, "^", 1);
+				inbuf[i] += 64;
+				substdio_bput(&ssout, inbuf + i, 1);
+			}
+		} else {
+			/*- Not quoting, -v not specified. */
+			if (ch == '\t' && show_tabs) {
+				substdio_bput(&ssout, "^", 1);
+				inbuf[i] += 64;
+				substdio_bput(&ssout, inbuf + i, 1);
+			} else
+			if (ch == '\r') {
+				if (i < len && inbuf[i + 1] == '\n')
+					substdio_bput(&ssout, "^M", 2);
+				else {
+					if (show_ends)
+						substdio_bput(&ssout, "^M$\n", 4);
+					else
+						substdio_bput(&ssout, "^M\n", 3);
+				}
+			} else
+			if (ch == '\n') {
+				if (show_ends)
+					substdio_bput(&ssout, "$\n", 2);
+				else
+					substdio_bput(&ssout, "\n", 1);
+			} else
+				substdio_bput(&ssout, inbuf + i, 1);
+		}
+	}
+}
+
+void
+output(int show_nonprinting, int show_tabs, int show_ends)
 {
 	stralloc        line = { 0 };
-	struct stat     st;
-	int             i, match, fd, mode = O_RDONLY;
+	int             match;
 
-	if (argc == 1) {
-		logerr("USAGE: ");
-		logerr(argv[0]);
-		logerrf(" filename\n");
-		_exit(1);
-	}
-	sig_termcatch(sigterm);
-	for (i = 1;i < argc;i++) {
-		if (stat(argv[i], &st) == -1)
-			my_error("qmail-cat: stat", argv[i], 1);
-		if ((st.st_mode & S_IFMT) == S_IFIFO)
-			mode = O_RDWR;
-		if ((fd = open(argv[i], mode)) == -1)
-			my_error("qmail-cat: open", argv[i], 1);
-		if (dup2(fd, 0))
-			my_error("qmail-cat: dup2", 0, 1);
-		if (fd && close(fd))
-			my_error("qmail-cat: close", 0, 1);
-		for (;;) {
-			if (getln(&ssin, &line, &match, '\n') == -1)
-				my_error("qmail-cat: read", 0, 2);
-			if (!match && line.len == 0)
-				break;
-			if (substdio_bput(&ssout, line.s, line.len) == -1)
-				my_error("qmail-cat: write", 0, 3);
-			if (substdio_flush(&ssout) == -1)
-				my_error("qmail-cat: write", 0, 3);
-		}
+	for (;;) {
+		if (getln(&ssin, &line, &match, '\n') == -1)
+			my_error("qmail-cat: read", 0, 2);
+		if (!match && line.len == 0)
+			break;
+		if (show_nonprinting || show_tabs || show_ends)
+			convert(line.s, line.len, show_nonprinting, show_tabs, show_ends);
+		else
+		if (substdio_bput(&ssout, line.s, line.len) == -1)
+			my_error("qmail-cat: write", 0, 3);
+		if (substdio_flush(&ssout) == -1)
+			my_error("qmail-cat: write", 0, 3);
 	}
 	if (substdio_flush(&ssout) == -1)
 		my_error("qmail-cat: write", 0, 3);
+}
+
+int
+main(int argc, char **argv)
+{
+	struct stat     st;
+	int             i, fd, mode = O_RDONLY, show_nonprinting = 0, show_ends = 0, show_tabs = 0;
+
+	while ((i = getopt(argc, argv, "etv")) != opteof) {
+		switch (i)
+		{
+		case 'e': /*- display $ at the end */
+			show_ends = 1;
+			break;
+		case 't': /*- show tables */
+			show_tabs = 1;
+			break;
+		case 'v': /*- show non-printing */
+			show_nonprinting = 1;
+			break;
+		}
+	}
+	sig_termcatch(sigterm);
+	if (!(argc - optind))
+		output(show_nonprinting, show_tabs, show_ends);
+	else {
+		for (i = optind; i < argc; i++) {
+			if (!str_diff(argv[i], "-")) {
+				if (dup2(0, 3) == -1)
+					my_error("qmail-cat: error duplicating descriptor 0 to 3", 0, 1);
+				break;
+			}
+		}
+		for (i = optind; i < argc; i++) {
+			if (!str_diff(argv[i], "-")) {
+				if (dup2(3, 0) == -1)
+					my_error("qmail-cat: error duplicating descriptor 3 to 0", 0, 1);
+			} else {
+				if (stat(argv[i], &st) == -1)
+					my_error("qmail-cat: stat", argv[i], 1);
+				mode = ((st.st_mode & S_IFMT) == S_IFIFO) ? O_RDWR : O_RDONLY;
+				if ((fd = open(argv[i], mode)) == -1)
+					my_error("qmail-cat: open", argv[i], 1);
+				if (dup2(fd, 0) == -1)
+					my_error("qmail-cat: error duplicating descriptor to 0", 0, 1);
+				if (fd && close(fd))
+					my_error("qmail-cat: close", 0, 1);
+			}
+			output(show_nonprinting, show_tabs, show_ends);
+		}
+	}
 	_exit(0);
 	/*- Not reached */
 	return(0);
@@ -139,7 +216,45 @@ main(int argc, char **argv)
 void
 getversion_qmail_cat_c()
 {
-	static char    *x = "$Id: qmail-cat.c,v 1.10 2024-01-23 01:24:23+05:30 Cprogrammer Exp mbhangui $";
+	static char    *x = "$Id: qmail-cat.c,v 1.11 2024-02-21 20:41:45+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
+
+/*
+ * $Log: qmail-cat.c,v $
+ * Revision 1.11  2024-02-21 20:41:45+05:30  Cprogrammer
+ * added -e, -t, -v options similar to gnu cat
+ *
+ * Revision 1.10  2024-01-23 01:24:23+05:30  Cprogrammer
+ * include buffer_defs.h for buffer size definitions
+ *
+ * Revision 1.9  2021-10-20 23:30:20+05:30  Cprogrammer
+ * added SIGTERM handler to flush io
+ *
+ * Revision 1.8  2021-08-29 23:27:08+05:30  Cprogrammer
+ * define functions as noreturn
+ *
+ * Revision 1.7  2009-10-03 15:02:59+05:30  Cprogrammer
+ * process all arguments on command line
+ * bug fix in my_error - 2nd arg never printed
+ *
+ * Revision 1.6  2009-03-31 09:07:55+05:30  Cprogrammer
+ * open files other than fifo in O_RDONLY
+ *
+ * Revision 1.5  2004-10-27 17:52:37+05:30  Cprogrammer
+ * stralloc_0() not needed if substdio_bput() is used instead of substdio_bputs()
+ *
+ * Revision 1.4  2004-10-22 20:28:10+05:30  Cprogrammer
+ * added RCS id
+ *
+ * Revision 1.3  2004-07-15 23:31:56+05:30  Cprogrammer
+ * fixed compilation warning
+ *
+ * Revision 1.2  2004-01-10 09:45:07+05:30  Cprogrammer
+ * close file descriptor
+ *
+ * Revision 1.1  2004-01-07 21:09:03+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
